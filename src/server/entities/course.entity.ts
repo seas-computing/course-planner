8c6fb41f578bd07ed5246d2e1d3594ca2cdd47d4
Robert Main <rmain@seas.harvard.edu>
2019-06-18 08:45:37 -0400

    Create course table

---
+++ src/server/entities/course.entity.ts
@@ -0,0 +1,119 @@
+import { Entity, Column, Index } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+export enum TERM_PATTERN {
+
+  /**
+   * This course is offered in the fall semester only
+   */
+  FALL = 'FALL',
+
+  /**
+   * This course is offered in the spring semester only
+   */
+  SPRING = 'SPRING',
+
+  /**
+   * This course is offered in both the spring and the fall semester
+   */
+  BOTH = 'BOTH',
+}
+
+
+/**
+ * The parent of many [[CourseInstance]] entities. The course entity is responsibile
+ * for managing area, title and course code information. This informaion does
+ * not change between course insances. It's modification would denote the
+ * creation of an entirely new course
+ */
+
+@Entity()
+export class Course extends BaseEntity {
+  /**
+   * The long title for the course
+   *
+   * @example `"Introduction to computer science"`
+   */
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    comment: 'The long title for the course (i.e - Introduction to computer'
+      + ' science)',
+  })
+  public title: string;
+
+  /**
+   * The alphabetical part of the course code (i.e - The CS in CS 50)
+   * @example `"CS"`
+   */
+  @Index()
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    comment: 'The alphabetical part of the course code (i.e - The CS in CS 50)',
+  })
+  public prefix: string;
+
+  /**
+   * The numerical part of the course code (i.e - the CS in CS 50). May also
+   * include an alphabetical component (i.e - CS 109b). This is split from
+   * [[prefix]] to facilitate indexing and searching on [[prefix]]
+   *
+   * @example `"50"`
+   * @example `"109B"`
+   */
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    comment: 'The numerical part of the course code (i.e - the CS in CS 50).'
+      + ' May also include an alphabetical component (i.e - CS 109b)',
+  })
+  public number: number;
+
+  /**
+   * Indicates whether or not this course is an undergraduate course.
+   */
+  @Column({
+    type: 'boolean',
+    nullable: false,
+    default: false,
+  })
+  public undergraduate: boolean = false;
+
+  /**
+   * Free text for administrators to record notes against a course
+   */
+  @Column({
+    type: 'text',
+    nullable: true,
+    comment: 'Free text for administrators to record notes against a course',
+  })
+  public readonly notes?: string;
+
+  /**
+   * Allows admin staff to hide courses and prevent their publication, either
+   * because the courses are non-SEAS courses and should not be displayed on
+   * the SEAS course schedule, or because they are still finalizing the course
+   * details
+  */
+  @Column({
+    type: 'boolean',
+    nullable: false,
+    default: true,
+    comment: 'Allows admin staff to hide courses and prevent their publication'
+    + ' either because the courses are non-SEAS courses and should not be'
+    + ' displayed on the SEAS course schedule, or because they are still'
+    + ' finalizing the course details',
+  })
+  public private: boolean = true;
+
+  /**
+   * The term this course is being delivered in. See [[TERM_PATTERN]] for
+   * allowed values.
+   */
+  @Column({
+    type: 'enum',
+    enum: Object.values(TERM_PATTERN),
+  })
+  public termPattern: TERM_PATTERN;
+}
---
+++ src/server/migrations/1560956281223-CourseTable.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CourseTable1560956281223 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE TYPE "course_termpattern_enum" AS ENUM(\'FALL\', \'SPRING\', \'BOTH\')');
+    await queryRunner.query('CREATE TABLE "course" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "title" character varying NOT NULL, "prefix" character varying NOT NULL, "number" character varying NOT NULL, "undergraduate" boolean NOT NULL DEFAULT false, "notes" text, "private" boolean NOT NULL DEFAULT true, "termPattern" "course_termpattern_enum" NOT NULL, CONSTRAINT "PK_bf95180dd756fd204fb01ce4916" PRIMARY KEY ("id"))');
+    await queryRunner.query('CREATE INDEX "IDX_57eb88c9f0f2c459e72a175109" ON "course" ("prefix") ');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP INDEX "IDX_57eb88c9f0f2c459e72a175109"');
+    await queryRunner.query('DROP TABLE "course"');
+    await queryRunner.query('DROP TYPE "course_termpattern_enum"');
+  }
+}
