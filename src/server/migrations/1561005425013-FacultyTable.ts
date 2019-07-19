151d7934d1c853fe81e66da372ee3a9e20494b80
Robert Main <rmain@seas.harvard.edu>
2019-07-19 12:22:00 -0400

    Swap email field for HUID
    The faculty member's email is not required, however their HUID is to differentiate between faculty members who may have the same name

--- src/server/migrations/1561005425013-FacultyTable.ts
+++ src/server/migrations/1561005425013-FacultyTable.ts
@@ -3,7 +3,8 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class FacultyTable1561005425013 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('CREATE TYPE "faculty_category_enum" AS ENUM(\'ladder\', \'non_ladder\')');
-    await queryRunner.query('CREATE TABLE "faculty" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "firstname" character varying, "lastname" character varying, "email" character varying, "category" "faculty_category_enum" NOT NULL DEFAULT \'non_ladder\', CONSTRAINT "PK_635ca3484f9c747b6635a494ad9" PRIMARY KEY ("id"))');
+    await queryRunner.query('CREATE TABLE "faculty" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "firstname" character varying, "lastname" character varying, "HUID" character varying NOT NULL, "category" "faculty_category_enum" NOT NULL DEFAULT \'non_ladder\', CONSTRAINT "PK_635ca3484f9c747b6635a494ad9" PRIMARY KEY ("id"))');
+    await queryRunner.query('CREATE INDEX "IDX_37a534d3bdf13e458a16d00e31" ON "faculty" ("HUID") ');
     await queryRunner.query('CREATE TABLE "faculty_course_instances_course_instance" ("facultyId" uuid NOT NULL, "courseInstanceId" uuid NOT NULL, CONSTRAINT "PK_43da76a139c24e501bcb78c45e2" PRIMARY KEY ("facultyId", "courseInstanceId"))');
     await queryRunner.query('CREATE INDEX "IDX_65d45c5857d419295ee55c16de" ON "faculty_course_instances_course_instance" ("facultyId") ');
     await queryRunner.query('CREATE INDEX "IDX_7d5d1b3b6714381a6e7ded5f63" ON "faculty_course_instances_course_instance" ("courseInstanceId") ');
@@ -17,6 +18,7 @@ export class FacultyTable1561005425013 implements MigrationInterface {
     await queryRunner.query('DROP INDEX "IDX_7d5d1b3b6714381a6e7ded5f63"');
     await queryRunner.query('DROP INDEX "IDX_65d45c5857d419295ee55c16de"');
     await queryRunner.query('DROP TABLE "faculty_course_instances_course_instance"');
+    await queryRunner.query('DROP INDEX "IDX_37a534d3bdf13e458a16d00e31"');
     await queryRunner.query('DROP TABLE "faculty"');
     await queryRunner.query('DROP TYPE "faculty_category_enum"');
   }
