d086f0711c65ce565757e4ef8693bac4e05953e8
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-19 09:15:04 -0500

    Move DAY enumerable to constants folder
    Remove DAY from src/server/entities/meeting.entity.ts and add it to src/common/constants/constants.ts folder.

---
+++ src/common/constants/constants.ts
@@ -0,0 +1,15 @@
+/**
+ * Set list of days on which a [[Meeting]] can be scheduled to occur
+ */
+export enum DAY {
+  /** Monday */
+  MON = 'MON',
+  /** Tuesday */
+  TUE = 'TUE',
+  /** Wednesday */
+  WED = 'WED',
+  /** Thursday */
+  THU = 'THU',
+  /** Friday */
+  FRI = 'FRI'
+}
