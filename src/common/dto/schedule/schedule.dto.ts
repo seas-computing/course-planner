9f9dd29c6443e71f2a1ee49afd71cccdbe99b1f4
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-19 10:42:33 -0500

    Add DTO example data in ApiModelProperty
    Pass DTO example data as an object into ApiModelProperty decorator

--- src/common/dto/schedule/schedule.dto.ts
+++ src/common/dto/schedule/schedule.dto.ts
@@ -1,59 +1,53 @@
 import { ApiModelProperty } from '@nestjs/swagger';
 import { DAY } from '../../constants/constants';
 
-/**
- * The following is an example of a response that may be returned from the server:
-* {
-*   "meetings": [
-*     {
-*       "startTime": {
-*         "hour": 14,
-*         "minute": 30
-*       },
-*       "endTime": {
-*         "hour": 16,
-*         "minute": 0
-*       },
-*       "duration": 90,
-*       "weekday": "TUE",
-*       "coursePrefix": "CS",
-*       "courses": [
-*         {
-*           id: "c7b1fa3f-c5b0-478d-a29c-7f85a4d80109",
-*           courseNumber: "209r",
-*           campus: "Cambridge",
-*           room: "Maxwell Dworkin 119",
-*           isUndergraduate: true,
-*         }
-*       ]
-*     }
-*   ]
-* }
-*/
-
 export abstract class ScheduleViewResponseDTO {
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: {
+      hour: 12,
+      minute: 0,
+    },
+  })
   public startTime: {
     hour: number;
     minute: number;
   };
 
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: {
+      hour: 13,
+      minute: 30,
+    },
+  })
   public endTime: {
     hour: number;
     minute: number;
   };
 
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: { duration: 90 },
+  })
   public duration: number;
 
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: DAY.TUE,
+  })
   public weekday: DAY;
 
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: 'CS',
+  })
   public coursePrefix: string;
 
-  @ApiModelProperty()
+  @ApiModelProperty({
+    example: {
+      id: '1252maefgjariejoqf',
+      courseNumber: '051',
+      campus: 'Cambridge',
+      room: 'Maxwell Dworkin 119',
+      isUndergraduate: true,
+    },
+  })
   public courses: [
     {
       id: string;
