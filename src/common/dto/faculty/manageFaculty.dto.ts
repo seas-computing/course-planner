267b60038d98e08ae3bca31724dfe2a283e671ac
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-21 14:44:47 -0500

    Add draft of manage faculty DTO
    Draft of manage faculty DTO including examples for Swagger documentation.

---
+++ src/common/dto/faculty/manageFaculty.dto.ts
@@ -0,0 +1,29 @@
+import { FACULTY_TYPE } from 'server/entities';
+import { ApiModelProperty } from '@nestjs/swagger';
+
+export abstract class ManageFacultyDTO {
+  @ApiModelProperty({
+    example: 'CS',
+  })
+  public coursePrefix: string;
+
+  @ApiModelProperty({
+    example: '80881118',
+  })
+  public HUID: string;
+
+  @ApiModelProperty({
+    example: 'Samantha',
+  })
+  public firstName: string;
+
+  @ApiModelProperty({
+    example: 'Johnston',
+  })
+  public lastName: string;
+
+  @ApiModelProperty({
+    example: FACULTY_TYPE.NON_SEAS_LADDER,
+  })
+  public facultyType: FACULTY_TYPE;
+}
