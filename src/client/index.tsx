2475b9182a8e3e37f769435233c0d7f5fbdf00eb
Chris Rogers <chrxr@outlook.com>
2019-11-22 10:28:59 -0500

    Fixing spacing issue generated by GITHUB!! bad github.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 
 render(
-        <BrowserRouter basename="/courses">
+  <BrowserRouter basename="/courses">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
