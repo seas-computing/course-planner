348fd91852b622247fe222e8bcfd61cd4d6474a5
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-11-25 15:47:09 -0500

    Add ManageCourse DTO
    Include ManageCourseResponseDTO with all applicable properties to build Manage Courses tab

---
+++ src/common/dto/courses/manageCourse.dto.ts
@@ -0,0 +1,49 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+import { TERM_PATTERN } from '../../constants';
+
+export abstract class ManageCourseResponseDTO {
+  @ApiModelProperty({
+    example: 'b8bc8456-51fd-48ef-b111-5a5990671cd1',
+  })
+  public id: string;
+
+  @ApiModelProperty({
+    example: 'Computational Modeling of Fluids and Soft Matter',
+  })
+  public title: string;
+
+  @ApiModelProperty({
+    example: 'ACS',
+  })
+  public area: string;
+
+  @ApiModelProperty({
+    example: 'AP',
+  })
+  public prefix: string;
+
+  @ApiModelProperty({
+    example: '274',
+  })
+  public number: string;
+
+  @ApiModelProperty({
+    example: TERM_PATTERN.FALL,
+  })
+  public termPattern?: TERM_PATTERN;
+
+  @ApiModelProperty({
+    example: false,
+  })
+  public isUndergraduate: boolean;
+
+  @ApiModelProperty({
+    example: true,
+  })
+  public isSeas: boolean;
+
+  @ApiModelProperty({
+    example: 'AC 227',
+  })
+  public sameAs?: string;
+}
