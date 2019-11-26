9cdf992e1097a2a5844ab07e209584ba97c42af6
Robert Main <rmain@seas.harvard.edu>
2019-11-26 10:24:21 -0500

    Add missing enum

---
+++ src/common/constants/termPattern.ts
@@ -0,0 +1,19 @@
+enum TERM_PATTERN {
+
+  /**
+   * This course is offered in the fall semester only
+   */
+  FALL = 'FALL',
+
+  /**
+   * This course is offered in the spring semester only
+   */
+  SPRING = 'SPRING',
+
+  /**
+   * This course is offered in both the spring and the fall semester
+   */
+  BOTH = 'BOTH',
+}
+
+export default TERM_PATTERN;
