743db3ff2c0af0acee509b7db687b2936e91aa82
Robert Main <rmain@seas.harvard.edu>
2019-12-04 11:29:51 -0500

    Add swagger decorators
    Add previously missing swagger decorators to document the controller operation and return value type

--- src/server/course/course.controller.ts
+++ src/server/course/course.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { ManageCourseResponseDTO } from 'common/dto/courses/manageCourse.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { Course } from './course.entity';
 
 @Controller('course')
@@ -10,6 +11,12 @@ export class CourseController {
   private courseRepository: Repository<Course>
 
   @Get('/')
+  @ApiOperation({ title: 'Retrieve all courses in the database' })
+  @ApiOkResponse({
+    type: ManageCourseResponseDTO,
+    description: 'An array of all the courses along with their area',
+    isArray: true,
+  })
   public async index(): Promise<ManageCourseResponseDTO[]> {
     const courses = await this.courseRepository.find({
       relations: ['area'],
