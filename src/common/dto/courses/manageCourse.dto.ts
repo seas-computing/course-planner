085780195f799a924c2ac3ab637ac21032827846
Robert Main <rmain@seas.harvard.edu>
2019-12-04 11:30:51 -0500

    Document area property structure
    Break the `area` property out into it's own type to allow it to be documented using swagger decorators.

--- src/common/dto/courses/manageCourse.dto.ts
+++ src/common/dto/courses/manageCourse.dto.ts
@@ -1,6 +1,20 @@
 import { ApiModelProperty } from '@nestjs/swagger';
 import { TERM_PATTERN } from 'common/constants';
 
+export abstract class CourseArea {
+  @ApiModelProperty({
+    type: 'string',
+    example: 'a49edd11-0f2d-4d8f-9096-a4062955a11a',
+  })
+  public id: string;
+
+  @ApiModelProperty({
+    type: 'string',
+    example: 'ACS',
+  })
+  public name: string;
+}
+
 export abstract class ManageCourseResponseDTO {
   @ApiModelProperty({
     example: 'b8bc8456-51fd-48ef-b111-5a5990671cd1',
@@ -13,15 +27,9 @@ export abstract class ManageCourseResponseDTO {
   public title: string;
 
   @ApiModelProperty({
-    example: {
-      id: 'cd823f34-a4dc-4485-a310-7f9b885df770',
-      name: 'ACS',
-    },
+    type: CourseArea,
   })
-  public area: {
-    id: string;
-    name: string;
-  };
+  public area: CourseArea;
 
   @ApiModelProperty({
     example: 'AP 227',
