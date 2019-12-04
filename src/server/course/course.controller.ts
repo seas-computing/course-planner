6bec3802e4e9886a872854643b07428af456f88b
Robert Main <rmain@seas.harvard.edu>
2019-12-04 10:32:30 -0500

    Return all courses from the database

--- src/server/course/course.controller.ts
+++ src/server/course/course.controller.ts
@@ -1,11 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { ManageCourseResponseDTO } from 'common/dto/courses/manageCourse.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Course } from './course.entity';
 
 @Controller('course')
 export class CourseController {
+  @InjectRepository(Course)
+  private courseRepository: Repository<Course>
+
   @Get('/')
-  public async index(): Promise<any> {
-    
+  public async index(): Promise<ManageCourseResponseDTO[]> {
+    const courses = await this.courseRepository.find({
+      relations: ['area'],
+    });
+
+    return courses.map(({
+      updatedAt, // eslint-disable-line  @typescript-eslint/no-unused-vars
+      createdAt, // eslint-disable-line  @typescript-eslint/no-unused-vars
+      ...course
+    }: Course): ManageCourseResponseDTO => ({
+      ...course,
+      area: {
+        id: course.area.id,
+        name: course.area.name,
+      },
+      catalogNumber: `${course.prefix} ${course.number}`,
+    }));
   }
 }
--- src/server/course/course.module.ts
+++ src/server/course/course.module.ts
@@ -1,8 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { CourseController } from './course.controller';
+import { Course } from './course.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([
+      Course,
+    ]),
   ],
   controllers: [CourseController],
   providers: [
