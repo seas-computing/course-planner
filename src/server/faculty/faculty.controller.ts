bb9456cc8fad81cbea53285f4e0917d36725eb1b
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-12-05 20:07:11 -0500

    Add draft of GET controller for manage faculty
    Include GET action for manage faculty controller

--- src/server/faculty/faculty.controller.ts
+++ src/server/faculty/faculty.controller.ts
@@ -1,10 +1,40 @@
 import { Controller, Get } from '@nestjs/common';
 import { ManageFacultyResponseDTO } from 'common/dto/faculty/manageFaculty.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+import { Faculty } from './faculty.entity';
 
 @Controller('faculty')
-export class ManageFaculty {
+export class ManageFacultyController {
+  @InjectRepository(Faculty)
+  private facultyRepository: Repository<Faculty>
+
   @Get('/')
-  public index(): Promise<ManageFacultyResponseDTO[]> {
-    return Promise.resolve([]);
+  @ApiOperation({ title: 'Retrieve all faculty in the database' })
+  @ApiOkResponse({
+    type: ManageFacultyResponseDTO,
+    description: 'An array of all the faculty along with their area',
+    isArray: true,
+  })
+  public async index(): Promise<ManageFacultyResponseDTO[]> {
+    const facultyMembers = await this.facultyRepository.find({
+      relations: ['area'],
+    });
+    return facultyMembers.map(({
+      updatedAt,
+      createdAt,
+      category: facultyType,
+      ...faculty
+    }: Faculty): ManageFacultyResponseDTO => ({
+      ...faculty,
+      area: {
+        // id: faculty.area.id,
+        // name: faculty.area.name,
+        id: 'fake id',
+        name: 'fake name',
+      },
+      facultyType,
+    }));
   }
 }
