19323fd25377cb4360c4b2b12c6440caa058db92
Robert Main <rmain@seas.harvard.edu>
2019-12-10 15:57:39 -0500

    Authentication guard requires authentication in production mode

--- src/server/auth/__tests__/authentication.guard.test.ts
+++ src/server/auth/__tests__/authentication.guard.test.ts
@@ -1,4 +1,7 @@
 import { strictEqual } from 'assert';
+import { ExecutionContext } from '@nestjs/common';
+import { stub } from 'sinon';
+import { AuthGuard } from '@nestjs/passport';
 import { Authentication } from '../../auth/authentication.guard';
 
 describe('Authentication guard', function () {
@@ -6,5 +9,14 @@ describe('Authentication guard', function () {
     const guard = new Authentication({ isProduction: false } as never);
 
     strictEqual(guard.canActivate({} as never), true);
+  });
+
+  it('requries authentication in production mode', function () {
+    AuthGuard('saml').prototype.canActivate = stub();
+
+    new Authentication({ isProduction: true } as never)
+      .canActivate({} as ExecutionContext);
+
+    strictEqual(AuthGuard('saml').prototype.canActivate.callCount, 1);
   });
 });
