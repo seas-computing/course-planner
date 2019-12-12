2b3f726404d8d5daf6e8192a12328646e3109492
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2019-12-12 10:49:31 -0500

    Add Update Faculty DTO
    'id' is not an included field, because the controller will grab the id from the url.

---
+++ src/common/dto/faculty/updateFaculty.dto.ts
@@ -0,0 +1,70 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  ValidateIf,
+  IsNotEmptyObject,
+} from 'class-validator';
+import { FACULTY_TYPE } from '../../constants';
+
+export abstract class FacultyArea {
+  @ApiModelProperty({
+    type: 'string',
+    example: 'b38dcc00-0f2d-4d8f-9096-b5173067b22b',
+  })
+  @IsUUID()
+  public id: string;
+
+  @ApiModelProperty({
+    type: 'string',
+    example: 'AP',
+  })
+  @IsString()
+  @IsNotEmpty()
+  public name: string;
+}
+
+export abstract class UpdateFacultyDTO {
+  @ApiModelProperty({
+    example: '87654321',
+  })
+  @IsNotEmpty()
+  public HUID: string;
+
+  @ApiModelProperty({
+    example: 'Ada',
+  })
+  @ValidateIf(({ lastName }): boolean => lastName === undefined)
+  @IsNotEmpty()
+  public firstName?: string;
+
+  @ApiModelProperty({
+    example: 'Lovelace',
+  })
+  @ValidateIf(({ firstName }): boolean => firstName === undefined)
+  @IsNotEmpty()
+  public lastName?: string;
+
+  @ApiModelProperty({
+    example: FACULTY_TYPE.NON_SEAS_LADDER,
+    enum: FACULTY_TYPE,
+  })
+  @IsEnum(FACULTY_TYPE)
+  public facultyType: FACULTY_TYPE;
+
+  @ApiModelProperty({
+    type: FacultyArea,
+  })
+  @IsNotEmptyObject()
+  public area: FacultyArea;
+
+  @ApiModelProperty({
+    example: 'PHYS (1 FTE SEAS)',
+  })
+  @IsOptional()
+  @IsString()
+  public jointWith?: string;
+}
