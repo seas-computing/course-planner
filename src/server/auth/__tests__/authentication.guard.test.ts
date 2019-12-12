655cb5f1597229b74c2a1b4882c2fe39a3b4aacc
Robert Main <rmain@seas.harvard.edu>
2019-12-12 08:43:50 -0500

    Update guard params and return types to use correct typings.
    The `never` type is meant to be used to tell typescript that a value will never occur(https://www.typescriptlang.org/docs/handbook/basic-types.html#never). This was being used to to allow passing of a stub `ExecutionContext` object(and TypeScript didn't allow `Partial<ExecutionContext`). Ultimately, an empty object was passed and cast to `ExecutionContext` as none of the methods from `ExecutionContext` are being used. Guard parameters were updated to be passed an instance of `ConfigService` instead of an object literal typed as `never`.

--- src/server/auth/__tests__/authentication.guard.test.ts
+++ src/server/auth/__tests__/authentication.guard.test.ts
@@ -2,6 +2,7 @@ import { strictEqual } from 'assert';
 import { ExecutionContext } from '@nestjs/common';
 import { stub } from 'sinon';
 import { AuthGuard } from '@nestjs/passport';
+import { ConfigService } from 'server/config/config.service';
 import { Authentication } from '../../auth/authentication.guard';
 
 describe('Authentication guard', function () {
@@ -12,14 +13,14 @@ describe('Authentication guard', function () {
     AuthGuard('saml').prototype.canActivate.reset();
   });
 
-  it('allows access in development mode', async function () {
-    const guard = new Authentication({ isProduction: false } as never);
+  it('allows access in development mode', function () {
+    const guard = new Authentication(new ConfigService({ NODE_ENV: 'development' }));
 
-    strictEqual(guard.canActivate({} as never), true);
+    strictEqual(guard.canActivate({} as ExecutionContext), true);
   });
 
   it('requries authentication in production mode', function () {
-    new Authentication({ isProduction: true } as never)
+    new Authentication(new ConfigService({ NODE_ENV: 'production' }))
       .canActivate({} as ExecutionContext);
 
     strictEqual(AuthGuard('saml').prototype.canActivate.callCount, 1);
