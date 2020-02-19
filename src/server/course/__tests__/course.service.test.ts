ce9347f206c915816d07439e6d8db4b99b128c2e
Robert Main <rmain@seas.harvard.edu>
2020-02-19 15:38:24 -0500

    Course service schedules one CourseInstance per semester in the database

--- src/server/course/__tests__/course.service.test.ts
+++ src/server/course/__tests__/course.service.test.ts
@@ -2,11 +2,16 @@ import { TestingModule, Test } from '@nestjs/testing';
 import { stub, SinonStub } from 'sinon';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { strictEqual } from 'assert';
+import { Semester, TERM } from 'server/semester/semester.entity';
 import { CourseService } from '../course.service';
 import { Course } from '../course.entity';
 
 const mockCourseRespository = {
   insert: stub(),
+};
+
+const mockSemesterRepository = {
+  find: stub(),
 };
 
 describe('Course service', function () {
@@ -19,6 +24,10 @@ describe('Course service', function () {
           provide: getRepositoryToken(Course),
           useValue: mockCourseRespository,
         },
+        {
+          provide: getRepositoryToken(Semester),
+          useValue: mockSemesterRepository,
+        },
       ],
       controllers: [],
     }).compile();
@@ -28,12 +37,17 @@ describe('Course service', function () {
   afterEach(function () {
     Object.values({
       ...mockCourseRespository,
+      ...mockSemesterRepository,
     }).forEach((sinonStub: SinonStub): void => {
       sinonStub.reset();
     });
   });
 
   describe('insert', function () {
+    beforeEach(function () {
+      mockSemesterRepository.find.resolves([]);
+    });
+
     it('creates one new course for every object provided', async function () {
       const coursesToCreate: Course[] = [
         new Course(),
@@ -45,6 +59,28 @@ describe('Course service', function () {
       strictEqual(
         mockCourseRespository.insert.args[0][0].length,
         coursesToCreate.length
+      );
+    });
+
+    it('schedules one CourseInstance per semester in the database', async function () {
+      const semesters: Semester[] = [
+        {
+          term: TERM.SPRING,
+          academicYear: 2020,
+        } as Semester,
+        {
+          term: TERM.FALL,
+          academicYear: 2020,
+        } as Semester,
+      ];
+
+      mockSemesterRepository.find.resolves(semesters);
+
+      await courseService.insert([new Course()]);
+
+      strictEqual(
+        mockCourseRespository.insert.args[0][0][0].instances.length,
+        2
       );
     });
   });
--- src/server/course/course.service.ts
+++ src/server/course/course.service.ts
@@ -2,13 +2,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeepPartial } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { Course } from './course.entity';
+import { CourseInstance } from '../courseInstance/courseinstance.entity';
+import { Semester } from '../semester/semester.entity';
 
 @Injectable()
 export class CourseService {
+  @InjectRepository(Semester)
+  private semesterRepository: Repository<Semester>;
+
   @InjectRepository(Course)
   private courseRepository: Repository<Course>;
 
   public async insert(courses: DeepPartial<Course>[]): Promise<void> {
-    await this.courseRepository.insert(courses);
+    const semesters = await this.semesterRepository.find({});
+
+    const scheduledCourses = courses.map((course: Course): Course => ({
+      ...course,
+      instances: semesters.map((semester: Semester): CourseInstance => ({
+        ...new CourseInstance(),
+        semester,
+      })),
+    }));
+
+    await this.courseRepository.insert(scheduledCourses);
   }
 }
