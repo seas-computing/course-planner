acc3bc9e37f12a992a4ec977fc173ab1ab72e0ba
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-03-11 16:19:19 -0400

    set up master population service

--- tests/mocks/database/population/population.service.ts
+++ tests/mocks/database/population/population.service.ts
@@ -3,6 +3,8 @@ import {
 } from '@nestjs/common';
 import { SemesterPopulationService } from './semester.population';
 import { AreaPopulationService } from './area.population';
+import { RoomPopulationService } from './room.population';
+import { CoursePopulationService } from './course.population';
 
 
 @Injectable()
@@ -15,13 +17,23 @@ export class PopulationService implements
   @Inject(AreaPopulationService)
   protected areaService: AreaPopulationService;
 
+  @Inject(RoomPopulationService)
+  protected roomService: RoomPopulationService;
+
+  @Inject(CoursePopulationService)
+  protected courseService: CoursePopulationService;
+
   public async onApplicationBootstrap(): Promise<void> {
     await this.areaService.populate();
     await this.semesterService.populate();
+    await this.roomService.populate();
+    await this.courseService.populate();
   }
 
   public async beforeApplicationShutdown(): Promise<void> {
-    await this.areaService.drop();
+    await this.courseService.drop();
+    await this.roomService.drop();
     await this.semesterService.drop();
+    await this.areaService.drop();
   }
 }
