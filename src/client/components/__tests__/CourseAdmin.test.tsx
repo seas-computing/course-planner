e7d8d91453455f0b55853422dc76130a85f78dd7
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-03-12 18:44:22 -0400

    Add tests to check that catalogNumber and course title match
    Include tests that check that the data being rendered in the table match what is expected. Use computed values instead of hard coding data for actual results

--- src/client/components/__tests__/CourseAdmin.test.tsx
+++ src/client/components/__tests__/CourseAdmin.test.tsx
@@ -11,6 +11,7 @@ import {
 import {
   render,
   waitForElement,
+  wait,
 } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import {
@@ -19,7 +20,11 @@ import {
 } from 'sinon';
 import request,
 { AxiosResponse } from 'axios';
-import * as dummy from 'testData';
+import {
+  computerScienceCourseResponse,
+  physicsCourseResponse,
+  error,
+} from 'testData';
 import * as api from 'client/api';
 import {
   MessageContext,
@@ -30,15 +35,35 @@ import { MarkOneTheme } from 'mark-one';
 import { ManageCourseResponseDTO } from 'common/dto/courses/ManageCourseResponse.dto';
 import CourseAdmin from '../pages/CourseAdmin';
 
+const AppStub: FunctionComponent = function ({ children }): ReactElement {
+  const [, dispatchMessage] = useReducer(
+    messageReducer,
+    {
+      queue: [],
+      currentMessage: undefined,
+    }
+  );
+  return (
+    <MemoryRouter>
+      <ThemeProvider theme={MarkOneTheme}>
+        <MessageContext.Provider value={dispatchMessage}>
+          {children}
+        </MessageContext.Provider>
+      </ThemeProvider>
+    </MemoryRouter>
+  );
+};
+
 describe('Course Admin', function () {
   let getStub: SinonStub;
+  const testData = [
+    computerScienceCourseResponse,
+    physicsCourseResponse,
+  ];
   beforeEach(function () {
     getStub = stub(request, 'get');
     getStub.resolves({
-      data: [
-        dummy.computerScienceCourseResponse,
-        dummy.physicsCourseResponse,
-      ],
+      data: testData,
     } as AxiosResponse<ManageCourseResponseDTO[]>);
   });
   afterEach(function () {
@@ -55,24 +80,6 @@ describe('Course Admin', function () {
       );
       return waitForElement(() => container.querySelector('.course-admin-table'));
     });
-    const AppStub: FunctionComponent = function ({ children }): ReactElement {
-      const [, dispatchMessage] = useReducer(
-        messageReducer,
-        {
-          queue: [],
-          currentMessage: undefined,
-        }
-      );
-      return (
-        <MemoryRouter>
-          <ThemeProvider theme={MarkOneTheme}>
-            <MessageContext.Provider value={dispatchMessage}>
-              {children}
-            </MessageContext.Provider>
-          </ThemeProvider>
-        </MemoryRouter>
-      );
-    };
     context('when course data fetch succeeds', function () {
       it('displays the correct course information', async function () {
         const { getByText } = render(
@@ -81,41 +88,67 @@ describe('Course Admin', function () {
           </AppStub>
         );
         strictEqual(getStub.callCount, 1);
-        const { title } = dummy.computerScienceCourseResponse;
+        const { title } = computerScienceCourseResponse;
         return waitForElement(() => getByText(title));
       });
-      it('displays the correct number of rows in the table', function () {
+      it('displays the correct number of rows in the table', async function () {
         const { getAllByRole } = render(
           <AppStub>
             <CourseAdmin />
           </AppStub>
         );
-        const rows = getAllByRole('row', { exact: false });
-        strictEqual(rows.length, 3);
+        await wait(() => getAllByRole('row').length > 1);
+        const rows = getAllByRole('row');
+        strictEqual(rows.length, testData.length + 1);
+      });
+      it('displays the correct content in the table cells', async function () {
+        const { getAllByRole } = render(
+          <AppStub>
+            <CourseAdmin />
+          </AppStub>
+        );
+        await wait(() => getAllByRole('row').length > 1);
+        const rows = Array.from(getAllByRole('row')) as HTMLTableRowElement[];
+        const rowsContent = rows
+          .map((x) => (Array.from(x.cells).map((y) => y.textContent)));
+        const computerScienceCourseCatalogNumber = rowsContent[1][1];
+        const computerScienceCourseTitle = rowsContent[1][2];
+        const physicsCourseCatalogNumber = rowsContent[2][1];
+        const physicsCourseTitle = rowsContent[2][2];
+        strictEqual(computerScienceCourseCatalogNumber,
+          computerScienceCourseResponse.catalogNumber);
+        strictEqual(computerScienceCourseTitle,
+          computerScienceCourseResponse.title);
+        strictEqual(physicsCourseCatalogNumber,
+          physicsCourseResponse.catalogNumber);
+        strictEqual(physicsCourseTitle, physicsCourseResponse.title);
       });
       context('when there are no course records', function () {
+        const emptyTestData = [];
         beforeEach(function () {
           getStub.resolves({
-            data: [],
+            data: emptyTestData,
           } as AxiosResponse<ManageCourseResponseDTO[]>);
         });
         afterEach(function () {
           getStub.restore();
         });
-        it('displays the correct number of rows in the table (only the header row)', function () {
-          const { getAllByRole } = render(
-            <AppStub>
-              <CourseAdmin />
-            </AppStub>
-          );
-          const rows = getAllByRole('row');
-          strictEqual(rows.length, 1);
-        });
+        it('displays the correct number of rows in the table (only the header row)',
+          async function () {
+            const { getAllByRole } = render(
+              <AppStub>
+                <CourseAdmin />
+              </AppStub>
+            );
+            await wait(() => getAllByRole('row').length > 0);
+            const rows = getAllByRole('row');
+            strictEqual(rows.length, emptyTestData.length + 1);
+          });
       });
     });
     context('when course data fetch fails', function () {
       beforeEach(function () {
-        getStub.rejects(dummy.error);
+        getStub.rejects(error);
       });
       afterEach(function () {
         getStub.restore();
@@ -125,7 +158,7 @@ describe('Course Admin', function () {
           await api.getAllCourses();
           fail('Did not throw an error');
         } catch (err) {
-          deepStrictEqual(err, dummy.error);
+          deepStrictEqual(err, error);
         }
       });
     });
