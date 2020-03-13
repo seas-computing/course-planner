b3a353d7c83d350f840e263c61d3e5eebd061dcf
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-03-13 15:36:18 -0400

    Create CourseInstance Service
    Uses querybuilder to get the appropriate data from the database to
    populate our view entities and create the DTO object.

---
+++ src/server/courseInstance/courseInstance.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CourseListingView } from 'server/course/CourseListingView.entity';
+import CourseInstanceResponseDTO from '../../common/dto/courses/CourseInstanceResponse';
+
+
+@Injectable()
+export class CourseInstanceService {
+  @InjectRepository(CourseListingView)
+  private readonly courseRepository: Repository<CourseListingView>
+
+  /**
+   * Resolves a list of courses, which in turn contain sub-lists of instances
+   * split up by semesters, with embedded faculty and meetings lists.
+   * @param acadYear The academic year for which courses are requested.
+   *   Corresponds to the year of the spring semester.
+   */
+
+  public async getAllByYear(
+    acadYear: number
+  ): Promise<CourseInstanceResponseDTO[]> {
+    const prevYear = acadYear - 1;
+
+    const courseQuery = this.courseRepository
+      .createQueryBuilder('c')
+      .leftJoinAndMapOne(
+        'c.spring',
+        'CourseInstanceListingView',
+        'spring',
+        'spring."courseId" = c.id AND spring.term = \'SPRING\''
+      )
+      .leftJoinAndMapMany(
+        'spring.instructors',
+        'FacultyListingView',
+        'spring_instructors',
+        'spring_instructors."courseInstanceId" = spring.id'
+      )
+      .leftJoinAndMapMany(
+        'spring.meetings',
+        'MeetingListingView',
+        'spring_meetings',
+        '"spring_meetings"."courseInstanceId" = spring.id'
+      )
+      .leftJoinAndMapOne(
+        'spring_meetings.room',
+        'RoomListingView',
+        'spring_meetings_room',
+        '"spring_meetings_room".id = "spring_meetings"."roomId"'
+      )
+      .leftJoinAndMapOne(
+        'c.fall',
+        'CourseInstanceListingView',
+        'fall',
+        'fall."courseId" = c.id AND fall.term = \'FALL\''
+      )
+      .leftJoinAndMapOne(
+        'fall.instructors',
+        'FacultyListingView',
+        'fall_instructors',
+        'fall_instructors."courseInstanceId" = fall.id'
+      )
+      .leftJoinAndMapMany(
+        'fall.meetings',
+        'MeetingListingView',
+        'fall_meetings',
+        'fall_meetings."courseInstanceId" = fall.id'
+      )
+      .leftJoinAndMapOne(
+        'fall_meetings.room',
+        'RoomListingView',
+        'fall_meetings_room',
+        'fall_meetings_room.id = fall_meetings."roomId"'
+      )
+      .where('fall."calendarYear" = :prevYear', { prevYear })
+      .andWhere('spring."calendarYear" = :acadYear', { acadYear })
+      .orderBy('fall_instructors."instructorOrder"', 'ASC')
+      .addOrderBy('spring_instructors."instructorOrder"', 'ASC');
+
+    return courseQuery.getMany() as unknown as CourseInstanceResponseDTO[];
+  }
+}
