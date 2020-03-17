b83cf5c4ad1ae43bcc2cee6d2ef0ce7f4740deda
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-03-17 16:24:52 -0400

    Add content to Faculty Admin View
    Include Faculty Admin table for the faculty admin view. Populate table with data

--- src/client/api/faculty.ts
+++ src/client/api/faculty.ts
@@ -4,7 +4,7 @@ import { FacultyResponseDTO } from '../../common/dto/faculty/facultyResponse.dto
 /**
  * Retrieves all faculty
  */
-export const getAllFaculty = async (): Promise<FacultyResponseDTO[]> => {
+export const getAllFacultyMembers = async (): Promise<FacultyResponseDTO[]> => {
   const response = await request.get('/api/faculty');
   return response.data;
 };
