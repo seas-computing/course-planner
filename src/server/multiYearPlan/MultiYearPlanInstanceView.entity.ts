84c5e08a5c168aaa3abda74fc99c51b142bc46c7
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-04-10 12:32:26 -0400

    Separate MultiYearPlanInstanceView into its own file

---
+++ src/server/multiYearPlan/MultiYearPlanInstanceView.entity.ts
@@ -0,0 +1,77 @@
+import {
+  ViewEntity,
+  Connection,
+  ViewColumn,
+  SelectQueryBuilder,
+} from 'typeorm';
+import {
+  Semester,
+  TERM,
+} from 'server/semester/semester.entity';
+import { FacultyListingView } from 'server/faculty/FacultyListingView.entity';
+import { CourseInstance } from '../courseInstance/courseinstance.entity';
+
+@ViewEntity('MultiYearPlanInstanceView', {
+  expression: (connection: Connection):
+  SelectQueryBuilder<CourseInstance> => connection.createQueryBuilder()
+    .select('ci.id', 'id')
+    .addSelect('ci."courseId"', 'courseId')
+    .addSelect('s.term', 'term')
+    .addSelect(`CASE
+        WHEN term == '${TERM.FALL}' THEN CAST((CAST(s.academicYear AS INTEGER) - 1) AS VARCHAR
+        ELSE s.academicYear
+      END`, 'calendarYear')
+    .leftJoin(Semester, 's', 's.id = ci."semesterId"')
+    // left join to FacultyInstance
+    // then left join to Faculty
+    .leftJoinAndMapMany(
+      'ci.faculty',
+      'FacultyListingView',
+      'instructors',
+      'instructors."courseInstanceId" = ci.id'
+    )
+    .from(CourseInstance, 'ci'),
+})
+/**
+ * Represents a course instance within [[MultiYearPlanView]]
+ */
+export class MultiYearPlanInstanceView {
+  /**
+   * From [[CourseInstance]]
+   */
+  @ViewColumn()
+  public id: string;
+
+  /**
+   * From [[CourseInstance]]
+   * The id of the parent course
+   */
+  @ViewColumn()
+  public courseId: string;
+
+  /**
+   * From [[Semester]]
+   * The academic year in which the course instances takes place
+   */
+  @ViewColumn()
+  public academicYear: number;
+
+  /**
+   * From [[Semester]]
+   * The calendar year in which the course instances takes place
+   */
+  @ViewColumn()
+  public calendarYear: number;
+
+  /**
+   * From [[Semester]]
+   * The term (Spring or Fall) in which the course instance takes place
+   */
+  @ViewColumn()
+  public term: TERM;
+
+  /**
+   * One [[MultiYearPlanInstanceView]] has many [[FacultyListingView]]
+   */
+  public faculty: FacultyListingView[];
+}
--- src/server/multiYearPlan/MultiYearPlanView.entity.ts
+++ src/server/multiYearPlan/MultiYearPlanView.entity.ts
@@ -4,79 +4,9 @@ import {
   ViewColumn,
   SelectQueryBuilder,
 } from 'typeorm';
-import {
-  Semester,
-  TERM,
-} from 'server/semester/semester.entity';
 import { Course } from 'server/course/course.entity';
-import { FacultyListingView } from 'server/faculty/FacultyListingView.entity';
 import { Area } from 'server/area/area.entity';
-import { CourseInstance } from '../courseInstance/courseinstance.entity';
-
-@ViewEntity('MultiYearPlanInstanceView', {
-  expression: (connection: Connection):
-  SelectQueryBuilder<CourseInstance> => connection.createQueryBuilder()
-    .select('ci.id', 'id')
-    .addSelect('ci."courseId"', 'courseId')
-    .addSelect('s.term', 'term')
-    .addSelect(`CASE
-        WHEN term == '${TERM.FALL}' THEN CAST((CAST(s.academicYear AS INTEGER) - 1) AS VARCHAR
-        ELSE s.academicYear
-      END`, 'calendarYear')
-    .leftJoin(Semester, 's', 's.id = ci."semesterId"')
-    // left join to FacultyInstance
-    // then left join to Faculty
-    .leftJoinAndMapMany(
-      'ci.faculty',
-      'FacultyListingView',
-      'instructors',
-      'instructors."courseInstanceId" = ci.id'
-    )
-    .from(CourseInstance, 'ci'),
-})
-/**
- * Represents a course instance within [[MultiYearPlanView]]
- */
-export class MultiYearPlanInstanceView {
-  /**
-   * From [[CourseInstance]]
-   */
-  @ViewColumn()
-  public id: string;
-
-  /**
-   * From [[CourseInstance]]
-   * The id of the parent course
-   */
-  @ViewColumn()
-  public courseId: string;
-
-  /**
-   * From [[Semester]]
-   * The academic year in which the course instances takes place
-   */
-  @ViewColumn()
-  public academicYear: number;
-
-  /**
-   * From [[Semester]]
-   * The calendar year in which the course instances takes place
-   */
-  @ViewColumn()
-  public calendarYear: number;
-
-  /**
-   * From [[Semester]]
-   * The term (Spring or Fall) in which the course instance takes place
-   */
-  @ViewColumn()
-  public term: TERM;
-
-  /**
-   * One [[MultiYearPlanInstanceView]] has many [[FacultyListingView]]
-   */
-  public faculty: FacultyListingView[];
-}
+import { MultiYearPlanInstanceView } from './MultiYearPlanInstanceView.entity';
 
 /**
  * A subset of fields from the [[Course]]
