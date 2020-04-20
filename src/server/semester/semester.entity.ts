138a01cc022d5b8f3a1d9a83ef325c63d4c8ed58
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-04-20 09:51:57 -0400

    Fix typo 'academic'

--- src/server/semester/semester.entity.ts
+++ src/server/semester/semester.entity.ts
@@ -3,6 +3,7 @@ import {
   Column,
   ObjectType,
   OneToMany,
+  ValueTransformer,
 } from 'typeorm';
 import { BaseEntity } from '../base/base.entity';
 import { CourseInstance } from '../courseInstance/courseinstance.entity';
@@ -14,6 +15,21 @@ export enum TERM {
   SPRING = 'SPRING',
 }
 
+/**
+ * This class allows an year column to be returned as a number
+ * when it would otherwise be returned as a string.
+ * See: https://github.com/typeorm/typeorm/issues/873
+ */
+export class YearTransformer implements ValueTransformer {
+  public to(data: number): number {
+    return data;
+  }
+
+  public from(data: string): number {
+    return parseInt(data, 10);
+  }
+}
+
 @Entity()
 export class Semester extends BaseEntity {
   /**
@@ -23,7 +39,10 @@ export class Semester extends BaseEntity {
     type: 'numeric',
     precision: 4,
     scale: 0,
-    comment: 'The academmic year as a 4 digit integer',
+    comment: 'The academic year as a 4 digit integer',
+    // Ensure that the academicYear property is returned as a number
+    // See: https://github.com/typeorm/typeorm/issues/873
+    transformer: new YearTransformer(),
   })
   public academicYear: number;
 
