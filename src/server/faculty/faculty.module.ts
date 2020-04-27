b66ee9042aa1d72413936f0e3c163d0faeb072c8
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-04-27 16:21:44 -0400

    Add Faculty controller, service, and views to the faculty module

--- src/server/faculty/faculty.module.ts
+++ src/server/faculty/faculty.module.ts
@@ -1,22 +1,46 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { Course } from 'server/course/course.entity';
+import { Semester } from 'server/semester/semester.entity';
+import { CourseInstance } from 'server/courseInstance/courseinstance.entity';
+import { Absence } from 'server/absence/absence.entity';
+import { FacultyCourseInstance } from 'server/courseInstance/facultycourseinstance.entity';
 import { ManageFacultyController } from './faculty.controller';
 import { Faculty } from './faculty.entity';
 import { Area } from '../area/area.entity';
 import { MultiYearPlanFacultyListingView } from '../courseInstance/MultiYearPlanFacultyListingView.entity';
 import { FacultyService } from './faculty.service';
+import { FacultyScheduleController } from './facultySchedule.controller';
+import { FacultyScheduleService } from './facultySchedule.service';
+import { FacultyScheduleView } from './FacultyScheduleView.entity';
+import { FacultyScheduleAbsenceView } from './FacultyScheduleAbsenceView.entity';
+import { FacultyScheduleCourseView } from './FacultyScheduleCourseView.entity';
+import { FacultyScheduleSemesterView } from './FacultyScheduleSemesterView.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Faculty,
       Area,
+      Semester,
+      Course,
+      CourseInstance,
+      Absence,
+      FacultyCourseInstance,
+      FacultyScheduleAbsenceView,
+      FacultyScheduleCourseView,
+      FacultyScheduleSemesterView,
+      FacultyScheduleView,
     ]),
   ],
-  controllers: [ManageFacultyController],
+  controllers: [
+    ManageFacultyController,
+    FacultyScheduleController,
+  ],
   providers: [
     MultiYearPlanFacultyListingView,
     FacultyService,
+    FacultyScheduleService,
   ],
 })
 export class FacultyModule { }
