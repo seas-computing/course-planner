00a3b5e8d8b1d2672d632c0706fbd7bd2cf4a8d3
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-05-06 11:45:29 -0400

    Move helper functions into tests/utils directory

--- tests/integration/server/faculty/facultySchedule.service.test.ts
+++ tests/integration/server/faculty/facultySchedule.service.test.ts
@@ -21,78 +21,10 @@ import { AUTH_MODE } from 'common/constants';
 import { PopulationModule } from '../../../mocks/database/population/population.module';
 import MockDB from '../../../mocks/database/MockDB';
 import { TestingStrategy } from '../../../mocks/authentication/testing.strategy';
-
-interface FacultyScheduleResponse {
-  [key: string]: FacultyResponseDTO[];
-}
-
-/**
- * Account for the way null is sorted in SQL.
- * In SQL, nulls are ordered last by default.
- * I.e., if `a` is not null (or undefined) and b is null (or undefined)
- * or if `a` is less than `b`,
- * then `a` is sorted before `b`.
- * @param a The first value
- * @param b The second value
- * @return true if `a` is sorted before `b`, false otherwise
-*/
-const sqlBefore = (a: string, b: string):
-boolean => (a !== null && b === null) || a < b;
-
-/**
- * Account for the way null is sorted in SQL.
- * In SQL, nulls are ordered last by default.
- * I.e., if `a` is null (or undefined) and b is not null (or undefined)
- * or if `a` is greater than `b`,
- * then `a` is sorted after `b`.
- * @param a The first value
- * @param b The second value
- * @return true if `a` is sorted after `b`, false otherwise
-*/
-const sqlAfter = (a: string, b: string):
-boolean => (a === null && b !== null) || a > b;
-
-/**
- * Sorts by area, then last name, and finally by first name.
- * @param result The object whose keys will be sorted
- */
-const sortResults = (result: {}): {
-  [key: string]: FacultyResponseDTO[];
-} => {
-  const sorted = {};
-  Object.keys(result).forEach((key): void => {
-    sorted[key] = result[key].slice().sort((a, b): number => {
-      if (sqlBefore(a.area, b.area)) {
-        return -1;
-      } if (sqlAfter(a.area, b.area)) {
-        return 1;
-      } if (a.lastName < b.lastName) {
-        return -1;
-      } if (a.lastName > b.lastName) {
-        return 1;
-      } if (a.firstName < b.firstName) {
-        return -1;
-      }
-      return 0;
-    });
-  });
-  return sorted;
-};
-
-/**
- * Verifies that the academic years within faculty data returned matches the
- * expected years (based on what years were requested)
- * @param result The object whose academic year values will be checked
- */
-const allDataValidYears = (result: FacultyScheduleResponse): boolean => (
-  Object.entries(result)
-    .every(([year, dtos]) => (
-      dtos.every((faculty) => (
-        faculty.fall.academicYear.toString() === year
-          && faculty.spring.academicYear.toString() === year
-      ))
-    ))
-);
+import {
+  allDataValidYears,
+  sortResults,
+} from '../../../utils/helperFunctions';
 
 describe('Faculty Schedule Service', function () {
   let testModule: TestingModule;
---
+++ tests/utils/helperFunctions.ts
@@ -0,0 +1,73 @@
+import { FacultyResponseDTO } from 'common/dto/faculty/FacultyResponse.dto';
+
+interface FacultyScheduleResponse {
+  [key: string]: FacultyResponseDTO[];
+}
+
+/**
+ * Account for the way null is sorted in SQL.
+ * In SQL, nulls are ordered last by default.
+ * I.e., if `a` is not null (or undefined) and b is null (or undefined)
+ * or if `a` is less than `b`,
+ * then `a` is sorted before `b`.
+ * @param a The first value
+ * @param b The second value
+ * @return true if `a` is sorted before `b`, false otherwise
+*/
+export const sqlBefore = (a: string, b: string):
+boolean => (a !== null && b === null) || a < b;
+
+/**
+ * Account for the way null is sorted in SQL.
+ * In SQL, nulls are ordered last by default.
+ * I.e., if `a` is null (or undefined) and b is not null (or undefined)
+ * or if `a` is greater than `b`,
+ * then `a` is sorted after `b`.
+ * @param a The first value
+ * @param b The second value
+ * @return true if `a` is sorted after `b`, false otherwise
+*/
+export const sqlAfter = (a: string, b: string):
+boolean => (a === null && b !== null) || a > b;
+
+/**
+ * Sorts by area, then last name, and finally by first name.
+ * @param result The object whose keys will be sorted
+ */
+export const sortResults = (result: {}): {
+  [key: string]: FacultyResponseDTO[];
+} => {
+  const sorted = {};
+  Object.keys(result).forEach((key): void => {
+    sorted[key] = result[key].slice().sort((a, b): number => {
+      if (sqlBefore(a.area, b.area)) {
+        return -1;
+      } if (sqlAfter(a.area, b.area)) {
+        return 1;
+      } if (a.lastName < b.lastName) {
+        return -1;
+      } if (a.lastName > b.lastName) {
+        return 1;
+      } if (a.firstName < b.firstName) {
+        return -1;
+      }
+      return 0;
+    });
+  });
+  return sorted;
+};
+
+/**
+ * Verifies that the academic years within faculty data returned matches the
+ * expected years (based on what years were requested)
+ * @param result The object whose academic year values will be checked
+ */
+export const allDataValidYears = (result: FacultyScheduleResponse): boolean => (
+  Object.entries(result)
+    .every(([year, dtos]) => (
+      dtos.every((faculty) => (
+        faculty.fall.academicYear.toString() === year
+          && faculty.spring.academicYear.toString() === year
+      ))
+    ))
+);
