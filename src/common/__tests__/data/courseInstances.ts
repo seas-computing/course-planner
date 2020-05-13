4eb6243a2b925002050f43694e296152ac603204
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-05-13 12:06:47 -0400

    Add additional test courseInstances
    I grabbed a number of real courseInstanceResponses from the dev
    database, covering a number of different cases (multiples/single
    instructors, multiple/single meetings, etc.).
    
    Currently, the ETL doesn't seem to be loading Room data into the
    database, so those values are made up -- other values are valid from my
    current db.

--- src/common/__tests__/data/courseInstances.ts
+++ src/common/__tests__/data/courseInstances.ts
@@ -1,6 +1,12 @@
 import CourseInstanceResponseDTO from 'common/dto/courses/CourseInstanceResponse';
 import { TERM_PATTERN, OFFERED, DAY } from 'common/constants';
 
+/**
+ * The data object representing the intro to computer science course CS 50 in
+ * the 2018 academic year. This course is only offered in the fall, with one
+ * instructor and one meeting. It has no notes or sameAs data.
+ */
+
 export const cs50CourseInstance: CourseInstanceResponseDTO = {
   id: 'ae6c6e2a-ffc4-4ac9-ae6d-d47b545be93e',
   title: 'Introduction to Computer Science',
@@ -13,7 +19,7 @@ export const cs50CourseInstance: CourseInstanceResponseDTO = {
   termPattern: TERM_PATTERN.FALL,
   spring: {
     id: '6cdaf745-4ab3-486b-b3fc-60ceba4ef621',
-    calendarYear: 2018,
+    calendarYear: '2018',
     offered: OFFERED.BLANK,
     preEnrollment: null,
     studyCardEnrollment: null,
@@ -31,7 +37,7 @@ export const cs50CourseInstance: CourseInstanceResponseDTO = {
   },
   fall: {
     id: '1ee96ccd-a36a-4602-8793-88e1b862add4',
-    calendarYear: 2017,
+    calendarYear: '2017',
     offered: OFFERED.Y,
     preEnrollment: null,
     studyCardEnrollment: 697,
@@ -48,12 +54,19 @@ export const cs50CourseInstance: CourseInstanceResponseDTO = {
         day: DAY.FRI,
         startTime: '10:00 AM',
         endTime: '12:00 PM',
-        room: null,
+        room: {
+          id: 'afc4ee36-b0cf-4cd7-97b7-b51b3323280d',
+          name: 'Sanders Theater',
+        },
       },
     ],
   },
 };
 
+/**
+ * Data representing ES 95, which is offered in both fall and spring, with one
+ * meeting and one instructor in each. It has no notes or sameAs data.
+ */
 
 export const es095CourseInstance: CourseInstanceResponseDTO = {
   id: '89c23511-3fcb-4436-aa2d-106f7c2aace0',
@@ -67,7 +80,7 @@ export const es095CourseInstance: CourseInstanceResponseDTO = {
   termPattern: TERM_PATTERN.BOTH,
   spring: {
     id: 'a311b26f-e033-4b41-8a74-5dcb7e390199',
-    calendarYear: 2018,
+    calendarYear: '2018',
     offered: OFFERED.Y,
     preEnrollment: null,
     studyCardEnrollment: 25,
@@ -84,13 +97,16 @@ export const es095CourseInstance: CourseInstanceResponseDTO = {
         day: DAY.TUE,
         startTime: '03:00 PM',
         endTime: '05:45 PM',
-        room: null,
+        room: {
+          id: 'bb64bef2-584c-4a40-8d3f-34137580472f',
+          name: 'Maxwell Dworkin G110',
+        },
       },
     ],
   },
   fall: {
     id: '213734a5-21d3-4476-9818-680761c19fee',
-    calendarYear: 2017,
+    calendarYear: '2017',
     offered: OFFERED.Y,
     preEnrollment: null,
     studyCardEnrollment: 39,
@@ -107,7 +123,86 @@ export const es095CourseInstance: CourseInstanceResponseDTO = {
         day: DAY.THU,
         startTime: '04:00 PM',
         endTime: '06:00 PM',
+        room: {
+          id: 'bb64bef2-584c-4a40-8d3f-34137580472f',
+          name: 'Maxwell Dworkin G110',
+        },
+      },
+    ],
+  },
+};
+
+/**
+ * Data representing the graduate intro to data science course, AC 209A, in the
+ * 2019 academic year. This course is offered in the fall, has multiple
+ * meetings and multiple instructors, and has notes and sameAs data.
+ */
+
+export const ac209aCourseInstance: CourseInstanceResponseDTO = {
+  id: '42a3fb1b-55ff-4d50-9a3f-d637f759119a',
+  title: 'Data Science 1: Introduction to Data Science',
+  area: 'ACS',
+  isUndergraduate: false,
+  catalogNumber: 'AC 209a',
+  sameAs: 'CS 109a, STATS 121a',
+  isSEAS: true,
+  notes: 'Same as CS 109a, STATS 121a',
+  termPattern: TERM_PATTERN.FALL,
+  spring: {
+    id: '62b375fa-f922-406f-bc08-7725edab27ac',
+    calendarYear: '2019',
+    offered: OFFERED.BLANK,
+    preEnrollment: null,
+    studyCardEnrollment: null,
+    actualEnrollment: null,
+    instructors: [],
+    meetings: [
+      {
+        id: null,
+        day: null,
+        startTime: null,
+        endTime: null,
         room: null,
+      },
+    ],
+  },
+  fall: {
+    id: 'b0aca4ce-e90b-47e4-97be-2187b3b7a621',
+    calendarYear: '2018',
+    offered: OFFERED.Y,
+    preEnrollment: null,
+    studyCardEnrollment: 77,
+    actualEnrollment: 66,
+    instructors: [
+      {
+        id: 'dcf88d99-1b7f-4863-93d3-d0b0bb43c8e7',
+        displayName: 'Rader, Kevin',
+      },
+      {
+        id: '78f75a40-b0bd-43af-84d8-0ec68f313dba',
+        displayName: 'Protopapas, Pavlos',
+      },
+    ],
+    meetings: [
+      {
+        id: '0a3b2708-6191-4e1a-857b-6d3352836955',
+        day: DAY.MON,
+        startTime: '01:30 PM',
+        endTime: '02:45 PM',
+        room: {
+          id: '533bc88d-3652-43c9-9936-56e280b57d6f',
+          name: 'Northwest Building B101',
+        },
+      },
+      {
+        id: '533bc88d-3652-43c9-9936-56e280b57d6e',
+        day: DAY.WED,
+        startTime: '01:30 PM',
+        endTime: '02:45 PM',
+        room: {
+          id: '533bc88d-3652-43c9-9936-56e280b57d6f',
+          name: 'Northwest Building B101',
+        },
       },
     ],
   },
