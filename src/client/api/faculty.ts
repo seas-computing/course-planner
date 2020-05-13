184a5450504025b80e50e49fd629ee209a79a701
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-05-12 23:46:03 -0400

    Include API function for Faculty Schedule table information

--- src/client/api/faculty.ts
+++ src/client/api/faculty.ts
@@ -1,11 +1,25 @@
 import request from 'axios';
 import { ManageFacultyResponseDTO } from 'common/dto/faculty/ManageFacultyResponse.dto';
+import { FacultyResponseDTO } from 'common/dto/faculty/FacultyResponse.dto';
 
 /**
- * Retrieves all faculty
+ * Retrieves all faculty for the Faculty Admin tab
  */
 export const getAllFacultyMembers = async ():
 Promise<ManageFacultyResponseDTO[]> => {
   const response = await request.get('/api/faculty/');
+  return response.data;
+};
+
+/**
+ * Retrieves faculty schedule information for the Faculty tab for specified
+ * academic year(s)
+ */
+export const getFacultySchedulesForYear = async (
+  acadYears: number
+):
+Promise<FacultyResponseDTO[]> => {
+  const response = await request
+    .get(`/api/faculty/schedule?acadYears=${acadYears}`);
   return response.data;
 };
