af46a0dca8df96f1c746d92aa4a76400fcddd3ef
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-05-15 15:50:39 -0400

    Add tests to check each Faculty category enum value
    Make sure that the Faculty category enum values map to the desired format that they should be displayed in the table

--- src/client/components/pages/Faculty/__tests__/tableFields.test.tsx
+++ src/client/components/pages/Faculty/__tests__/tableFields.test.tsx
@@ -13,9 +13,17 @@ describe('Faculty Schedule Table Fields', function () {
       });
     });
     describe('categoryEnumToTitleCase', function () {
-      it('should remove the underscore(s) and capitalize the first letter of each word', function () {
+      it('convert NON_SEAS_LADDER enum value to Non-SEAS Ladder', function () {
         const formattedCategory = categoryEnumToTitleCase('NON_SEAS_LADDER');
         strictEqual(formattedCategory, 'Non-SEAS Ladder');
+      });
+      it('convert NON_LADDER enum value to Non-Ladder', function () {
+        const formattedCategory = categoryEnumToTitleCase('NON_LADDER');
+        strictEqual(formattedCategory, 'Non-Ladder');
+      });
+      it('convert LADDER enum value to Ladder', function () {
+        const formattedCategory = categoryEnumToTitleCase('LADDER');
+        strictEqual(formattedCategory, 'Ladder');
       });
     });
   });
