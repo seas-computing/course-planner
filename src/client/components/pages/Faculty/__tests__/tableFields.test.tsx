2d3cd98ff22c03e5048d6d0396c7b34d2b1bdb8a
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-05-15 00:04:35 -0400

    Add tests for helper functions that format table field data

---
+++ src/client/components/pages/Faculty/__tests__/tableFields.test.tsx
@@ -0,0 +1,20 @@
+import { strictEqual } from 'assert';
+import {
+  absenceEnumToTitleCase,
+  categoryEnumToTitleCase,
+} from '../FacultyScheduleTable';
+
+describe('Faculty Schedule Table Fields', function () {
+  describe('helper functions', function () {
+    describe('absenceEnumToTitleCase', function () {
+      it('should remove the underscore(s) and capitalize each word', function () {
+        const formattedAbsence = absenceEnumToTitleCase('SABBATICAL_ELIGIBLE');
+        strictEqual(formattedAbsence, 'Sabbatical Eligible');
+      });
+    });
+    describe('categoryEnumToTitleCase', function () {
+      const formattedCategory = categoryEnumToTitleCase('NON_SEAS_LADDER');
+      strictEqual(formattedCategory, 'Non-SEAS Ladder');
+    });
+  });
+});
