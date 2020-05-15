0efe5e4e4efe3b2b23a35266b352b253883d4f44
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-05-15 12:47:29 -0400

    Add tests to check the output of helper functions

--- src/client/components/pages/Faculty/__tests__/tableFields.test.tsx
+++ src/client/components/pages/Faculty/__tests__/tableFields.test.tsx
@@ -7,14 +7,16 @@ import {
 describe('Faculty Schedule Table Fields', function () {
   describe('helper functions', function () {
     describe('absenceEnumToTitleCase', function () {
-      it('should remove the underscore(s) and capitalize each word', function () {
+      it('should remove the underscore(s) and capitalize the first letter of each word', function () {
         const formattedAbsence = absenceEnumToTitleCase('SABBATICAL_ELIGIBLE');
         strictEqual(formattedAbsence, 'Sabbatical Eligible');
       });
     });
     describe('categoryEnumToTitleCase', function () {
-      const formattedCategory = categoryEnumToTitleCase('NON_SEAS_LADDER');
-      strictEqual(formattedCategory, 'Non-SEAS Ladder');
+      it('should remove the underscore(s) and capitalize the first letter of each word', function () {
+        const formattedCategory = categoryEnumToTitleCase('NON_SEAS_LADDER');
+        strictEqual(formattedCategory, 'Non-SEAS Ladder');
+      });
     });
   });
 });
