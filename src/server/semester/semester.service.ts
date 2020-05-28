4f1cc0c6c8f15e11ab3e01bacf9347e445863438
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-05-28 02:51:59 -0400

    Update semester service to include getFullSemesters method
    getFullSemesters method retrieves the semester info including the id, calendarYear, academicYear, and term for the requested academic years. This orders the semesters by calendarYear and then by academicYear

--- src/server/semester/semester.service.ts
+++ src/server/semester/semester.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { TERM } from 'common/constants';
 import { Semester } from './semester.entity';
+
+/**
+ * A version of [[Semester]] with a corrected academic year
+ * and the calendar year
+ */
+export interface FullSemester {
+  id: string;
+  calendarYear: string;
+  academicYear: string;
+  term: TERM;
+}
 
 /**
  * @class SemesterService
  * Injectable service that provides additional methods for querying the
  * database for Semester data.
  */
-
 @Injectable()
 export class SemesterService {
   @InjectRepository(Semester)
   private readonly semesterRepository: Repository<Semester>
 
   /**
-   * Resolve an array containing all of the years that currently exist in the
+   * Resolves to an array containing all of the years that currently exist in the
    * database, as strings
    */
-
   public async getYearList(): Promise<string[]> {
     return this.semesterRepository
       .createQueryBuilder('sem')
@@ -30,5 +40,32 @@ export class SemesterService {
         // raw result is array of e.g. { year: '2020'} so we need to map
         (results): string[] => results.map(({ year }): string => year)
       );
+  }
+
+  /**
+   * Retrieve the list of semesters for the given academic years
+   * @param academicYears the academic years for which to find semesters
+   */
+  public async getFullSemesters(academicYears: number[]):
+  Promise<FullSemester[]> {
+    const semesters = await this.semesterRepository.createQueryBuilder('s')
+      .select('semester.id', 'id')
+      // Note that academicYear in the semester table is actually calendar year
+      // and it comes back as a string because the decimal type
+      // is translated to a string by typeorm
+      .addSelect(`CASE
+        WHEN term = '${TERM.FALL}' THEN semester."academicYear" + 1
+        ELSE semester."academicYear"
+      END`, 'academicYear')
+      .addSelect('academicYear', 'calendarYear')
+      .addSelect('semester.term', 'term')
+      .from(Semester, 'semester')
+      .where('academicYear IN (:...academicYears)', { academicYears })
+      // This will order fall semester before spring semester for an academic year
+      // since fall has an earlier calendar year
+      .orderBy('calendarYear', 'ASC')
+      .addOrderBy('academicYear', 'ASC')
+      .getRawMany() as unknown[] as FullSemester[];
+    return semesters;
   }
 }
