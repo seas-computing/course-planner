c52429c24d749fa53f490898cd99657370e39789
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-06-04 18:07:51 -0400

    Add getSemesterList method back as this is needed for metadata controller
    This should not have been deleted

--- src/server/semester/semester.service.ts
+++ src/server/semester/semester.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Semester } from './semester.entity';
+import { TERM } from 'common/constants';
 
 /**
  * @class SemesterService
@@ -27,6 +28,30 @@ export class SemesterService {
       .then(
         // raw result is array of e.g. { year: '2020'} so we need to map
         (results): string[] => results.map(({ year }): string => year)
+      );
+  }
+
+  /**
+   * Resolve an array containing all semesters that currently exist in the
+   * database, as strings
+   */
+  public async getSemesterList(): Promise<string[]> {
+    return this.semesterRepository
+      .createQueryBuilder('sem')
+      .select('sem.term', 'term')
+      .addSelect('sem."academicYear"', 'year')
+      .addSelect(`CASE
+      WHEN term = '${TERM.SPRING}' THEN 1
+      WHEN term = '${TERM.FALL}' THEN 2
+      ELSE 3
+    END`, 'termOrder')
+      .distinct(true)
+      .orderBy('year', 'ASC')
+      .addOrderBy('"termOrder"', 'ASC')
+      .getRawMany()
+      .then(
+        // raw result is array, so we need to map to get the desired format (e.g. 'FALL 2021')
+        (results): string[] => results.map(({ term, year }): string => `${term} ${year}`)
       );
   }
 }
