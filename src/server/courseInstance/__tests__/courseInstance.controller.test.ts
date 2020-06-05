8fe4c71dea36c72c99611deebf9be8a5f2a9c8a7
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-06-05 13:20:43 -0400

    Update controller test to use new static test data

--- src/server/courseInstance/__tests__/courseInstance.controller.test.ts
+++ src/server/courseInstance/__tests__/courseInstance.controller.test.ts
@@ -8,103 +8,15 @@ import { SemesterService } from 'server/semester/semester.service';
 import { Semester } from 'server/semester/semester.entity';
 import { ConfigService } from 'server/config/config.service';
 import { Course } from 'server/course/course.entity';
-import { TERM } from 'common/constants';
-import flatMap from 'lodash.flatmap';
-import { MultiYearPlanServiceResponseDTO } from 'common/dto/multiYearPlan/MultiYearPlanServiceResponseDTO';
-import { MultiYearPlanResponseDTO } from 'common/dto/multiYearPlan/MultiYearPlanResponseDTO';
+import {
+  testFourYearPlan,
+  testThreeYearPlan,
+  testMultiYearPlanStartYear,
+} from 'testData';
 import { CourseInstanceService } from '../courseInstance.service';
 import { CourseInstanceController } from '../courseInstance.controller';
 import { MultiYearPlanView } from '../MultiYearPlanView.entity';
 import { MultiYearPlanInstanceView } from '../MultiYearPlanInstanceView.entity';
-import { courses } from '../../../../tests/mocks/database/population/data';
-
-/**
- * Retrieve an array of years for the requested number of years
- */
-const getYearList = (year: number, numYears: number) => (
-  Array.from({ length: numYears }, (_, index): number => year + index)
-);
-
-const testSemesterId = '1d1957de-c199-67f1-1d03-2265bec6bbc4';
-const testCourseId = 'b353a486-3108-fdc9-470d-213a3932ba60';
-const testInstanceId = '32faf56f-f84a-1d19-3f16-e0720f24f506';
-const testFacultyId = '23573a38-14d3-9e2f-1252-121d4b5a702e';
-const testFaculty = [{
-  id: testFacultyId,
-  displayName: 'Smith, John',
-  instructorOrder: 0,
-}];
-
-/**
- * Retrieve a few instances of course test data
- */
-const testCourses = courses.slice(0, 2);
-
-/**
- * Get the fall and spring semesters for the requested academic years,
- * ordered by academic year and term (Fall, Spring)
- * @param academicYears The academic years for which semesters will be returned
- */
-const getSemesterList = (academicYears: number[]) => (
-  flatMap(academicYears,
-    (academicYear) => (
-      [TERM.FALL, TERM.SPRING].map((term) => ({
-        id: testSemesterId,
-        term,
-        academicYear: academicYear.toString(),
-        calendarYear: (term === TERM.FALL
-          ? academicYear - 1
-          : academicYear).toString(),
-      }))
-    ))
-);
-
-/**
- * Get test data in the format returned by the service
- */
-const getTestMultiYearPlanServiceResponse = (academicYears: number[]):
-MultiYearPlanServiceResponseDTO[] => {
-  const semesterList = getSemesterList(academicYears);
-  return testCourses
-    .map((course) => ({
-      id: testCourseId,
-      area: course.area,
-      catalogNumber: `${course.prefix} ${course.number}`,
-      title: course.title,
-      instances: semesterList.map((semester) => ({
-        id: testInstanceId,
-        academicYear: semester.academicYear,
-        calendarYear: semester.calendarYear,
-        term: semester.term,
-        faculty: testFaculty,
-      })),
-    }));
-};
-
-/**
- * Get test data in the format returned by the controller
- */
-const getTestMultiYearPlanResponse = (academicYears: number[]):
-MultiYearPlanResponseDTO[] => {
-  const semesterList = getSemesterList(academicYears);
-  return testCourses
-    .map((course) => ({
-      id: testCourseId,
-      area: course.area,
-      catalogNumber: `${course.prefix} ${course.number}`,
-      title: course.title,
-      semesters: semesterList.map((semester) => ({
-        id: testSemesterId,
-        academicYear: semester.academicYear,
-        calendarYear: semester.calendarYear,
-        term: semester.term,
-        instance: {
-          id: testInstanceId,
-          faculty: testFaculty,
-        },
-      })),
-    }));
-};
 
 describe('Course Instance Controller', function () {
   let ciController: CourseInstanceController;
@@ -226,96 +138,51 @@ describe('Course Instance Controller', function () {
   });
   describe('/multi-year-plan', function () {
     let getStub: SinonStub;
-    let getFullSemestersStub: SinonStub;
-    const testAcademicYear = 2020;
-
-    const defaultAcademicYearList = getYearList(testAcademicYear, 4);
-    const fiveYearAcademicYearList = getYearList(testAcademicYear, 5);
-
-    const defaultSemesterList = getSemesterList(defaultAcademicYearList);
-    const fiveYearSemesterList = getSemesterList(fiveYearAcademicYearList);
-
-    /**
-     * Test data returned in the format returned by the service
-     */
-    const defaultServiceResponse = getTestMultiYearPlanServiceResponse(
-      defaultAcademicYearList
-    );
-    const fiveYearServiceResponse = getTestMultiYearPlanServiceResponse(
-      fiveYearAcademicYearList
-    );
-
-    /**
-     * Test data returned in the format returned by the controller
-     */
-    const defaultResponse:
-    MultiYearPlanResponseDTO[] = getTestMultiYearPlanResponse(
-      defaultAcademicYearList
-    );
-    const fiveYearResponse:
-    MultiYearPlanResponseDTO[] = getTestMultiYearPlanResponse(
-      fiveYearAcademicYearList
-    );
 
     beforeEach(function () {
-      stub(configService, 'academicYear').get(() => testAcademicYear);
+      stub(configService, 'academicYear').get(() => testMultiYearPlanStartYear);
     });
     context('When number of years parameter is not set', function () {
       it('should return the data in the expected format for the default number of years', async function () {
-        getStub = stub(ciService, 'getMultiYearPlan').resolves(defaultServiceResponse);
-        getFullSemestersStub = stub(semesterService, 'getFullSemesters').resolves(defaultSemesterList);
+        getStub = stub(ciService, 'getMultiYearPlan').resolves(testFourYearPlan);
         const actual = await ciController.getMultiYearPlan();
         deepStrictEqual(getStub.args, [[undefined]]);
-        deepStrictEqual(getFullSemestersStub.args, [[defaultAcademicYearList]]);
-        deepStrictEqual(actual, defaultResponse);
+        deepStrictEqual(actual, testFourYearPlan);
       });
     });
     context('When number of years parameter is set', function () {
       it('should return the data in the expected format for the default number of years when numYears is equal to 0', async function () {
-        getStub = stub(ciService, 'getMultiYearPlan').resolves(defaultServiceResponse);
-        getFullSemestersStub = stub(semesterService, 'getFullSemesters').resolves(defaultSemesterList);
+        getStub = stub(ciService, 'getMultiYearPlan').resolves(testFourYearPlan);
         const actual = await ciController.getMultiYearPlan(0);
         deepStrictEqual(getStub.args, [[0]]);
-        deepStrictEqual(getFullSemestersStub.args, [[defaultAcademicYearList]]);
-        deepStrictEqual(actual, defaultResponse);
+        deepStrictEqual(actual, testFourYearPlan);
       });
       it('should return the data in the expected format for the default number of years when numYears is negative', async function () {
-        getStub = stub(ciService, 'getMultiYearPlan').resolves(defaultServiceResponse);
-        getFullSemestersStub = stub(semesterService, 'getFullSemesters').resolves(defaultSemesterList);
+        getStub = stub(ciService, 'getMultiYearPlan').resolves(testFourYearPlan);
         const actual = await ciController.getMultiYearPlan(-3);
         deepStrictEqual(getStub.args, [[-3]]);
-        deepStrictEqual(getFullSemestersStub.args, [[defaultAcademicYearList]]);
-        deepStrictEqual(actual, defaultResponse);
+        deepStrictEqual(actual, testFourYearPlan);
       });
       it('should return the data in the expected format for the default number of years if numYears is null', async function () {
-        getStub = stub(ciService, 'getMultiYearPlan').resolves(defaultServiceResponse);
-        getFullSemestersStub = stub(semesterService, 'getFullSemesters').resolves(defaultSemesterList);
+        getStub = stub(ciService, 'getMultiYearPlan').resolves(testFourYearPlan);
         const actual = await ciController.getMultiYearPlan(null);
         deepStrictEqual(getStub.args, [[null]]);
-        deepStrictEqual(getFullSemestersStub.args, [[defaultAcademicYearList]]);
-        deepStrictEqual(actual, defaultResponse);
+        deepStrictEqual(actual, testFourYearPlan);
       });
       it('should return the data in the expected format for the default number of years when numYears is equal to a float', async function () {
-        getStub = stub(ciService, 'getMultiYearPlan').resolves(fiveYearServiceResponse);
-        getFullSemestersStub = stub(semesterService, 'getFullSemesters').resolves(fiveYearSemesterList);
+        getStub = stub(ciService, 'getMultiYearPlan').resolves(testThreeYearPlan);
         const actual = await ciController.getMultiYearPlan(5.3);
         deepStrictEqual(getStub.args, [[5.3]]);
-        deepStrictEqual(getFullSemestersStub.args,
-          [[fiveYearAcademicYearList]]);
-        deepStrictEqual(actual, fiveYearResponse);
+        deepStrictEqual(actual, testThreeYearPlan);
       });
       it('should return the data in the expected format for the specified number of years when numYears is a positive integer', async function () {
-        getStub = stub(ciService, 'getMultiYearPlan').resolves(fiveYearServiceResponse);
-        getFullSemestersStub = stub(semesterService, 'getFullSemesters').resolves(fiveYearSemesterList);
+        getStub = stub(ciService, 'getMultiYearPlan').resolves(testThreeYearPlan);
         const actual = await ciController.getMultiYearPlan(5);
         deepStrictEqual(getStub.args, [[5]]);
-        deepStrictEqual(getFullSemestersStub.args,
-          [[fiveYearAcademicYearList]]);
-        deepStrictEqual(actual, fiveYearResponse);
+        deepStrictEqual(actual, testThreeYearPlan);
       });
       it('should return the semesters in chronological order', async function () {
-        getStub = stub(ciService, 'getMultiYearPlan').resolves(defaultServiceResponse);
-        getFullSemestersStub = stub(semesterService, 'getFullSemesters').resolves(defaultSemesterList);
+        getStub = stub(ciService, 'getMultiYearPlan').resolves(testFourYearPlan);
         const actual = await ciController.getMultiYearPlan(null);
         const sorted = actual.map((course) => ({
           ...course,
