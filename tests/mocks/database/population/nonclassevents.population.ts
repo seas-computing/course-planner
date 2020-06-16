c9ee966bf2d99ee0cda79dcadd953174aeb5e78e
Robert Main <rmain@seas.harvard.edu>
2020-06-16 14:39:57 -0400

    Populate meetings against NonClassEvents in the test DB
    This makes it easier later to write tests around the meetings data in the NonClassEvents service

--- tests/mocks/database/population/nonclassevents.population.ts
+++ tests/mocks/database/population/nonclassevents.population.ts
@@ -7,6 +7,7 @@ import { Semester } from 'server/semester/semester.entity';
 import { TERM } from 'common/constants';
 import { BasePopulationService } from './base.population';
 import { NonClassParentData, NonClassEventData } from './data';
+import { Meeting } from 'server/meeting/meeting.entity';
 
 export class NonClassEventPopulationService
   extends BasePopulationService<NonClassEvent> {
@@ -22,6 +23,9 @@ export class NonClassEventPopulationService
 
   @InjectRepository(Semester)
   protected semesterRepository: Repository<Semester>;
+
+  @InjectRepository(Meeting)
+  protected meetingRepository: Repository<Meeting>;
 
   public async populate({
     parents,
@@ -71,26 +75,34 @@ export class NonClassEventPopulationService
       },
     ]);
 
+    const meetings = await this.meetingRepository.find({
+      take: 4,
+    });
+
     return this.eventRepository.save([
       {
         ...events[0],
         nonClassParent: dataScienceParent,
         semester: fall,
+        meetings: [meetings[0]],
       },
       {
         ...events[0],
         nonClassParent: dataScienceParent,
         semester: spring,
+        meetings: [meetings[1]],
       },
       {
         ...events[1],
         nonClassParent: appliedMathParent,
         semester: fall,
+        meetings: [meetings[2]],
       },
       {
         ...events[1],
         nonClassParent: appliedMathParent,
         semester: spring,
+        meetings: [meetings[3]],
       },
     ]);
   }
