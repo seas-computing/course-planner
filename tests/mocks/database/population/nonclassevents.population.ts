3f43ea3c6e1af8b69ddb7045b0fda686b69dc145
Robert Main <rmain@seas.harvard.edu>
2020-06-16 14:39:57 -0400

    Populate meetings against NonClassEvents in the test DB
    This makes it easier later to write tests around the meetings data in the NonClassEvents service
    
    ***
    Rebased from feature/210-non-class-meeting-controller-initial-get
    
    Original Commit:
     - c9ee966bf2d99ee0cda79dcadd953174aeb5e78e
    
    Squashes:
     - 654b9140929c4024059f9450f84d7e1f304da6e5
     - 59cd58392dd27d77e974ad09014c93812a468309
    ***

--- tests/mocks/database/population/nonclassevents.population.ts
+++ tests/mocks/database/population/nonclassevents.population.ts
@@ -5,6 +5,7 @@ import { NonClassEvent } from 'server/nonClassEvent/nonclassevent.entity';
 import { Course } from 'server/course/course.entity';
 import { Semester } from 'server/semester/semester.entity';
 import { TERM } from 'common/constants';
+import { Meeting } from 'server/meeting/meeting.entity';
 import { BasePopulationService } from './base.population';
 import { NonClassParentData, NonClassEventData } from './data';
 
@@ -19,9 +20,11 @@ export class NonClassEventPopulationService
   @InjectRepository(NonClassEvent)
   protected eventRepository: Repository<NonClassEvent>;
 
-
   @InjectRepository(Semester)
   protected semesterRepository: Repository<Semester>;
+
+  @InjectRepository(Meeting)
+  protected meetingRepository: Repository<Meeting>;
 
   public async populate({
     parents,
@@ -71,26 +74,34 @@ export class NonClassEventPopulationService
       },
     ]);
 
+    const meetings = await this.meetingRepository.find({
+      take: 4,
+    });
+
     return this.eventRepository.save([
       {
         ...events[0],
         nonClassParent: dataScienceParent,
         semester: fall,
+        meetings: [meetings[0]],
       },
       {
         ...events[0],
         nonClassParent: dataScienceParent,
         semester: spring,
+        meetings: [meetings[1]],
       },
       {
         ...events[1],
         nonClassParent: appliedMathParent,
         semester: fall,
+        meetings: [meetings[2]],
       },
       {
         ...events[1],
         nonClassParent: appliedMathParent,
         semester: spring,
+        meetings: [meetings[3]],
       },
     ]);
   }
