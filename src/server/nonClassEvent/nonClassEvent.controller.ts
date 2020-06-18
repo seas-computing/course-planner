8352aa1792e1ec25674b828bf309cc8d3a031d54
Robert Main <rmain@seas.harvard.edu>
2020-06-17 23:42:46 -0400

    Controller uses current academic year if one isn't specified

--- src/server/nonClassEvent/nonClassEvent.controller.ts
+++ src/server/nonClassEvent/nonClassEvent.controller.ts
@@ -2,18 +2,29 @@ import {
   Controller, Get, Inject, Query,
 } from '@nestjs/common';
 import NonClassMeetingResponseDTO from 'common/dto/nonclassmeetings/NonClassMeeting.dto';
+import { ConfigService } from 'server/config/config.service';
 import { NonClassEventService } from './nonClassEvent.service';
 
 
 @Controller('api/non-class-events')
 export class NonClassEventController {
+  @Inject(ConfigService)
+  private config: ConfigService;
+
   @Inject(NonClassEventService)
   private service: NonClassEventService;
 
   @Get('')
   public async find(
-    @Query('acyr') acyr: number
+    @Query('acyr') acyr?: number
   ): Promise<NonClassMeetingResponseDTO[]> {
-    return this.service.find(acyr);
+    let academicYear: number;
+    if (acyr) {
+      academicYear = acyr;
+    } else {
+      ({ academicYear } = this.config);
+    }
+
+    return this.service.find(academicYear);
   }
 }
