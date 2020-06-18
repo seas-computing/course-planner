159bcbf18fc247a62d5db0571096047d55a2503d
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-06-18 11:52:57 -0400

    Export the users (getCurrentUser) method as part of an object so that we can stub them as part of the TypeScript 3.9.2+ update
    It turns out that you can no longer stub free-standing functions in Typescript 3.9.2+ according to this issue:
    
    https://github.com/microsoft/TypeScript/issues/38568
    https://github.com/sinonjs/sinon/issues/562
    
    The test in App.test.tsx was failing when it tried to get the callCount for a stub for a free-standing function getCurrentUser, and currently, my package-lock.json has 3.9.5, so that appears to be why. My fix was to use an object where the functions become methods instead.

--- src/client/api/users.ts
+++ src/client/api/users.ts
@@ -1,11 +1,16 @@
 import { UserResponse } from 'common/dto/users/userResponse.dto';
-import request from './request';
+import request, { AxiosPromise } from './request';
 
 /**
  * Get the currently authenticated user
  */
 
-export const getCurrentUser = async (): Promise<UserResponse> => {
-  const response = await request.get('/api/users/current');
-  return response.data as UserResponse;
+const getCurrentUser = (): AxiosPromise<UserResponse> => request.get('/api/users/current');
+
+/**
+ * Export the methods as part of an object so that they are stubbable.
+ * See: https://github.com/sinonjs/sinon/issues/562
+ */
+export const UserAPI = {
+  getCurrentUser,
 };
