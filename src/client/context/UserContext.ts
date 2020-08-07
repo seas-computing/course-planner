f5f16df08f1bbaaa4fd5f77fc0084359e97ee8eb
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-08-07 16:14:47 -0400

    Fix typing of Contexts
    Providing a Type to the createContext call instead of typing the
    exported context value itself to fix a weird typing/linting bug.

--- src/client/context/UserContext.ts
+++ src/client/context/UserContext.ts
@@ -1,8 +1,8 @@
-import { createContext, Context } from 'react';
+import { createContext } from 'react';
 import { UserResponse } from 'common/dto/users/userResponse.dto';
 
 /**
  * Manage the currently logged-in user throught Context
  */
 
-export const UserContext: Context<UserResponse> = createContext(null);
+export const UserContext = createContext<UserResponse>(null);
