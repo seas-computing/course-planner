688ab47add6e61a45e127a96685816210a2f8df0
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-09-01 22:44:11 -0400

    Add render test for Space component

---
+++ src/client/components/layout/__tests__/VerticalSpace.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BoundFunction,
+  GetByBoundAttribute,
+} from '@testing-library/react';
+import {
+  render,
+} from 'test-utils';
+import VerticalSpace from '../VerticalSpace';
+
+describe('VerticalSpace Component', function () {
+  let getByTestId: BoundFunction<GetByBoundAttribute>;
+  beforeEach(function () {
+    ({ getByTestId } = render(
+      <VerticalSpace data-testid="test-component" />,
+      () => {}
+    ));
+  });
+  it('creates a div', async function () {
+    getByTestId('test-component');
+  });
+});
