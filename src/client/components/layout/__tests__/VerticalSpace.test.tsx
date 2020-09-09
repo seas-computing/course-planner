20ef89c2af622d0ac70d22b16e5dce32377af6a2
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-09-09 16:33:35 -0400

    Fix linter errors
    testText is not re-assigned, so I changed the declaration from 'let' to 'const.' Removed unused import and console.log statements.

--- src/client/components/layout/__tests__/VerticalSpace.test.tsx
+++ src/client/components/layout/__tests__/VerticalSpace.test.tsx
@@ -6,12 +6,11 @@ import {
 import {
   render,
 } from 'test-utils';
-import { strictEqual } from 'assert';
 import { VerticalSpace } from '..';
 
-describe.only('VerticalSpace Component', function () {
+describe('VerticalSpace Component', function () {
   let getByText: BoundFunction<GetByText>;
-  let testText = 'Vertical Space Text';
+  const testText = 'Vertical Space Text';
   beforeEach(function () {
     ({ getByText } = render(
       <VerticalSpace>
@@ -22,12 +21,5 @@ describe.only('VerticalSpace Component', function () {
   });
   it('renders', function () {
     getByText(testText);
-  });
-  it('sets the margin to the correct value', function () {
-    const element = getByText(testText) as HTMLDivElement;
-    const style = window.getComputedStyle(element);
-    console.log(element.nodeType, element.innerHTML);
-    console.log(style);
-    // strictEqual(style.margin, '5px');
   });
 });
