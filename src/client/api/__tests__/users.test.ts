30d65ba017e08dbc51423840fa21d0ecfad44b0e
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-09-10 10:37:03 -0400

    Simplify the login-on-failure flow
    Rather than handle the redirect route as a query parameter, we can just
    save the referer header in the session (done in the
    passport-harvardkey package) and extract it from there.

---
+++ src/client/api/__tests__/users.test.ts
@@ -0,0 +1,89 @@
+import { User } from 'common/classes';
+import {
+  stub,
+  SinonStub,
+} from 'sinon';
+import * as dummy from 'testData';
+import * as api from 'client/api/users';
+import {
+  strictEqual,
+  deepStrictEqual,
+  rejects,
+} from 'assert';
+import { UnauthorizedException } from '@nestjs/common';
+import request, {
+  AxiosResponse,
+} from '../request';
+
+describe('User API', function () {
+  let result: User;
+  let getStub: SinonStub;
+  const locationReplaceStub = stub();
+  let globalWindow: Window & typeof globalThis;
+  describe('getCurrentUser', function () {
+    beforeEach(function () {
+      // window.location.replace can't be stubbed directly, so we need to
+      // manually replace then restore the function on the global object
+      globalWindow = global.window;
+      global.window = {
+        location: {
+          replace: locationReplaceStub,
+        } as unknown as Location,
+      } as unknown as Window & typeof globalThis;
+      getStub = stub(request, 'get');
+    });
+    afterEach(function () {
+      global.window = globalWindow;
+    });
+    context('when data fetch succeeds', function () {
+      beforeEach(async function () {
+        getStub.resolves({
+          data: dummy.regularUser,
+        } as AxiosResponse<User>);
+        result = await api.getCurrentUser();
+      });
+      it('should call request.get', function () {
+        strictEqual(getStub.callCount, 1);
+      });
+      it('should request /api/users/current', function () {
+        const [[path]] = getStub.args;
+        strictEqual(path, '/api/users/current');
+      });
+      it('should return the metadata', function () {
+        deepStrictEqual(result, dummy.regularUser);
+      });
+    });
+    context('when data fetch fails', function () {
+      context('With a 401 error', function () {
+        let prevURL: string;
+        const SERVER_URL = 'https://computingapps.seas.harvard.edu';
+        beforeEach(async function () {
+          getStub.rejects(new UnauthorizedException());
+          // replace and restore the SERVER_URL env var
+          prevURL = process.env.SERVER_URL;
+          process.env.SERVER_URL = SERVER_URL;
+          await api.getCurrentUser();
+        });
+        afterEach(function () {
+          process.env.SERVER_URL = prevURL;
+        });
+        it('should redirect', function () {
+          strictEqual(locationReplaceStub.callCount, 1);
+        });
+        it('Should send the user to SERVER_URL/login', function () {
+          strictEqual(locationReplaceStub.args[0][0], `${SERVER_URL}/login`);
+        });
+      });
+    });
+    context('With any other error', function () {
+      beforeEach(function () {
+        getStub.rejects(dummy.error);
+      });
+      it('should throw an error', async function () {
+        return rejects(async () => {
+          await api.getCurrentUser();
+        }, dummy.error);
+      });
+    });
+  });
+});
--- src/client/api/users.ts
+++ src/client/api/users.ts
@@ -9,13 +9,11 @@ export const getCurrentUser = async (): Promise<User> => {
   try {
     const response = await request.get('/api/users/current');
     return new User(response.data);
-  } catch ({ response }) {
-    if (
-      response.status === 401 && 'loginPath' in response.data
-    ) {
-      const currentPath = window.location.pathname;
-      const { loginPath } = response.data;
-      window.location.href = `${process.env.SERVER_URL}/${loginPath}?redirectTo=${currentPath}`;
+  } catch (err) {
+    if (/401/.test(err)) {
+      window.location.replace(`${process.env.SERVER_URL}/login`);
+    } else {
+      throw err;
     }
   }
 };
