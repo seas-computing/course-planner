c8573f3b90f3e005075edace8be83fca450d3586
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-09-14 15:44:20 -0400

    Fix UnhandledPromiseRejectionWarning
    The warning states that this is an unhandled promise rejection and that we need to check whether onClose is null or undefined before calling it in the case that there is no catch block.

--- src/client/components/pages/FacultyModal.tsx
+++ src/client/components/pages/FacultyModal.tsx
@@ -211,7 +211,7 @@ const FacultyModal: FunctionComponent<FacultyModalProps> = function ({
       setFacultyErrorMessage('');
       setFacultyModalFocus();
     }
-  }, [isVisible]);
+  }, [isVisible, currentFaculty]);
   return (
     <Modal
       ariaLabelledBy="editFaculty"
@@ -338,7 +338,9 @@ const FacultyModal: FunctionComponent<FacultyModalProps> = function ({
               // leave the modal visible after an error
               return;
             }
-            onClose();
+            if (onClose != null) {
+              onClose();
+            }
           }}
           variant={VARIANT.PRIMARY}
         >
