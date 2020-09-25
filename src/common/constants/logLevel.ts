bf384380eab52fb769681bb277817a5211bd8bec
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-09-25 16:43:49 -0400

    Add constants for default winston log levels
    The winston package has basic typescript support, but I wanted to have
    an enum to represent the log levels that we've decided to implement.
    Additionally, we're associating the levels with their string
    representations, since that's what winston.createLogger expects.

---
+++ src/common/constants/logLevel.ts
@@ -0,0 +1,15 @@
+/**
+ * Enumerates the Winston log levels as their string representations.
+ */
+
+enum LOG_LEVEL {
+  ERROR = 'error',
+  WARN = 'warn',
+  INFO = 'info',
+  HTTP = 'http',
+  VERBOSE = 'verbose',
+  DEBUG = 'debug',
+  SILLY = 'silly',
+}
+
+export default LOG_LEVEL;
