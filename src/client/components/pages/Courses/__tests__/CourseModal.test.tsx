b8e58b9639dc7f051dcf9a5e416f35a51e7eeb68
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-09-28 17:22:12 -0400

    Add tests to check the initial modal state of the Edit Course modal
    Check that when the edit course modal is opened, the fields are populated according to the current course selected and that there are no error messages

--- src/client/components/pages/Courses/__tests__/CourseModal.test.tsx
+++ src/client/components/pages/Courses/__tests__/CourseModal.test.tsx
@@ -12,7 +12,7 @@ import {
   stub,
 } from 'sinon';
 import { render } from 'test-utils';
-import { metadata } from 'testData';
+import { metadata, physicsCourseResponse } from 'testData';
 import { IS_SEAS } from 'common/constants';
 import CourseModal from '../CourseModal';
 
@@ -22,33 +22,92 @@ describe('Course Modal', function () {
     let getByLabelText: BoundFunction<GetByText>;
     const dispatchMessage: SinonStub = stub();
     describe('On Open Behavior', function () {
-      beforeEach(function () {
-        ({ getByLabelText, queryAllByRole } = render(
-          <CourseModal isVisible />,
-          dispatchMessage,
-          metadata
-        ));
+      context('when currentCourse is null', function () {
+        beforeEach(function () {
+          ({ getByLabelText, queryAllByRole } = render(
+            <CourseModal isVisible />,
+            dispatchMessage,
+            metadata
+          ));
+        });
+        it('renders empty form fields except for the "IS SEAS" dropdown, which defaults to "Yes"', function () {
+          const existingAreaSelect = document.getElementById('existingArea') as HTMLSelectElement;
+          const newAreaInput = document.getElementById('newArea') as HTMLInputElement;
+          const courseNumberInput = getByLabelText('Course Number', { exact: false }) as HTMLInputElement;
+          const courseTitleInput = getByLabelText('Course Title', { exact: false }) as HTMLInputElement;
+          const sameAsInput = getByLabelText('Same as', { exact: false }) as HTMLInputElement;
+          const undergraduateCheckbox = getByLabelText('Undergraduate', { exact: false }) as HTMLInputElement;
+          const isSEASSelect = getByLabelText('Is SEAS', { exact: false }) as HTMLSelectElement;
+          const termPatternSelect = getByLabelText('Term Pattern', { exact: false }) as HTMLSelectElement;
+          strictEqual(existingAreaSelect.value, '');
+          strictEqual(newAreaInput.value, '');
+          strictEqual(courseNumberInput.value, '');
+          strictEqual(courseTitleInput.value, '');
+          strictEqual(sameAsInput.value, '');
+          strictEqual(undergraduateCheckbox.checked, false);
+          strictEqual(isSEASSelect.value, IS_SEAS.Y);
+          strictEqual(termPatternSelect.value, '');
+        });
+        it('renders no error messages prior to initial form submission', function () {
+          strictEqual(queryAllByRole('alert').length, 0);
+        });
       });
-      it('renders empty form fields except for the "IS SEAS" dropdown, which defaults to "Yes"', function () {
-        const existingAreaSelect = document.getElementById('existingArea') as HTMLSelectElement;
-        const newAreaInput = document.getElementById('newArea') as HTMLInputElement;
-        const courseNumberInput = getByLabelText('Course Number', { exact: false }) as HTMLInputElement;
-        const courseTitleInput = getByLabelText('Course Title', { exact: false }) as HTMLInputElement;
-        const sameAsInput = getByLabelText('Same as', { exact: false }) as HTMLInputElement;
-        const undergraduateCheckbox = getByLabelText('Undergraduate', { exact: false }) as HTMLInputElement;
-        const isSEASSelect = getByLabelText('Is SEAS', { exact: false }) as HTMLSelectElement;
-        const termPatternSelect = getByLabelText('Term Pattern', { exact: false }) as HTMLSelectElement;
-        strictEqual(existingAreaSelect.value, '');
-        strictEqual(newAreaInput.value, '');
-        strictEqual(courseNumberInput.value, '');
-        strictEqual(courseTitleInput.value, '');
-        strictEqual(sameAsInput.value, '');
-        strictEqual(undergraduateCheckbox.checked, false);
-        strictEqual(isSEASSelect.value, IS_SEAS.Y);
-        strictEqual(termPatternSelect.value, '');
-      });
-      it('renders no error messages prior to initial form submission', function () {
-        strictEqual(queryAllByRole('alert').length, 0);
+      context('when currentCourse is not null', function () {
+        beforeEach(function () {
+          ({ getByLabelText, queryAllByRole } = render(
+            <CourseModal
+              isVisible
+              currentCourse={physicsCourseResponse}
+            />,
+            dispatchMessage,
+            metadata
+          ));
+        });
+        it('populates the modal fields according to the current course selected', function () {
+          const existingAreaSelect = document.getElementById('existingArea') as HTMLSelectElement;
+          const newAreaInput = document.getElementById('newArea') as HTMLInputElement;
+          const courseNumberInput = getByLabelText('Course Number', { exact: false }) as HTMLInputElement;
+          const courseTitleInput = getByLabelText('Course Title', { exact: false }) as HTMLInputElement;
+          const sameAsInput = getByLabelText('Same as', { exact: false }) as HTMLInputElement;
+          const undergraduateCheckbox = getByLabelText('Undergraduate', { exact: false }) as HTMLInputElement;
+          const isSEASSelect = getByLabelText('Is SEAS', { exact: false }) as HTMLSelectElement;
+          const termPatternSelect = getByLabelText('Term Pattern', { exact: false }) as HTMLSelectElement;
+          strictEqual(
+            existingAreaSelect.value,
+            physicsCourseResponse.area.name
+          );
+          strictEqual(
+            newAreaInput.value,
+            ''
+          );
+          strictEqual(
+            courseNumberInput.value,
+            physicsCourseResponse.catalogNumber
+          );
+          strictEqual(
+            courseTitleInput.value,
+            physicsCourseResponse.title
+          );
+          strictEqual(
+            sameAsInput.value,
+            physicsCourseResponse.sameAs
+          );
+          strictEqual(
+            undergraduateCheckbox.checked,
+            physicsCourseResponse.isUndergraduate
+          );
+          strictEqual(
+            isSEASSelect.value,
+            physicsCourseResponse.isSEAS
+          );
+          strictEqual(
+            termPatternSelect.value,
+            physicsCourseResponse.termPattern
+          );
+        });
+        it('renders no error messages prior to initial form submission', function () {
+          strictEqual(queryAllByRole('alert').length, 0);
+        });
       });
     });
   });
