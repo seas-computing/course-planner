89c7000ec6339dd2ea2a4b325230e8198a61397f
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-10-07 16:06:34 -0400

    Add global Interceptor to log request data
    The LogInterceptor will run with every request to the server and log
    information beyond what's being provided by the morgan logger. In
    particular, it will log:
    
    - The user associated with each request
    - Any data sent to the server with the request
    - Any data sent back from the server in repsonse
    
    While making use of the different log levels to provide more control
    over what's logged in different environments.

---
+++ src/server/log/log.interceptor.ts
@@ -0,0 +1,73 @@
+import {
+  Injectable,
+  Inject,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Request } from 'express';
+import { HealthCheckController } from 'server/healthCheck/healthCheck.controller';
+import { User } from 'common/classes';
+import { LogService, Inspectable } from './log.service';
+
+@Injectable()
+class LogInterceptor<T extends Inspectable> implements NestInterceptor<T> {
+  @Inject(LogService)
+  private readonly log: LogService;
+
+  /**
+   * Logs metadata about every incoming request and outgoing response.
+   * This interceptor will be applied globally in the [[LogModule]], so you
+   * should not need to manually include it anywhere.
+   */
+  public intercept(
+    context: ExecutionContext,
+    next: CallHandler
+  ): Observable<T> {
+    const controller = context.getClass();
+    //
+    // Ignore logging the healthcheck controller
+    if (controller.name === HealthCheckController.name) {
+      return next.handle() as Observable<T>;
+    }
+    const handler = context.getHandler();
+
+    // Save a reference to the controller#method for logging
+    const logLabel = `${controller.name}#${handler.name}`;
+    const http = context.switchToHttp();
+    const req = http.getRequest<Request>();
+
+    // Log details about the user
+    if (req.session?.user) {
+      const userData = new User(req.session.user);
+      this.log.verbose(userData, logLabel);
+    } else {
+      this.log.warn('Request made by anonymous user', logLabel);
+    }
+
+    // Log whether there's any payload associated with the request
+    if (Object.keys(req.body).length > 0) {
+      this.log.verbose('Request includes data', logLabel);
+      this.log.debug(req.body as Inspectable, logLabel);
+    } else if (req.method !== 'GET') {
+      this.log.verbose('Request does not include any data', logLabel);
+    }
+
+    // Log any data sent back with the response
+    return next
+      .handle()
+      .pipe(
+        tap((data: T) => {
+          this.log.verbose(`Response includes data containing ${
+            Array.isArray(data)
+              ? `${data.length} item${data.length > 1 ? 's' : ''}`
+              : '1 item'}`, logLabel);
+          this.log.debug(data, logLabel);
+        })
+      );
+  }
+}
+
+export { LogInterceptor };
--- src/server/log/log.module.ts
+++ src/server/log/log.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LogService } from './log.service';
+import { LogInterceptor } from './log.interceptor';
 
 @Module({
   providers: [
     LogService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LogInterceptor,
+    },
   ],
   exports: [
     LogService,
