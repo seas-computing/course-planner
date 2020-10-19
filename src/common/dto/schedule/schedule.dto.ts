550ff91f7a3d8d9a0f44596c56e9dd06d7537a62
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-10-19 13:50:53 -0400

    Create Schedule{Block,Entry}View
    This adds two database views for the ScheduleBlock, which represents the
    blocks of time in the schedule, and the ScheduleEntry, which represents
    the individual courses inside those blocks. The latter includes some
    additional data that will be shown on hover/click.
    
    Since we'll need a unique "key" value for each block, as well as a way
    to join these two entities, we're creating a unique id by concatenating
    the prefix, weekday, starttime, endtime, term, and year, then including
    that in both views.

--- src/common/dto/schedule/schedule.dto.ts
+++ src/common/dto/schedule/schedule.dto.ts
@@ -23,6 +23,16 @@ abstract class ScheduleEntry {
   room: string;
 
   @ApiModelProperty({
+    example: '12:30 p.m.',
+  })
+  startTime: string;
+
+  @ApiModelProperty({
+    example: '2:00 p.m.',
+  })
+  endTime: string;
+
+  @ApiModelProperty({
     example: true,
   })
   isUndergraduate: boolean;
