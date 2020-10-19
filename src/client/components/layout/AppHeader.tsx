a60bba02bdbd5b2bbb4cb05d7465c258ca3eb256
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-10-19 14:34:43 -0400

    Display Log Out only when logged in
    This adds a currentUser prop to the AppHeader, and based on its presence
    or absence will show or not show the log out link.
    
    Since the AppHeader lives just below the App component, it's easier to
    accept the currentUser as a prop, rather than fetching it from context.
    We'll need the whole user when we start changing the navigation options
    in response to the user's permissions.

--- src/client/components/layout/AppHeader.tsx
+++ src/client/components/layout/AppHeader.tsx
@@ -1,15 +1,33 @@
 import React, { FunctionComponent, ReactElement } from 'react';
 import {
-  Header, Logo, PageTitle, TabList, TabListItem, Link,
+  Header,
+  Logo,
+  PageTitle,
+  TabList,
+  TabListItem,
+  Link,
+  ExternalLink,
 } from 'mark-one';
 import { useLocation } from 'react-router-dom';
 import logo from 'client/img/seas-logo.svg';
 import { HeaderFlex } from 'client/components/general';
+import { User } from 'common/classes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
+interface AppHeaderProps {
+  /** The user currently logged into the app */
+  currentUser: User;
+}
 
-const AppHeader: FunctionComponent = (): ReactElement => {
+/**
+ * The header bar that appears at the top of every pages, along with the
+ * navigation tabs beneath it.
+ */
+
+const AppHeader: FunctionComponent<AppHeaderProps> = ({
+  currentUser,
+}: AppHeaderProps): ReactElement<AppHeaderProps> => {
   /**
    * Ordered list of the navigation tabs to display under the header.
    * TODO: Need to set up conditional display of tabs based on current user
@@ -41,13 +59,15 @@ const AppHeader: FunctionComponent = (): ReactElement => {
           <PageTitle>Course Planning</PageTitle>
         </HeaderFlex>
         <HeaderFlex>
-          <a title="Log Out" href={`${process.env.SERVER_URL}/logout`}>
-            <>
-              Log Out
-              {' '}
-              <FontAwesomeIcon icon={faSignOutAlt} />
-            </>
-          </a>
+          { currentUser && (
+            <ExternalLink title="Log Out" href={`${process.env.SERVER_URL}/logout`}>
+              <>
+                Log Out
+                {' '}
+                <FontAwesomeIcon icon={faSignOutAlt} />
+              </>
+            </ExternalLink>
+          )}
         </HeaderFlex>
       </Header>
       <nav>
