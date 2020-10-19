aea39a7a9bda86b06c6e52026df31af7d14269b1
Jonathan Seitz <jseitz@seas.harvard.edu>
2020-10-19 14:38:18 -0400

    Add tests for AppHeader
    The tests had originally been configured to stub out the `useContext`
    call to provide dummy user data, but switching to a currentUser prop
    makes this easier. Also adds test to ensure the log out link is show or
    not shown as appropriate.

--- src/client/components/layout/__tests__/AppHeader.test.tsx
+++ src/client/components/layout/__tests__/AppHeader.test.tsx
@@ -6,7 +6,6 @@ import { stub, SinonStub } from 'sinon';
 import assert from 'assert';
 import * as dummy from 'testData';
 import AppHeader from '../AppHeader';
-import { UserContext } from '../../../context';
 
 describe('AppHeader', function () {
   let contextStub: SinonStub;
@@ -14,43 +13,62 @@ describe('AppHeader', function () {
     contextStub = stub(React, 'useContext');
     contextStub.callThrough();
   });
+  context('When user is logged in', function () {
   // TODO: Setting up the test for the full permission view. Needs to be
   // duplicated/modified for other views when implemented
-  context('When user is an admin', function () {
+    context('When user is an admin', function () {
+      let queryByText: BoundFunction<QueryByText>;
+      beforeEach(function () {
+        ({ queryByText } = render(
+          <AppHeader currentUser={dummy.adminUser} />,
+          () => {}
+        ));
+      });
+      it('should display the "Schedule" link', function () {
+        const link = queryByText('Schedule');
+        assert(link);
+      });
+      it('should display the "4 Year Plan" link', function () {
+        const link = queryByText('4 Year Plan');
+        assert(link);
+      });
+      it('should display the "Courses" link', function () {
+        const link = queryByText('Courses');
+        assert(link);
+      });
+      it('should display the "Non class meetings" link', function () {
+        const link = queryByText('Non class meetings');
+        assert(link);
+      });
+      it('should display the "Faculty" link', function () {
+        const link = queryByText('Faculty');
+        assert(link);
+      });
+      it('should display the "Course Admin" link', function () {
+        const link = queryByText('Course Admin');
+        assert(link);
+      });
+      it('should display the "Faculty Admin" link', function () {
+        const link = queryByText('Faculty Admin');
+        assert(link);
+      });
+      it('Should display a log out link', function () {
+        const link = queryByText('Log Out');
+        assert(link);
+      });
+    });
+  });
+  context('When user is not logged in', function () {
     let queryByText: BoundFunction<QueryByText>;
     beforeEach(function () {
-      contextStub
-        .withArgs(UserContext)
-        .returns(dummy.adminUser);
-      ({ queryByText } = render(<AppHeader />, () => {}));
+      ({ queryByText } = render(
+        <AppHeader currentUser={null} />,
+        () => {}
+      ));
     });
-    it('should display the "Schedule" link', function () {
-      const link = queryByText('Schedule');
-      assert(link);
-    });
-    it('should display the "4 Year Plan" link', function () {
-      const link = queryByText('4 Year Plan');
-      assert(link);
-    });
-    it('should display the "Courses" link', function () {
-      const link = queryByText('Courses');
-      assert(link);
-    });
-    it('should display the "Non class meetings" link', function () {
-      const link = queryByText('Non class meetings');
-      assert(link);
-    });
-    it('should display the "Faculty" link', function () {
-      const link = queryByText('Faculty');
-      assert(link);
-    });
-    it('should display the "Course Admin" link', function () {
-      const link = queryByText('Course Admin');
-      assert(link);
-    });
-    it('should display the "Faculty Admin" link', function () {
-      const link = queryByText('Faculty Admin');
-      assert(link);
+    it('Should not display a log out link', function () {
+      const link = queryByText('Log Out');
+      assert(!link);
     });
   });
 });
