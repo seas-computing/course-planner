ebccc581595f531edc76c07eb08310c37e9e7595
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-10-21 10:20:55 -0400

    Update the sabbatical modal functionality
    Add functionality to the sabbatical modal by calling the API appropriately. Pass down the currentAbsence and fix other functionality including setting the absence form field to be the absence type instead of an empty string.

--- src/client/components/pages/Faculty/FacultySabbaticalModal.tsx
+++ src/client/components/pages/Faculty/FacultySabbaticalModal.tsx
@@ -1,7 +1,6 @@
 import React, {
   FunctionComponent,
   ReactElement,
-  useContext,
   useState,
   useRef,
   useEffect,
@@ -18,8 +17,7 @@ import {
   Dropdown,
   ValidationErrorMessage,
 } from 'mark-one';
-import { MetadataContext } from 'client/context/MetadataContext';
-import { FacultyResponseDTO } from 'common/dto/faculty/FacultyResponse.dto';
+import { FacultyAbsence, FacultyResponseDTO } from 'common/dto/faculty/FacultyResponse.dto';
 import { FacultyAPI } from 'client/api';
 import { ABSENCE_TYPE } from 'common/constants';
 import { absenceEnumToTitleCase } from 'common/utils/facultyHelperFunctions';
@@ -34,6 +32,10 @@ interface SabbaticalModalProps {
    */
   currentFaculty?: FacultyResponseDTO;
   /**
+   * The current absence entry being edited.
+   */
+  currentAbsence?: FacultyAbsence;
+  /**
    * Handler to be invoked when the modal closes
    * e.g. to clear data entered into a form
    */
@@ -41,7 +43,7 @@ interface SabbaticalModalProps {
   /**
    * Handler to be invoked when the edit is successful
    */
-  onSuccess?: (faculty: FacultyResponseDTO) => Promise<void>;
+  onSuccess?: (absence: FacultyAbsence) => Promise<void>;
 }
 
 /**
@@ -52,16 +54,12 @@ const FacultySabbaticalModal:
 FunctionComponent<SabbaticalModalProps> = function ({
   isVisible,
   currentFaculty,
+  currentAbsence,
   onClose,
   onSuccess,
 }): ReactElement {
-  /**
-   * The current value for the metadata context
-   */
-  const metadata = useContext(MetadataContext);
-
   const [form, setFormFields] = useState({
-    absence: '',
+    absence: currentAbsence.type,
   });
 
   const updateFormFields = (event: ChangeEvent): void => {
@@ -95,23 +93,23 @@ FunctionComponent<SabbaticalModalProps> = function ({
     setTimeout((): void => modalHeaderRef.current.focus());
   };
 
-  //   /**
-  //    * TODO: Submits the faculty sabbatical/leave form
-  //    */
-  //   const submitSabbaticalLeaveForm = async (): Promise<FacultyResponseDTO> => {
-  //     let result: FacultyResponseDTO;
-  //     result = await FacultyAPI.
-  //   };
-
+  /**
+   * Submits the faculty sabbatical/leave form
+   */
+  const submitSabbaticalLeaveForm = async ():
+  Promise<FacultyAbsence> => FacultyAPI.updateFacultyAbsence({
+    id: currentAbsence.id,
+    type: form.absence,
+  });
   useEffect((): void => {
     if (isVisible) {
       setFormFields({
-        absence: currentFaculty.fall
-          ? currentFaculty.fall.absence.type
-          : currentFaculty.spring.absence.type,
+        absence: currentAbsence.type,
       });
+      setSabbaticalLeaveErrorMessage('');
+      setSabbaticalLeaveModalFocus();
     }
-  }, [isVisible, currentFaculty]);
+  }, [isVisible, currentAbsence]);
   return (
     <Modal
       ariaLabelledBy="editFacultySabbaticalLeave"
@@ -156,17 +154,17 @@ FunctionComponent<SabbaticalModalProps> = function ({
         <Button
           id="editSabbaticalLeaveSubmit"
           onClick={async (): Promise<void> => {
-            // try {
-            //   const editedSabbaticalLeave = await submitSabbaticalLeaveForm();
-            //   await onSuccess(editedSabbaticalLeave);
-            // } catch (error) {
-            //   setSabbaticalLeaveErrorMessage((error as Error).message);
-            //   // leave the modal visible after an error
-            //   return;
-            // }
-            // if (onClose != null) {
-            //   onClose();
-            // }
+            try {
+              const editedSabbaticalLeave = await submitSabbaticalLeaveForm();
+              await onSuccess(editedSabbaticalLeave);
+            } catch (error) {
+              setSabbaticalLeaveErrorMessage((error as Error).message);
+              // leave the modal visible after an error
+              return;
+            }
+            if (onClose != null) {
+              onClose();
+            }
           }}
           variant={VARIANT.PRIMARY}
         >
