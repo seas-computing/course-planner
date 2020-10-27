914297a159a77ff80a5818d1f8668055746de474
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-10-27 18:32:00 -0400

    Update the FacultyScheduleTable to include functionality for the absence edit button
    Set the states for the current absence, term, and faculty, and set the modal visibility to true on click.

--- src/client/components/pages/Faculty/FacultyScheduleTable.tsx
+++ src/client/components/pages/Faculty/FacultyScheduleTable.tsx
@@ -1,6 +1,8 @@
 import React, {
   FunctionComponent,
   ReactElement,
+  useCallback,
+  useContext,
   useState,
 } from 'react';
 import {
@@ -27,7 +29,10 @@ import {
   absenceEnumToTitleCase,
   facultyTypeEnumToTitleCase,
 } from 'common/utils/facultyHelperFunctions';
-import FacultySabbaticalModal from './FacultySabbaticalModal';
+import { MessageContext } from 'client/context';
+import { AppMessage, MESSAGE_ACTION, MESSAGE_TYPE } from 'client/classes';
+import { FacultyAPI } from 'client/api';
+import FacultyAbsenceModal from './FacultyAbsenceModal';
 
 interface FacultyScheduleTableProps {
   /**
@@ -43,8 +48,8 @@ interface FacultyScheduleTableProps {
 /**
  * Computes the id of the button for the absence being edited
  */
-const computeEditAbsenceButtonId = (absence: FacultyAbsence):
-string => `editAbsence${absence.id}`;
+const computeEditAbsenceButtonId = (faculty: FacultyResponseDTO, term: TERM):
+string => `editAbsence${faculty.id}${term}`;
 
 /**
  * Component representing the Faculty Schedules for a given academic year
@@ -53,9 +58,53 @@ const FacultyScheduleTable: FunctionComponent<FacultyScheduleTableProps> = ({
   academicYear,
   facultySchedules,
 }): ReactElement => {
-  const [currentAbsence, setAbsence] = useState(null);
-  const [currentFaculty, setFaculty] = useState(null);
+  /**
+   * The current list of schedules used to populate the Faculty Schedule table
+   */
+  const [currentFacultySchedules, setFacultySchedules] = useState(
+    facultySchedules
+  );
+
+  /**
+   * The currently selected faculty
+   */
+  const [currentFaculty, setFaculty] = useState(null as FacultyResponseDTO);
+
+  /**
+   * The current academic term
+   */
+  const [currentTerm, setTerm] = useState(null as TERM);
+
+  /**
+   * The currently selected absence
+   */
+  const [currentAbsence, setAbsence] = useState(null as FacultyAbsence);
+
+  /**
+   * Keeps track of whether the absence modal is currently visible.
+   * By default, the modal is not visible.
+   */
   const [absenceModalVisible, setAbsenceModalVisible] = useState(false);
+
+  /**
+   * The current value for the message context
+   */
+  const dispatchMessage = useContext(MessageContext);
+
+  const loadSchedules = useCallback(async (): Promise<void> => {
+    try {
+      setFacultySchedules(await FacultyAPI
+        .getFacultySchedulesForYear(academicYear));
+    } catch (e) {
+      dispatchMessage({
+        message: new AppMessage(
+          'Unable to get faculty schedule data from server. If the problem persists, contact SEAS Computing',
+          MESSAGE_TYPE.ERROR
+        ),
+        type: MESSAGE_ACTION.PUSH,
+      });
+    }
+  }, [dispatchMessage, academicYear]);
   return (
     <>
       <Table>
@@ -100,7 +149,7 @@ const FacultyScheduleTable: FunctionComponent<FacultyScheduleTableProps> = ({
           </TableRow>
         </TableHead>
         <TableBody isScrollable>
-          {facultySchedules && facultySchedules
+          {currentFacultySchedules && currentFacultySchedules
             .map((faculty, facultyIndex): ReactElement<TableRowProps> => (
               <TableRow isStriped={facultyIndex % 2 === 1} key={faculty.id}>
                 <TableCell
@@ -123,11 +172,13 @@ const FacultyScheduleTable: FunctionComponent<FacultyScheduleTableProps> = ({
                   )}
                   <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
                     <BorderlessButton
-                      id={computeEditAbsenceButtonId(faculty.fall.absence)}
+                      id={computeEditAbsenceButtonId(faculty, TERM.FALL)}
                       variant={VARIANT.INFO}
                       onClick={
                         (): void => {
+                          setAbsenceModalVisible(true);
                           setFaculty(faculty);
+                          setTerm(TERM.FALL);
                           setAbsence(faculty.fall.absence);
                         }
                       }
@@ -151,11 +202,13 @@ const FacultyScheduleTable: FunctionComponent<FacultyScheduleTableProps> = ({
                   )}
                   <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
                     <BorderlessButton
-                      id={computeEditAbsenceButtonId(faculty.spring.absence)}
+                      id={computeEditAbsenceButtonId(faculty, TERM.SPRING)}
                       variant={VARIANT.INFO}
                       onClick={
                         (): void => {
+                          setAbsenceModalVisible(true);
                           setFaculty(faculty);
+                          setTerm(TERM.SPRING);
                           setAbsence(faculty.spring.absence);
                         }
                       }
@@ -183,24 +236,31 @@ const FacultyScheduleTable: FunctionComponent<FacultyScheduleTableProps> = ({
             ))}
         </TableBody>
       </Table>
-      <FacultySabbaticalModal
-        isVisible={absenceModalVisible}
-        currentFaculty={currentFaculty}
-        currentAbsence={currentAbsence}
-        onClose={(): void => {
-          setAbsenceModalVisible(false);
-          const buttonId = computeEditAbsenceButtonId(currentAbsence);
-          const button = document.getElementById(buttonId);
-          // this will run after the data is loaded, so no delay is necessary
-          window.setTimeout((): void => {
-            button.focus();
-          }, 0);
-        }}
-        onSuccess={async (): Promise<void> => {
-          // wait for the table to load before allowing the dialog to close
-          await ;
-        }}
-      />
+      {(currentFaculty && currentAbsence)
+        ? (
+          <FacultyAbsenceModal
+            isVisible={absenceModalVisible}
+            currentFaculty={currentFaculty}
+            currentAbsence={currentAbsence}
+            onClose={(): void => {
+              setAbsenceModalVisible(false);
+              const buttonId = computeEditAbsenceButtonId(
+                currentFaculty,
+                currentTerm
+              );
+              const button = document.getElementById(buttonId);
+              // this will run after the data is loaded, so no delay is necessary
+              window.setTimeout((): void => {
+                button.focus();
+              }, 0);
+            }}
+            onSuccess={async (): Promise<void> => {
+              // wait for the table to load before allowing the dialog to close
+              await loadSchedules();
+            }}
+          />
+        )
+        : (<></>)}
     </>
   );
 };
