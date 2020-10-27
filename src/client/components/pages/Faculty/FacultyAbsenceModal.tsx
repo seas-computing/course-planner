b8f20d519f8a0a6ffe121d9e4f1261494e1dbb0b
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2020-10-27 18:37:48 -0400

    Add the FacultyAbsenceModal along with tests to check its behavior
    The FacultyAbsenceModal includes a dropdown that the user can use to change the absence type for the faculty for that term. It also includes an area for a general error message. On modal open, it sets the focus to the modal's header. The tests check the open behavior and submit behavior to make sure that the dropdown gets populated appropriately and that onSuccess/onClose gets called appropriately.

---
+++ src/client/components/pages/Faculty/FacultyAbsenceModal.tsx
@@ -0,0 +1,190 @@
+import React, {
+  FunctionComponent,
+  ReactElement,
+  useState,
+  useRef,
+  useEffect,
+  ChangeEvent,
+  Ref,
+} from 'react';
+import {
+  VARIANT,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Button,
+  Dropdown,
+  ValidationErrorMessage,
+} from 'mark-one';
+import { FacultyAbsence, FacultyResponseDTO } from 'common/dto/faculty/FacultyResponse.dto';
+import { FacultyAPI } from 'client/api';
+import { ABSENCE_TYPE } from 'common/constants';
+import { absenceEnumToTitleCase } from 'common/utils/facultyHelperFunctions';
+import { AbsenceResponseDTO } from 'common/dto/faculty/AbsenceResponse.dto';
+
+interface AbsenceModalProps {
+  /**
+   * Whether or not the modal should be visible on the page.
+   */
+  isVisible: boolean;
+  /**
+   * The current faculty entry being edited.
+   */
+  currentFaculty: FacultyResponseDTO;
+  /**
+   * The current absence entry being edited.
+   */
+  currentAbsence: FacultyAbsence;
+  /**
+   * Handler to be invoked when the modal closes
+   * e.g. to clear data entered into a form
+   */
+  onClose?: () => void;
+  /**
+   * Handler to be invoked when the edit is successful
+   */
+  onSuccess?: (absence: FacultyAbsence) => Promise<void>;
+}
+
+/**
+ * This component represents the Faculty Sabbatical/Leave modal, which appears
+ * on the Faculty tab
+ */
+const FacultyAbsenceModal:
+FunctionComponent<AbsenceModalProps> = function ({
+  isVisible,
+  currentFaculty,
+  currentAbsence,
+  onClose,
+  onSuccess,
+}): ReactElement {
+  const [form, setFormFields] = useState({
+    absence: '',
+  });
+
+  const updateFormFields = (event: ChangeEvent): void => {
+    const target = event.target as HTMLSelectElement;
+    setFormFields({
+      ...form,
+      [target.name]:
+      target.value,
+    });
+  };
+
+  /**
+   * The current value of the error message within the sabbatical/leave modal
+   */
+  const [
+    absenceErrorMessage,
+    setAbsenceErrorMessage,
+  ] = useState('');
+
+  /**
+   * The current value of the Sabbatical/Leave Modal ref
+   */
+  const modalHeaderRef: Ref<HTMLHeadingElement> = useRef(null);
+
+  /**
+   * Set the ref focus.
+   * Since modal may not have been rendered in DOM, wait for it to be
+   * rendered by letting next task of event queue run first.
+   */
+  const setAbsenceModalFocus = (): void => {
+    setTimeout((): void => modalHeaderRef.current.focus());
+  };
+
+  /**
+   * Submits the faculty sabbatical/leave form
+   */
+  const submitAbsenceForm = async ():
+  Promise<AbsenceResponseDTO> => {
+    const updatedAbsenceInfo = {
+      id: currentAbsence.id,
+      type: form.absence as ABSENCE_TYPE,
+    };
+    const result: FacultyAbsence = await FacultyAPI
+      .updateFacultyAbsence(updatedAbsenceInfo);
+    return result;
+  };
+  useEffect((): void => {
+    if (isVisible) {
+      setFormFields({
+        absence: currentAbsence.type,
+      });
+      setAbsenceErrorMessage('');
+      setAbsenceModalFocus();
+    }
+  }, [isVisible, currentAbsence]);
+  return (
+    <Modal
+      ariaLabelledBy="editFacultySabbaticalLeave"
+      closeHandler={onClose}
+      isVisible={isVisible}
+    >
+      <ModalHeader
+        forwardRef={modalHeaderRef}
+        tabIndex={0}
+      >
+        {`Sabbatical/Leave for ${currentFaculty.firstName} ${currentFaculty.lastName}`}
+      </ModalHeader>
+      <ModalBody>
+        <form id="editAbsenceForm">
+          <Dropdown
+            id="sabbaticalLeave"
+            name="absence"
+            label="Sabbatical/Leave"
+            isLabelVisible={false}
+            options={(Object.values(ABSENCE_TYPE)
+              .map((absence):
+              {value: string;label: string} => {
+                const absenceTitle = absenceEnumToTitleCase(absence);
+                return {
+                  value: absence,
+                  label: absenceTitle,
+                };
+              }))}
+            onChange={updateFormFields}
+            value={form.absence}
+          />
+          {absenceErrorMessage && (
+            <ValidationErrorMessage
+              id="editAbsenceFormErrorMessage"
+            >
+              {absenceErrorMessage}
+            </ValidationErrorMessage>
+          )}
+        </form>
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          id="editSabbaticalLeaveSubmit"
+          onClick={async (): Promise<void> => {
+            try {
+              const editedSabbaticalLeave = await submitAbsenceForm();
+              await onSuccess(editedSabbaticalLeave);
+            } catch (error) {
+              setAbsenceErrorMessage((error as Error).message);
+              // leave the modal visible after an error
+              return;
+            }
+            if (onClose != null) {
+              onClose();
+            }
+          }}
+          variant={VARIANT.PRIMARY}
+        >
+          Submit
+        </Button>
+        <Button
+          onClick={onClose}
+          variant={VARIANT.SECONDARY}
+        >
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default FacultyAbsenceModal;
---
+++ src/client/components/pages/Faculty/__tests__/FacultyAbsenceModal.test.tsx
@@ -0,0 +1,93 @@
+import {
+  strictEqual,
+} from 'assert';
+import React from 'react';
+import {
+  SinonStub,
+  stub,
+} from 'sinon';
+import {
+  AllByRole,
+  BoundFunction,
+  fireEvent,
+  GetByText,
+  render,
+  wait,
+} from 'test-utils';
+import request from 'client/api/request';
+import {
+  appliedMathFacultyScheduleResponse,
+  facultyAbsence,
+  metadata,
+} from 'testData';
+import FacultyAbsenceModal from '../FacultyAbsenceModal';
+
+describe('Faculty Absence Modal', function () {
+  let queryAllByRole: BoundFunction<AllByRole>;
+  let getByLabelText: BoundFunction<GetByText>;
+  let getByText: BoundFunction<GetByText>;
+  const dispatchMessage: SinonStub = stub();
+  let onSuccessStub: SinonStub;
+  let onCloseStub: SinonStub;
+  let putStub: SinonStub;
+  describe('On Open Behavior', function () {
+    beforeEach(function () {
+      ({ getByLabelText, queryAllByRole } = render(
+        <FacultyAbsenceModal
+          isVisible
+          currentFaculty={appliedMathFacultyScheduleResponse}
+          currentAbsence={facultyAbsence}
+        />,
+        dispatchMessage,
+        metadata
+      ));
+    });
+    it('populates the absence field according to the current absence selected', function () {
+      const absenceSelect = getByLabelText('Sabbatical/Leave', { exact: false }) as HTMLSelectElement;
+      strictEqual(
+        absenceSelect.value,
+        facultyAbsence.type
+      );
+    });
+    it('renders no error messages prior to initial form submission', function () {
+      strictEqual(queryAllByRole('alert').length, 0);
+    });
+  });
+  describe('Submit Behavior', function () {
+    beforeEach(function () {
+      putStub = stub(request, 'put');
+      putStub.resolves({ data: facultyAbsence });
+      onSuccessStub = stub();
+      onCloseStub = stub();
+      ({ getByLabelText, getByText } = render(
+        <FacultyAbsenceModal
+          isVisible
+          currentFaculty={appliedMathFacultyScheduleResponse}
+          currentAbsence={facultyAbsence}
+          onSuccess={onSuccessStub}
+          onClose={onCloseStub}
+        />,
+        dispatchMessage,
+        metadata
+      ));
+    });
+    it('calls the onSuccess handler once on submit', async function () {
+      const submitButton = getByText('Submit');
+      fireEvent.click(submitButton);
+      await wait(() => strictEqual(onSuccessStub.callCount, 1));
+    });
+    it('calls the onSuccess handler with the provided arguments', async function () {
+      const submitButton = getByText('Submit');
+      fireEvent.click(submitButton);
+      await wait(() => strictEqual(
+        onSuccessStub.args[0][0],
+        facultyAbsence
+      ));
+    });
+    it('calls the onClose handler once', async function () {
+      const submitButton = getByText('Submit');
+      fireEvent.click(submitButton);
+      await wait(() => strictEqual(onCloseStub.callCount, 1));
+    });
+  });
+});
