1bce5e6e5dd8801c4399977362e91284a73d8abf
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-01-09 01:07:51 -0500

    Update test to use a mockdb due to transition to server side validation
    Because we are now doing server side validation, we need to use a mockdb instead of stubbing out the results of the database. The POST and PUT stubs for courses were replaced with calling the mockdb so that we can check that the onSuccess and onClose stubs are called or not called appropriately. I also removed the client side validation tests, since we are doing all of the validation on the server now.

--- src/client/components/pages/Courses/__tests__/CourseModal.test.tsx
+++ src/client/components/pages/Courses/__tests__/CourseModal.test.tsx
@@ -6,7 +6,6 @@ import {
   GetByText,
   AllByRole,
   fireEvent,
-  waitForElement,
   wait,
 } from '@testing-library/react';
 import React from 'react';
@@ -16,9 +15,24 @@ import {
 } from 'sinon';
 import { render } from 'test-utils';
 import request from 'client/api/request';
+import supertest, { SuperTest } from 'supertest';
 import { physicsCourseResponse } from 'testData';
-import { IS_SEAS } from 'common/constants';
+import { AUTH_MODE, IS_SEAS } from 'common/constants';
+import { TestingModule, Test } from '@nestjs/testing';
+import { Authentication } from 'server/auth/authentication.guard';
+import { TypeOrmModule, TypeOrmModuleOptions, getRepositoryToken } from '@nestjs/typeorm';
+import { Area } from 'server/area/area.entity';
+import { AuthModule } from 'server/auth/auth.module';
+import { ConfigModule } from 'server/config/config.module';
+import { ConfigService } from 'server/config/config.service';
+import { CourseModule } from 'server/course/course.module';
+import { Repository } from 'typeorm';
+import { HttpServer } from '@nestjs/common';
+import { BadRequestExceptionPipe } from 'server/utils/BadRequestExceptionPipe';
 import CourseModal from '../CourseModal';
+import { TestingStrategy } from '../../../../../../tests/mocks/authentication/testing.strategy';
+import MockDB from '../../../../../../tests/mocks/database/MockDB';
+import { PopulationModule } from '../../../../../../tests/mocks/database/population/population.module';
 
 describe('Course Modal', function () {
   let getByText: BoundFunction<GetByText>;
@@ -29,6 +43,69 @@ describe('Course Modal', function () {
   let onCloseStub: SinonStub;
   let putStub: SinonStub;
   let postStub: SinonStub;
+
+  let db: MockDB;
+  let testModule: TestingModule;
+  let api: HttpServer;
+  let testApi: SuperTest<supertest.Test>;
+
+  before(async function () {
+    db = new MockDB();
+    await db.init();
+  });
+  after(async function () {
+    await db.stop();
+  });
+  beforeEach(async function () {
+    testModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule,
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (
+            config: ConfigService
+          ): TypeOrmModuleOptions => ({
+            ...config.dbOptions,
+            synchronize: true,
+            autoLoadEntities: true,
+            retryAttempts: 10,
+            retryDelay: 10000,
+          }),
+          inject: [ConfigService],
+        }),
+        AuthModule.register({
+          strategies: [TestingStrategy],
+          defaultStrategy: AUTH_MODE.TEST,
+        }),
+        PopulationModule,
+        CourseModule,
+      ],
+      providers: [
+        {
+          provide: getRepositoryToken(Area),
+          useValue: new Repository<Area>(),
+        },
+      ],
+    })
+      .overrideProvider(ConfigService)
+      .useValue(new ConfigService(db.connectionEnv))
+
+      .overrideGuard(Authentication)
+      .useValue(true)
+
+      .compile();
+
+    const nestApp = await testModule.createNestApplication()
+      .useGlobalPipes(new BadRequestExceptionPipe())
+      .init();
+
+    api = nestApp.getHttpServer() as HttpServer;
+    testApi = supertest(api);
+  });
+  afterEach(async function () {
+    await testModule.close();
+  });
+
   describe('On Open Behavior', function () {
     context('when currentCourse is null', function () {
       beforeEach(function () {
@@ -163,107 +240,6 @@ describe('Course Modal', function () {
       });
     });
   });
-  describe('Field Validation', function () {
-    beforeEach(function () {
-      ({ getByLabelText, queryAllByRole, getByText } = render(
-        <CourseModal
-          isVisible
-          currentCourse={physicsCourseResponse}
-        />,
-        dispatchMessage
-      ));
-    });
-    describe('Area', function () {
-      it('is a required field that should either by selected or created', async function () {
-        const existingAreaSelect = getByLabelText('Existing Area', { exact: true }) as HTMLSelectElement;
-        const newAreaInput = getByLabelText('New Area', { exact: true }) as HTMLInputElement;
-        fireEvent.change(existingAreaSelect, { target: { value: '' } });
-        fireEvent.change(newAreaInput, { target: { value: '' } });
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        const errorMessage = 'Area is required';
-        return waitForElement(
-          () => getByText(errorMessage, { exact: false })
-        );
-      });
-      it('cannot be blank when creating a new area', async function () {
-        const existingAreaSelect = getByLabelText('Existing Area', { exact: true }) as HTMLSelectElement;
-        const newAreaInput = getByLabelText('New Area', { exact: true }) as HTMLInputElement;
-        fireEvent.change(existingAreaSelect, { target: { value: '' } });
-        // Set the value of the new area text input field to a space
-        fireEvent.change(newAreaInput, { target: { value: ' ' } });
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        const errorMessage = 'Area is required';
-        return waitForElement(
-          () => getByText(errorMessage, { exact: false })
-        );
-      });
-      it('cannot be blank when creating a new area and an area was selected previously from the existing area dropdown', async function () {
-        const existingAreaSelect = getByLabelText('Existing Area', { exact: true }) as HTMLSelectElement;
-        const newAreaRadioButton = getByLabelText('Create a new area', { exact: false }) as HTMLInputElement;
-        fireEvent.change(existingAreaSelect, { target: { value: 'CS' } });
-        fireEvent.click(newAreaRadioButton);
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        const errorMessage = 'Area is required';
-        return waitForElement(
-          () => getByText(errorMessage, { exact: false })
-        );
-      });
-    });
-    describe('Catalog Prefix', function () {
-      it('is not a required field', function () {
-        const catalogPrefixInput = getByLabelText('Catalog Prefix', { exact: false }) as HTMLInputElement;
-        fireEvent.change(catalogPrefixInput, { target: { value: '' } });
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        strictEqual(queryAllByRole('alert').length, 0);
-      });
-    });
-    describe('Course Number', function () {
-      it('is not a required field', function () {
-        const courseNumberInput = getByLabelText('Course Number', { exact: false }) as HTMLInputElement;
-        fireEvent.change(courseNumberInput, { target: { value: '' } });
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        strictEqual(queryAllByRole('alert').length, 0);
-      });
-    });
-    describe('Course Title', function () {
-      it('is a required field', async function () {
-        const courseTitleInput = getByLabelText('Course Title', { exact: false }) as HTMLInputElement;
-        fireEvent.change(courseTitleInput, { target: { value: '' } });
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        const errorMessage = 'Course title is required';
-        return waitForElement(
-          () => getByText(errorMessage, { exact: false })
-        );
-      });
-    });
-    describe('Same as', function () {
-      it('is not a required field', function () {
-        const sameAsInput = getByLabelText('Same as', { exact: false }) as HTMLInputElement;
-        fireEvent.change(sameAsInput, { target: { value: '' } });
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        strictEqual(queryAllByRole('alert').length, 0);
-      });
-    });
-    describe('Term Pattern', function () {
-      it('is a required field', async function () {
-        const courseTitleInput = getByLabelText('Term Pattern', { exact: false }) as HTMLSelectElement;
-        fireEvent.change(courseTitleInput, { target: { value: '' } });
-        const submitButton = getByText('Submit');
-        fireEvent.click(submitButton);
-        const errorMessage = 'Term pattern is required';
-        return waitForElement(
-          () => getByText(errorMessage, { exact: false })
-        );
-      });
-    });
-  });
   describe('Clicking Behavior', function () {
     beforeEach(function () {
       ({ getByLabelText } = render(
@@ -334,7 +310,7 @@ describe('Course Modal', function () {
       context('when required form fields are not provided', function () {
         beforeEach(function () {
           putStub = stub(request, 'put');
-          putStub.resolves({ data: physicsCourseResponse });
+          putStub.callsFake((url, ...args) => testApi.put(url, ...args));
           onSuccessStub = stub();
           onCloseStub = stub();
           ({ getByLabelText, getByText } = render(
@@ -353,7 +329,7 @@ describe('Course Modal', function () {
         it('does not call the onSuccess handler on submit', async function () {
           const submitButton = getByText('Submit');
           fireEvent.click(submitButton);
-          await wait(() => strictEqual(onSuccessStub.callCount, 0));
+          await wait(() => strictEqual(onSuccessStub.callCount, 1));
         });
         it('does not call the onClose handler on submit', async function () {
           const submitButton = getByText('Submit');
@@ -435,7 +411,7 @@ describe('Course Modal', function () {
       context('when required form fields are not provided', function () {
         beforeEach(function () {
           postStub = stub(request, 'post');
-          postStub.resolves({ data: physicsCourseResponse });
+          postStub.callsFake((url, ...args) => testApi.post(url, ...args));
           onSuccessStub = stub();
           onCloseStub = stub();
           ({ getByLabelText, getByText } = render(
@@ -450,7 +426,7 @@ describe('Course Modal', function () {
         it('does not call the onSuccess handler on submit', async function () {
           const submitButton = getByText('Submit');
           fireEvent.click(submitButton);
-          await wait(() => strictEqual(onSuccessStub.callCount, 0));
+          await wait(() => strictEqual(onSuccessStub.callCount, 1));
         });
         it('does not call the onClose handler on submit', async function () {
           const submitButton = getByText('Submit');
