716690d8298abc13e048cc3607a9efadf57941a5
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-01-11 11:23:54 -0500

    Add test case to check the behavior of trimIfString when there is no surrounding whitespace

--- src/common/dto/__tests__/util.test.tsx
+++ src/common/dto/__tests__/util.test.tsx
@@ -1,12 +1,24 @@
 import { strictEqual } from 'assert';
+import { computerScienceCourseResponse, physicsCourseResponse } from 'testData';
 import { trimIfString } from '../util';
 
 describe('DTO Helper Functions', function () {
   describe('trimIfString', function () {
-    it('should remove the whitespace surrounding a string', function () {
-      const stringWithWhitespace = '  CS 050 ';
-      const trimmedString = stringWithWhitespace.trim();
-      strictEqual(trimIfString(stringWithWhitespace), trimmedString);
+    context('when there is whitespace surrounding a string', function () {
+      it('should remove the surrounding whitespace', function () {
+        const stringWithWhitespace = `  ${computerScienceCourseResponse.catalogNumber} `;
+        const trimmedString = stringWithWhitespace.trim();
+        strictEqual(trimIfString(stringWithWhitespace), trimmedString);
+      });
+    });
+    context('when there is no whitespace surrounding the string', function () {
+      it('should return the original string', function () {
+        const stringWithoutWhitespace = physicsCourseResponse.catalogNumber;
+        strictEqual(
+          trimIfString(stringWithoutWhitespace),
+          physicsCourseResponse.catalogNumber
+        );
+      });
     });
   });
 });
