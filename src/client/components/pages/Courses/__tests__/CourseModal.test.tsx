7299da882285da43eb55b8bd237efd308ffd30d8
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-01-12 21:09:18 -0500

    Remove mockdb setup from unit test and stub the result of putStub and postStub
    Since this is a unit test, we are stubbing the result of tests instead of making requests to the server. The mockdb will exist in the integration tests.

--- src/client/components/pages/Courses/__tests__/CourseModal.test.tsx
+++ src/client/components/pages/Courses/__tests__/CourseModal.test.tsx
@@ -7,8 +7,6 @@ import {
   AllByRole,
   fireEvent,
   wait,
-  FindByText,
-  QueryByText,
 } from '@testing-library/react';
 import React from 'react';
 import {
@@ -16,47 +14,15 @@ import {
   stub,
 } from 'sinon';
 import { render } from 'test-utils';
-import supertest from 'supertest';
 import {
-  adminUser,
   physicsCourseResponse,
-  string,
 } from 'testData';
-import { AUTH_MODE, IS_SEAS } from 'common/constants';
-import { TestingModule, Test } from '@nestjs/testing';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { AuthModule } from 'server/auth/auth.module';
-import { ConfigModule } from 'server/config/config.module';
-import { ConfigService } from 'server/config/config.service';
-import { CourseModule } from 'server/course/course.module';
-import { HttpServer } from '@nestjs/common';
-import { BadRequestExceptionPipe } from 'server/utils/BadRequestExceptionPipe';
+import { IS_SEAS } from 'common/constants';
 import request from 'client/api/request';
-import { SessionModule } from 'nestjs-session';
 import CourseModal from '../CourseModal';
-import { TestingStrategy } from '../../../../../../tests/mocks/authentication/testing.strategy';
-import MockDB from '../../../../../../tests/mocks/database/MockDB';
-
-/**
- * This class takes a supertest response with an error
- * and reshapes it into error with the same structure as AxiosError.
- */
-class AxiosSupertestError extends Error {
-  public response: supertest.Response;
-
-  constructor(response) {
-    super(response.error.message);
-    this.response = {
-      ...response,
-      data: response.body,
-    };
-  }
-}
 
 describe('Course Modal', function () {
   let getByText: BoundFunction<GetByText>;
-  let findByText: BoundFunction<FindByText>;
-  let queryByText: BoundFunction<QueryByText>;
   let queryAllByRole: BoundFunction<AllByRole>;
   let getByLabelText: BoundFunction<GetByText>;
   const dispatchMessage: SinonStub = stub();
@@ -64,69 +30,6 @@ describe('Course Modal', function () {
   let onCloseStub: SinonStub;
   let putStub: SinonStub;
   let postStub: SinonStub;
-  let authStub: SinonStub;
-
-  let db: MockDB;
-  let testModule: TestingModule;
-  let api: HttpServer;
-  let supertestedApi: supertest.SuperTest<supertest.Test>;
-
-  before(async function () {
-    db = new MockDB();
-    await db.init();
-  });
-  after(async function () {
-    await db.stop();
-  });
-  beforeEach(async function () {
-    authStub = stub(TestingStrategy.prototype, 'login');
-    authStub.resolves(adminUser);
-    testModule = await Test.createTestingModule({
-      imports: [
-        SessionModule.forRoot({
-          session: {
-            secret: string,
-            resave: true,
-            saveUninitialized: true,
-          },
-        }),
-        ConfigModule,
-        TypeOrmModule.forRootAsync({
-          imports: [ConfigModule],
-          useFactory: (
-            config: ConfigService
-          ): TypeOrmModuleOptions => ({
-            ...config.dbOptions,
-            synchronize: true,
-            autoLoadEntities: true,
-            retryAttempts: 10,
-            retryDelay: 10000,
-          }),
-          inject: [ConfigService],
-        }),
-        AuthModule.register({
-          strategies: [TestingStrategy],
-          defaultStrategy: AUTH_MODE.TEST,
-        }),
-        CourseModule,
-      ],
-    })
-      .overrideProvider(ConfigService)
-      .useValue(new ConfigService(db.connectionEnv))
-
-      .compile();
-
-    const nestApp = await testModule.createNestApplication()
-      .useGlobalPipes(new BadRequestExceptionPipe())
-      .init();
-
-    api = nestApp.getHttpServer() as HttpServer;
-    supertestedApi = supertest(api);
-  });
-  afterEach(async function () {
-    await testModule.close();
-  });
-
   describe('On Open Behavior', function () {
     context('when currentCourse is null', function () {
       beforeEach(function () {
@@ -330,19 +233,9 @@ describe('Course Modal', function () {
       });
       context('when required form fields are not provided', function () {
         beforeEach(function () {
-          putStub = putStub.callsFake(async (url, data) => {
-            const result = await supertestedApi.put(url)
-              .send(data);
-            // An error is not thrown for an error HTTP status,
-            // so we must throw it ourselves.
-            if (result.error) {
-              throw new AxiosSupertestError(result);
-            }
-            return {
-              ...result,
-              data: result.body,
-            };
-          });
+          const errorMessage = 'Title should not be empty';
+          putStub = stub(request, 'put');
+          putStub.rejects(new Error(errorMessage));
           onSuccessStub = stub();
           onCloseStub = stub();
           ({ getByLabelText, getByText } = render(
@@ -444,22 +337,11 @@ describe('Course Modal', function () {
       });
       context('when required form fields are not provided', function () {
         beforeEach(function () {
-          postStub = postStub.callsFake(async (url, data) => {
-            const result = await supertestedApi.post(url)
-              .send(data);
-            // An error is not thrown for an error HTTP status,
-            // so we must throw it ourselves.
-            if (result.error) {
-              throw new AxiosSupertestError(result);
-            }
-            return {
-              ...result,
-              data: result.body,
-            };
-          });
+          const errorMessage = 'Title should not be empty';
+          postStub.rejects(new Error(errorMessage));
           onSuccessStub = stub();
           onCloseStub = stub();
-          ({ getByText, findByText, queryByText } = render(
+          ({ getByText } = render(
             <CourseModal
               isVisible
               onSuccess={onSuccessStub}
@@ -477,97 +359,6 @@ describe('Course Modal', function () {
           const submitButton = getByText('Submit');
           fireEvent.click(submitButton);
           await wait(() => strictEqual(onCloseStub.callCount, 0));
-        });
-        context('when Area value is missing', function () {
-          it('displays a validation error', async function () {
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            await findByText('Area should not be empty', { exact: false });
-          });
-        });
-        context('when Catalog Prefix value is missing', function () {
-          it('does not display a validation error', function () {
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            const catalogPrefixError = queryByText('Catalog Prefix should not be empty', { exact: false });
-            strictEqual(catalogPrefixError, null);
-          });
-        });
-        context('when Course Number value is missing', function () {
-          it('does not display a validation error', function () {
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            const courseNumberError = queryByText('Course Number should not be empty', { exact: false });
-            strictEqual(courseNumberError, null);
-          });
-        });
-        context('when Course Title value is missing', function () {
-          it('displays a validation error', async function () {
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            await findByText('Title should not be empty', { exact: false });
-          });
-        });
-        context('when Same As value is missing', function () {
-          it('does not display a validation error', function () {
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            const sameAsError = queryByText('Same As should not be empty', { exact: false });
-            strictEqual(sameAsError, null);
-          });
-        });
-        context('when Is Undergraduate value is missing', function () {
-          it('does not display a validation error', function () {
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            const isUndergraduateError = queryByText('Is Undergraduate should not be empty', { exact: false });
-            strictEqual(isUndergraduateError, null);
-          });
-        });
-        context('when Is SEAS value is missing', function () {
-          it('displays a validation error', async function () {
-            const isSEASSelect = getByLabelText('Is SEAS', { exact: false }) as HTMLSelectElement;
-            // Is SEAS dropdown defaults to IS_SEAS.Y
-            fireEvent.change(
-              isSEASSelect,
-              { target: { value: '' } }
-            );
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            await findByText('Is SEAS should not be empty', { exact: false });
-          });
-        });
-        context('when Is SEAS value is not a valid IS_SEAS enum value', function () {
-          it('displays a validation error', async function () {
-            const isSEASSelect = getByLabelText('Is SEAS', { exact: false }) as HTMLSelectElement;
-            fireEvent.change(
-              isSEASSelect,
-              { target: { value: 'invalidValue' } }
-            );
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            await findByText('Is SEAS must be a valid enum value', { exact: false });
-          });
-        });
-        context('when Term Pattern value is missing', function () {
-          it('displays a validation error', function () {
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            const termPatternError = queryByText('Term Pattern should not be empty', { exact: false });
-            strictEqual(termPatternError, null);
-          });
-        });
-        context('when Term Pattern value is not a valid Term Pattern enum value', function () {
-          it('displays a validation error', async function () {
-            const termPatternSelect = getByLabelText('Term Pattern', { exact: false }) as HTMLSelectElement;
-            fireEvent.change(
-              termPatternSelect,
-              { target: { value: 'invalidValue' } }
-            );
-            const submitButton = getByText('Submit');
-            fireEvent.click(submitButton);
-            await findByText('Term Pattern must be a valid enum value', { exact: false });
-          });
         });
       });
     });
