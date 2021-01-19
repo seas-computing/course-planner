90630b7e86aabb0579f343a5fbbad4e63bc228c0
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-01-19 16:52:19 -0500

    Update comments to be in a docblock format
    This is so that the comments show up correctly in documentation.

--- src/common/utils/util.ts
+++ src/common/utils/util.ts
@@ -1,4 +1,6 @@
-// A helper function to convert a string from camel case to title case
+/**
+ * A helper function to convert a string from camel case to title case
+ */
 export const camelCaseToTitleCase = (original: string): string => {
   const sentenceCase = original.replace(/([a-z])([A-Z])/g, '$1 $2');
   return sentenceCase.charAt(0).toUpperCase() + sentenceCase.slice(1);
