3e92f10197b8d6d18a95213bc3fca983a0c9fcbf
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-03-01 18:23:12 -0500

    Remove room creation data
    During our March 1 sprint planning meeting we decided to punt on the
    "Add Room" feature, since the level of validation and normalization
    required to let users create their own rooms would have required
    significant work.
    
    We may revisit this after the MVP is complete.

--- src/common/dto/meeting/MeetingRequest.dto.ts
+++ src/common/dto/meeting/MeetingRequest.dto.ts
@@ -6,52 +6,6 @@ import {
 import { IsMutuallyExclusiveWith } from '../utils';
 
 /**
- * Defines a new room that will be created alongside the meeting.
- */
-
-abstract class MeetingRoomRequest {
-  /**
-   * The campus within the university where the room is located
-   */
-  @ApiModelProperty({
-    type: 'string',
-    example: 'Cambridge',
-  })
-  @IsNotEmpty()
-  public campus: string;
-
-  /**
-   * The name of the building where the room is located
-   */
-  @ApiModelProperty({
-    type: 'string',
-    example: 'Maxwell Dworkin',
-  })
-  @IsNotEmpty()
-  public building: string;
-
-  /**
-   * The name/number of the room
-   */
-  @ApiModelProperty({
-    type: 'string',
-    example: 'G125',
-  })
-  @IsNotEmpty()
-  public name: string;
-
-  /**
-   * The number of people that can use the room at one time
-   */
-  @ApiModelProperty({
-    type: 'number',
-    example: '80',
-  })
-  @IsOptional()
-  public capacity?: number;
-}
-
-/**
  * Represents a request sent to the server to create or edit a meeting
  * associated with a course instance or non class event
  */
@@ -125,24 +79,14 @@ export default abstract class MeetingRequest {
   public nonClassEventId?: string;
 
   /**
-   * Identifies an existing room in the database where the meeting will be held
-   * Either this or [[MeetingRoomRequest]] must be defined
+   * The ID of the room where the meeting will be held, which can be undefined
+   * if one hasn't been chosen yet.
    */
   @ApiModelProperty({
     type: 'string',
     example: 'c7b1fa3f-c5b0-478d-a29c-7f85a4d80109',
   })
   @IsUUID()
-  @IsMutuallyExclusiveWith(['room'])
+  @IsOptional()
   public roomId?: string;
-
-  /**
-   * Defines a new room that should be created in database where the meeting
-   * will be held.  Either this or [[MeetingRoomRequest]] must be defined
-   */
-  @ApiModelProperty({
-    type: MeetingRoomRequest,
-  })
-  @IsMutuallyExclusiveWith(['roomId'])
-  public room?: MeetingRoomRequest;
 }
