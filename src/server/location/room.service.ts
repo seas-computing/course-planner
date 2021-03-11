7d3aaf7487744928379ca10fea64125342ed60d2
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-03-11 16:13:12 -0500

    Initial implementation of availability check
    Makes use of the SQL OVERLAPS operator to check if there are any
    existing reservations for this room whose time overlaps the requested
    time.

---
+++ src/server/location/room.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RoomBookingInfoView } from './RoomBookingInfoView.entity';
+
+@Injectable()
+export class RoomService {
+  @InjectRepository(RoomBookingInfoView)
+  private readonly roomBookRepository: Repository<RoomBookingInfoView>;
+
+  public async checkRoomAvailability(
+    details: Partial<RoomBookingInfoView>
+  ): Promise<boolean> {
+    const {
+      roomId,
+      calendarYear,
+      term,
+      day,
+      startTime,
+      endTime,
+    } = details;
+    const bookings = await this.roomBookRepository.createQueryBuilder()
+      .select('array_agg("meetingTitle")')
+      .groupBy('"roomId"')
+      .addGroupBy('"calendarYear"')
+      .addGroupBy('term')
+      .addGroupBy('day')
+      .where('"roomId"=:roomId', { roomId })
+      .andWhere('term=:term', { term })
+      .andWhere('"calendarYear"=:calendarYear', { calendarYear })
+      .andWhere('"day"=:day', { day })
+      .andWhere(
+        '(:startTime, :endTime) OVERLAPS ("startTime", "endTime")',
+        { startTime, endTime }
+      )
+      .getRawMany();
+    return bookings.length === 0;
+  }
+}
