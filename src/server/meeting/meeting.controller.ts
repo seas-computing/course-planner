b7431a46a5fee99a18c0dd306fa3bb2964e3432d
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-03-12 14:34:35 -0500

    Return proper room object
    The reponse DTO expects the fuller RoomListingView-style object, instead
    of just a Room object, so we need to get the rest of the details from
    that view and replace the room value, assuming there is a room.

--- src/server/meeting/meeting.controller.ts
+++ src/server/meeting/meeting.controller.ts
@@ -6,17 +6,46 @@ import {
 } from '@nestjs/common';
 import MeetingRequestDTO from 'common/dto/meeting/MeetingRequest.dto';
 import MeetingResponseDTO from 'common/dto/meeting/MeetingResponse.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { MeetingService } from './meeting.service';
+import { RoomListingView } from '../location/RoomListingView.entity';
+
+/**
+ * API routes for managing meetings
+ */
 
 @Controller('api/meetings')
 export class MeetingController {
   @Inject(MeetingService)
   private readonly meetingService: MeetingService;
 
+  @InjectRepository(RoomListingView)
+  private readonly roomListingRepository: Repository<RoomListingView>;
+
+  /**
+   * Provides a single route for creating and updating meetings
+   */
   @Put('/')
   public async createOrUpdateMeeting(
     @Body() data: MeetingRequestDTO
   ): Promise<MeetingResponseDTO> {
-    return this.meetingService.saveMeeting(data);
+    let response: MeetingResponseDTO;
+    let room: RoomListingView;
+
+    const saved = await this.meetingService.saveMeeting(data);
+
+    // Our [[MeetingResponseDTO]] expects a richer room object that includes
+    // the [[Campus]] and [[Building]]/[[Room]] concatenated name. It is legal
+    // to create a [[Meeting]] without an associated room, so the fallback returns
+    // a meeting with `room: undefined`
+    if (saved.room) {
+      room = await this.roomListingRepository
+        .findOne(response.room.id);
+    }
+    return {
+      ...saved,
+      room,
+    };
   }
 }
