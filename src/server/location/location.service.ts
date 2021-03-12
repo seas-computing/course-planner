6971e06ac63c48adc0bae8a89a7ff0cfe7d40834
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-03-12 14:23:42 -0500

    Treat empty results as "available"
    If there's a room in the database with no associated bookings, the query
    might return no reults at all. In that case, we should treat the room as
    available.
    
    This commit also provides a type for the return value of the query, and
    adds some additional inline comments to better explain what's happening.

--- src/server/location/location.service.ts
+++ src/server/location/location.service.ts
@@ -3,10 +3,21 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RoomBookingInfoView } from './RoomBookingInfoView.entity';
 
+/**
+ * A service for managing room, building, and campus entities in the database.
+ */
+
 @Injectable()
 export class LocationService {
   @InjectRepository(RoomBookingInfoView)
-  private readonly roomBookRepository: Repository<RoomBookingInfoView>;
+  private readonly roomBookingRepository: Repository<RoomBookingInfoView>;
+
+  /**
+   * Queries the view of room booking info in our database for any existing
+   * bookings that might overlap with the meeting represented by the data in
+   * the details argument. Returns true when there are no conflicts, and false
+   * when there is a conflict
+   */
 
   public async checkRoomAvailability(
     details: Partial<RoomBookingInfoView>
@@ -19,8 +30,14 @@ export class LocationService {
       startTime,
       endTime,
     } = details;
-    const bookings = await this.roomBookRepository.createQueryBuilder()
-      .select('array_agg("meetingTitle")')
+
+    const bookings: {
+      roomId: string,
+      meetingTitles: string[]
+    }[] = await this.roomBookingRepository
+      .createQueryBuilder()
+      .select('roomId')
+      .addSelect('array_agg("meetingTitle")', 'meetingTitles')
       .groupBy('"roomId"')
       .addGroupBy('"calendarYear"')
       .addGroupBy('term')
@@ -34,6 +51,15 @@ export class LocationService {
         { startTime, endTime }
       )
       .getRawMany();
-    return bookings.length === 0;
+
+    // - If nothing is returned from the query, there are no bookings and we
+    //   can return true.
+    // - If there is a room returned, but the list of meetingTitles is empty,
+    //   then there are no overlapping bookings and we can return true.
+    // - If there is a room entry and there are any entries in the
+    //   meetingTitles list, those meetings conflict with the requested meeting
+    //   and we need to return false
+    return bookings.length === 0
+      || bookings[0].meetingTitles.length === 0;
   }
 }
