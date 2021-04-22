afe401517ac229bf268ebbe9c56726279ac9455d
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-04-22 10:33:14 -0400

    Remove unnecessary imports for meeting module
    Room, Building, and Campus are not needed as imports in the meeting module.

--- src/server/meeting/meeting.module.ts
+++ src/server/meeting/meeting.module.ts
@@ -2,18 +2,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { RoomBookingInfoView } from 'server/location/RoomBookingInfoView.entity';
 import { RoomListingView } from 'server/location/RoomListingView.entity';
-import { Room } from 'server/location/room.entity';
-import { Building } from 'server/location/building.entity';
-import { Campus } from 'server/location/campus.entity';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-      Room,
-      Building,
-      Campus,
       RoomListingView,
       RoomBookingInfoView,
     ]),
