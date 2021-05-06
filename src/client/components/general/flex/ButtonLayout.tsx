c2b22d4ee9923d3694e291eaddf2594fdd8c358f
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-05-06 10:34:04 -0400

    Add description for ButtonLayout styled component

--- src/client/components/general/flex/ButtonLayout.tsx
+++ src/client/components/general/flex/ButtonLayout.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
+/**
+ * A wrapper around elements to position elements in a right aligned fashion
+ */
 export default styled.div`
   display: flex;
   flex-direction: row-reverse;
