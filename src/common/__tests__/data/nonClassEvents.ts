seas-computing/course-planner

commit 0d2ec50bca8852864cb3fd95c327b736cef88274

Replace .filter with within().queryAllByRole()
The usage of getAllbyRole().filter seemed to have a cumulative timeout
effect where the tests would pass when run in isolation with .only, but
when run as part of the full test suite would then timeout and fail.

The solution was to use within() to narrow down the search space of the
test function, making the test run faster and preventing the timeout
issue.

More info on this here: https://github.com/testing-library/dom-testing-library/issues/552

src/client/components/pages/NonClassMeetings/__tests__/NonClassMeetingsTable.test.tsx
@@ -5,6 +5,7 @@ import {
   BoundFunction,
   AllByRole,
   getRoles,
+  within,
 } from 'test-utils';
 import {
   dataScienceReadingGroup,
@@ -30,11 +31,8 @@ describe('NonClassMeetings Table', function () {
       );
     });
     it('Renders two rows of headers', function () {
-      const allRows = getAllByRole('row');
-      const headerRows = allRows.filter((row) => {
-        const roles = getRoles(row);
-        return 'columnheader' in roles && roles.columnheader.length > 0;
-      });
+      const headerRows = getAllByRole('row')
+        .filter((row) => within(row).queryAllByRole('columnheader').length > 0);
       strictEqual(headerRows.length, 2);
     });
     it('Renders the semesters into the top header row', function () {
@@ -56,11 +54,8 @@ describe('NonClassMeetings Table', function () {
       );
     });
     it('renders the non class meetings into the body', function () {
-      const allRows = getAllByRole('row');
-      const headerRows = allRows.filter((row) => {
-        const roles = getRoles(row);
-        return !('columnheader' in roles && roles.columnheader.length > 0);
-      });
+      const headerRows = getAllByRole('row')
+        .filter((row) => within(row).queryAllByRole('columnheader').length > 0);
       strictEqual(headerRows.length, nonClassMeetings.length);
     });
   });

commit 38169b24002f5b1cad71c05393ef458ca5c5dead

Flip boolean logic for table body rows
The changes made in 0d2ec50bca8852864cb3fd95c327b736cef88274
accidentally duplicated the logic from the header tests above and missed
the subtlety of the `!` to invert the logic.

This meant that the two tests were effectively testing the same thing,
and the test that was _claiming_ to test the number of rows in the table
body, was _actually_ the number of rows in the table head. Clearly this
is incorrect, but the test passed anyway because the number of rows of
table data(2) was the same as the number of rows of headings (also 2).

This commit adds another row of table (and test) data to ensure that
this cannot happen again.

src/client/components/pages/NonClassMeetings/__tests__/NonClassMeetingsTable.test.tsx
@@ -10,6 +10,7 @@ import {
 import {
   dataScienceReadingGroup,
   computationalModelingofFluidsReadingGroup,
+  appliedMathematicsReadingGroup,
 } from 'testData';
 import NonClassMeetingsTable from '../NonClassMeetingsTable';
 
@@ -18,6 +19,7 @@ describe('NonClassMeetings Table', function () {
   const nonClassMeetings = [
     dataScienceReadingGroup,
     computationalModelingofFluidsReadingGroup,
+    appliedMathematicsReadingGroup,
   ];
   describe('Header', function () {
     let getAllByRole: BoundFunction<AllByRole>;
@@ -54,9 +56,11 @@ describe('NonClassMeetings Table', function () {
       );
     });
     it('renders the non class meetings into the body', function () {
-      const headerRows = getAllByRole('row')
-        .filter((row) => within(row).queryAllByRole('columnheader').length > 0);
-      strictEqual(headerRows.length, nonClassMeetings.length);
+      const tableBodyRows = getAllByRole('row')
+        .filter(
+          (row) => within(row).queryAllByRole('columnheader').length === 0
+        );
+      strictEqual(tableBodyRows.length, nonClassMeetings.length);
     });
   });
 });

src/common/__tests__/data/nonClassEvents.ts
@@ -149,3 +149,78 @@ export const computationalModelingofFluidsReadingGroup = {
     ],
   },
 };
+
+/**
+ * Applied Math Reading Group
+ *
+ * Non-class parent for applied math reading group formatted to match
+ * [[NonClassMeetingResponseDTO]].
+ */
+ export const appliedMathematicsReadingGroup = {
+  area: 'AM',
+  id: '6512a306-3d2f-4a03-97fe-2dc7aac37107',
+  contactName: 'Cliff Taube',
+  contactEmail: 'ctaube@fas.harvard.edu',
+  contactPhone: null,
+  notes: 'This reading group is usually well attended',
+  expectedSize: null,
+  title: 'Applied mathematics reading group',
+  spring: {
+    id: '1cdcc7f4-2f0b-42f5-bd41-51dba7deda16',
+    academicYear: '2020',
+    term: 'SPRING' as TERM,
+    meetings: [
+      {
+        id: 'b5916e2b-42d6-46f3-acb7-dea1e3541a00',
+        day: 'FRI' as DAY,
+        startTime: '12:14 AM',
+        endTime: '12:17 AM',
+        room: {
+          id: '17d19e35-617d-486d-9b40-f30197342c18',
+          name: 'Maxwell Dworkin G115 Lecture Theatre',
+          campus: 'Cambridge',
+        },
+      },
+      {
+        id: 'dbe4a352-6755-4ffb-b0ca-e58e13fa8320',
+        day: 'TUE' as DAY,
+        startTime: '04:00 PM',
+        endTime: '05:00 PM',
+        room: {
+          id: '17d19e35-617d-486d-9b40-f30197342c18',
+          name: 'Maxwell Dworkin G115 Lecture Theatre',
+          campus: 'Cambridge',
+        },
+      },
+    ],
+  },
+  fall: {
+    id: '44f22dbb-d6c7-4673-8a7d-f70aa0562350',
+    academicYear: '2020',
+    term: 'FALL' as TERM,
+    meetings: [
+      {
+        id: '732c4b36-9239-4aa6-b131-c8d5cd15908d',
+        day: 'WED' as DAY,
+        startTime: '08:00 AM',
+        endTime: '10:00 AM',
+        room: {
+          id: '17d19e35-617d-486d-9b40-f30197342c18',
+          name: 'Maxwell Dworkin G115 Lecture Theatre',
+          campus: 'Cambridge',
+        },
+      },
+      {
+        id: '59be22e9-29b4-4b98-87f5-e157cbbc76ee',
+        day: 'TUE' as DAY,
+        startTime: '10:00 AM',
+        endTime: '11:10 AM',
+        room: {
+          id: '17d19e35-617d-486d-9b40-f30197342c18',
+          name: 'Maxwell Dworkin G115 Lecture Theatre',
+          campus: 'Cambridge',
+        },
+      },
+    ],
+  },
+};