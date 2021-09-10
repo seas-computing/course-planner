47cb85547636e4f6ff93ea206e24d42ef3c425ff
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-09-10 14:03:25 -0400

    Add a wrapper span component that updates the font to sans-serif

---
+++ src/client/components/general/typography/FontWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+/**
+ * A wrapper span component that converts the font to sans-serif
+ */
+const FontWrapper = styled.span`
+  font-family: "Roboto", sans-serif;
+`;
+
+export default FontWrapper;
