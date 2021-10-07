14b03229bf0e3ff6af8b30c3567d6b18c10811fc
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-10-07 12:39:15 -0400

    Update AppRouter tests
    The AppRouter tests were updated to check the behavior when there is no metadata in context. The renderRoute function was updated to accept both user and metadata so that we can pass in an empty metadata object. If there is no metadata loaded yet, the loading spinner should still show, since we are waiting for the metadata to load before removing the spinner in the AppRouter file.

--- src/client/components/layout/__tests__/AppRouter.test.tsx
+++ src/client/components/layout/__tests__/AppRouter.test.tsx
@@ -7,11 +7,12 @@ import {
   UserContext, MessageContext, MetadataContext, MetadataContextValue,
 } from 'client/context';
 import { User } from 'common/classes';
+import { MetadataResponse } from 'common/dto/metadata/MetadataResponse.dto';
 import AppRouter from '../AppRouter';
 
 describe('App Router', function () {
-  const Metadata = ({ user, path }) => {
-    const [currentMetadata, setMetadata] = useState(dummy.metadata);
+  const Metadata = ({ user, metadata, path }) => {
+    const [currentMetadata, setMetadata] = useState(metadata);
     const metadataContext = new MetadataContextValue(
       currentMetadata, setMetadata
     );
@@ -29,60 +30,83 @@ describe('App Router', function () {
       </MemoryRouter>
     );
   };
-  const renderRoute = (user: User) => (path: string) => render(
+  const renderRoute = (
+    user: User,
+    metadata: MetadataResponse
+  ) => (path: string) => render(
     <Metadata
       user={user}
+      metadata={metadata}
       path={path}
     />
   );
 
   context('When there is a user in context', function () {
-    const renderWithUser = renderRoute(dummy.regularUser);
+    const emptyMetadata = {
+      currentAcademicYear: undefined,
+      areas: [],
+      semesters: [],
+      catalogPrefixes: [],
+    };
+    const renderWithUserAndMetadata = renderRoute(
+      dummy.regularUser, dummy.metadata
+    );
+    const renderWithUser = renderRoute(
+      dummy.regularUser, emptyMetadata
+    );
     context('/', function () {
-      it('renders the courses component', async function () {
-        const { findByText } = renderWithUser('/');
-        return findByText(/Is SEAS?/);
+      context('When there is metadata in context', function () {
+        it('renders the courses component', async function () {
+          const { findByText } = renderWithUserAndMetadata('/');
+          return findByText(/Is SEAS?/);
+        });
+      });
+      context('When there is no metadata in context', function () {
+        it('renders the LoadSpinner', async function () {
+          const { findByText } = renderWithUser('/courses');
+          return findByText(/Fetching User Data/);
+        });
       });
     });
     context('/course-admin', function () {
       it('renders the CourseAdmin component', function () {
-        const { getByTestId } = renderWithUser('/course-admin');
+        const { getByTestId } = renderWithUserAndMetadata('/course-admin');
         return getByTestId(/courseAdminPage/);
       });
     });
     context('/faculty-admin', function () {
       it('renders the FacultyAdmin component', async function () {
-        const { findAllByText } = renderWithUser('/faculty-admin');
+        const { findAllByText } = renderWithUserAndMetadata('/faculty-admin');
         return findAllByText(/HUID/);
       });
     });
     context('/faculty', function () {
       it('renders the Faculty component', async function () {
-        const { findAllByText } = renderWithUser('/faculty');
+        const { findAllByText } = renderWithUserAndMetadata('/faculty');
         return findAllByText(/Sabbatical Leave/);
       });
     });
     context('/courses', function () {
       it('renders the Courses component', async function () {
-        const { findByText } = renderWithUser('/courses');
+        const { findByText } = renderWithUserAndMetadata('/courses');
         return findByText(/Is SEAS?/);
       });
     });
     context('/four-year-plan', function () {
       it('renders the MultiYearPlan component', async function () {
-        const { findAllByText } = renderWithUser('/four-year-plan');
+        const { findAllByText } = renderWithUserAndMetadata('/four-year-plan');
         return findAllByText(/Catalog Prefix|Catalog Number|Title/);
       });
     });
     context('Undefined routes', function () {
       it('renders the NoMatch component', async function () {
-        const { findByText } = renderWithUser('/foobar');
+        const { findByText } = renderWithUserAndMetadata('/foobar');
         return findByText(/404/);
       });
     });
   });
   context('When there is no user in context', function () {
-    const renderWithoutUser = renderRoute(null);
+    const renderWithoutUser = renderRoute(null, dummy.metadata);
     context('/', function () {
       it('renders the LoadSpinner', async function () {
         const { findByText } = renderWithoutUser('/');
