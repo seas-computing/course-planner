6251077b5db0dbde11496037b62bda944f8d1b63
Robert Main <rmain@seas.harvard.edu>
2021-11-05 11:52:18 -0400

    Add new DTO for updating views

---
+++ src/common/dto/view/UpdateView.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { COURSE_TABLE_COLUMN } from 'common/constants';
+
+/**
+ * Update View DTO
+ *
+ * Defines the data shape for updating a [[View]] entry in the database.
+ */
+export class UpdateViewDto {
+  @ApiProperty({
+    description: 'Array of column names for this view',
+    example: COURSE_TABLE_COLUMN.AREA,
+    enum: COURSE_TABLE_COLUMN,
+  })
+  @IsEnum(COURSE_TABLE_COLUMN, { each: true })
+  public columns: COURSE_TABLE_COLUMN[] = [];
+
+  @ApiProperty({
+    example: 'No semester data',
+    description: 'A descriptive name for this custom view',
+  })
+  @IsString()
+  @MinLength(1)
+  public name: string;
+}
