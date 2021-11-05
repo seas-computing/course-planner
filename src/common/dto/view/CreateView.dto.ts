f5e7e997260989df60734c1f0bee2689cfa7ee27
Robert Main <rmain@seas.harvard.edu>
2021-11-05 10:26:17 -0400

    Remove EPPN From CreateView DTO
    This DTO was created with the intention of mimicking the View entity. However, we have no need for EPPN on the front end, nor should we be exposing it.
    
    Therefore, it has been removed.

--- src/common/dto/view/CreateView.dto.ts
+++ src/common/dto/view/CreateView.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
-  IsEmail,
   IsEnum,
   IsString,
   MinLength,
@@ -13,27 +12,6 @@ import { COURSE_TABLE_COLUMN } from 'common/constants';
  * Defines the data shape for creating a new [[View]] entry in the database.
  */
 export class CreateViewDTO {
-  /**
-   * User EPPN
-   * A user's EPPN is _NOT_ an email address. However according to internet2
-   * documentation, it appears to follow the same format - so despite not being
-   * an email address, we can use an email validator(with a custom message) to
-   * validate it
-   * @see https://spaces.at.internet2.edu/display/InCFederation/2014/08/24/Asserting+ePPN+Across+the+Gateway
-   */
-  @IsEmail(undefined, {
-    message: '$value is an invalid EPPN',
-  })
-  @ApiProperty({
-    example: '4A2849CF119852@harvard.edu',
-    description: 'User EPPN',
-    externalDocs: {
-      url: 'https://spaces.at.internet2.edu/display/federation/user-attr-eppn',
-      description: 'Internet2 EPPN documentation',
-    },
-  })
-  public eppn: string;
-
   @ApiProperty({
     description: 'Array of column names for this view',
     example: COURSE_TABLE_COLUMN.AREA,
