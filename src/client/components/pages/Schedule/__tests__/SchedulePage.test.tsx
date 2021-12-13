6342c9647ba46e4efd1cf6044e54b92e60db7087
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-12-13 14:39:49 -0500

    Implement initial unit tests
    This adds initial tests that make sure the expected term and calendar
    year are passed to the API function. In a future update, these may be
    changed to find the term and year in the output on the pages.
    
    Written during initial mob programming session.
    
    Co-authored-by: Robert Main <50675045+rmainseas@users.noreply.github.com>
    Co-authored-by: Natalyn Middleton-Yu <natalyn.yu@gmail.com>
    Co-authored-by: Faras Sadek <fadel@seas.harvard.edu>

--- src/client/components/pages/Schedule/__tests__/SchedulePage.test.tsx
+++ src/client/components/pages/Schedule/__tests__/SchedulePage.test.tsx
@@ -1,18 +1,59 @@
 import React from 'react';
-import { render, waitForElementToBeRemoved } from 'test-utils';
-import { stub, SinonStub } from 'sinon';
+import { render, waitForElementToBeRemoved, RenderResult } from 'test-utils';
+import { stub, SinonStub, useFakeTimers, SinonFakeTimers } from 'sinon';
 import * as CourseAPI from 'client/api/courses';
 import { strictEqual } from 'assert';
 import * as dummy from 'testData';
+import { MetadataContextValue } from 'client/context/MetadataContext';
 import SchedulePage from '../SchedulePage';
 
 describe('Schedule Page', function () {
   let dispatchMessage: SinonStub;
   let apiStub: SinonStub;
+  let clock: SinonFakeTimers;
+  const metadata = new MetadataContextValue(
+    { ...dummy.metadata, currentAcademicYear: 1999 },
+    () => {}
+  );
+
   beforeEach(function () {
     dispatchMessage = stub();
     apiStub = stub(CourseAPI, 'getCourseScheduleForSemester');
   });
+  describe.only('Requesting Semester Data', function () {
+    context('When current date is before July 1', function () {
+      beforeEach(function () {
+        console.log('Withnout timers', Date.now());
+        apiStub.resolves([]);
+        const fakeDate = new Date(1999, 5, 30);
+        clock = useFakeTimers(fakeDate); 
+        render(
+          <SchedulePage />,
+          { metadataContext: metadata }
+        );
+      });
+      it.only('Should fetch Spring data', function () {
+        // Get academic year from contxt API
+        // Get month from date object
+        const [calendarYear, term] = apiStub.args[0];
+        console.log(term, calendarYear);
+        console.log('with timers', Date.now());
+        //strictEqual(term.includes(/Spring/), true);
+      });
+      it('Should use the academicYear in metadata');
+    })
+    context('When current date is July 1 or later', function () {
+      beforeEach(function () {
+        apiStub.resolves([]);
+        render(
+          <SchedulePage />,
+          { metadataContext: metadata }
+        );
+      });
+      it('Should fetch Fall data');
+      it('Should use the year before the academicYear in metadata')
+    })
+  })
   context('While fetching data', function () {
     it('Should display the spinner', function () {
       apiStub.resolves([]);
