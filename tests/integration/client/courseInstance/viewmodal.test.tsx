881a5ee058de3a36f121c0ba458c909bac68ab78
Robert Main <rmain@seas.harvard.edu>
2021-12-16 13:36:02 -0500

    Add tests for customize view modal
    Add some integration tests to test the usage and closing of the customize view
    modal

--- tests/integration/client/courseInstance/viewmodal.test.tsx
+++ tests/integration/client/courseInstance/viewmodal.test.tsx
@@ -1,28 +1,60 @@
 import React from 'react';
-import { render } from 'test-utils';
 import {
+  render,
   fireEvent,
-  waitForElement,
   RenderResult,
-} from '@testing-library/react';
+  within,
+} from 'test-utils';
 import CoursesPage from 'client/components/pages/Courses/CoursesPage';
 import {
   physicsCourseResponse,
 } from 'testData';
 import { CourseAPI } from 'client/api';
 import { stub } from 'sinon';
+import { strictEqual } from 'assert';
 
-describe('View modal', function () {
+describe('Customize view modal', function () {
   let coursePage: RenderResult;
-  beforeEach(function () {
+  let modal: HTMLElement;
+  let table: HTMLElement;
+
+  beforeEach(async function () {
     stub(CourseAPI, 'getAllCourses').resolves([physicsCourseResponse]);
     coursePage = render(<CoursesPage />);
+    const button = await coursePage.findByText(
+      'Customize View',
+      { exact: false }
+    );
+    table = coursePage.getByRole('table');
+    fireEvent.click(button);
+    modal = await coursePage.findByRole('dialog');
   });
-  it('appears when the "Customize View" button is clicked', function () {
-    return waitForElement(() => {
-      const button = coursePage.getByText('Customize View', { exact: false });
-      fireEvent.click(button);
-      return coursePage.queryByRole('dialog');
-    });
+  it('disappears when the "Done" button is clicked', function () {
+    const doneButton = within(modal).getByText('Done');
+    fireEvent.click(doneButton);
+    const modalRef = coursePage.queryByRole('dialog');
+    strictEqual(modalRef, null);
+  });
+  it('prevents a mandatory column from being hidden', async function () {
+    const mandatoryColumnCheckbox = await within(modal)
+      .findByLabelText('Area');
+    fireEvent.click(mandatoryColumnCheckbox);
+
+    return within(table).findByText('Area');
+  });
+  it('allows visible optional columns to be hidden', async function () {
+    const optionalColumnCheckbox = await within(modal)
+      .findByLabelText('Is SEAS?');
+    fireEvent.click(optionalColumnCheckbox);
+
+    const isSeasColumn = within(table).queryByText('Is SEAS?');
+    strictEqual(isSeasColumn, null);
+  });
+  it('allows invisible optional columns to be shown', async function () {
+    const optionalColumnCheckbox = await within(modal)
+      .findByLabelText('Same As');
+    fireEvent.click(optionalColumnCheckbox);
+
+    return within(table).findByText('Same As');
   });
 });
