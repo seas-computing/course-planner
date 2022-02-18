ec2f758d7d9a00525a633514afacd082b6051093
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-02-18 14:47:57 -0500

    Tweak group-based authorization
    Per the specs in #465, we can refactor our group-based authorization
    strategy by adding two "circuit breaker" clauses:
    
    - Admins can access anything
    - Read-Only pages can be viewed by any member of a valid group
    
    Beyond this, any other use must belong to the group passed to the group
    guard.

--- src/server/auth/group.guard.ts
+++ src/server/auth/group.guard.ts
@@ -20,8 +20,18 @@ class RequireGroup implements CanActivate {
     context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
-    if (request?.session?.user) {
+    if (request?.session?.user?.groups) {
       const user = request.session.user as User;
+      // Allow admins to access any endpoint
+      if (user.groups.includes(GROUP.ADMIN)) {
+        return true;
+      }
+      // Allow members of any group to access read-only endpoints
+      if (this.requiredGroup === GROUP.READ_ONLY) {
+        return user.groups.some(
+          (group) => Object.values(GROUP).includes(group)
+        );
+      }
       return user.groups.includes(this.requiredGroup);
     }
     return false;
