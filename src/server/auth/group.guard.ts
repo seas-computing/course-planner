34e97939639bd0bddfd4fd4780b9aaa844db00c1
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-02-23 10:56:37 -0500

    Extract group checking logic
    This is a pretty simple bit of code, but bringing it up into a utility
    function gives us the flexibility to reuse it in our client
    authorization logic.

--- src/server/auth/group.guard.ts
+++ src/server/auth/group.guard.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 import { GROUP } from 'common/constants';
 import { Request } from 'express';
 import { User } from 'common/classes';
+import { checkUserGroup } from 'common/utils/checkUserGroup';
 
 @Injectable()
 class RequireGroup implements CanActivate {
@@ -21,18 +22,8 @@ class RequireGroup implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     if (request?.session?.user?.groups) {
-      const user = request.session.user as User;
-      // Allow admins to access any endpoint
-      if (user.groups.includes(GROUP.ADMIN)) {
-        return true;
-      }
-      // Allow members of any group to access read-only endpoints
-      if (this.requiredGroup === GROUP.READ_ONLY) {
-        return user.groups.some(
-          (group) => Object.values(GROUP).includes(group)
-        );
-      }
-      return user.groups.includes(this.requiredGroup);
+      const user = new User(request.session.user);
+      return checkUserGroup(user, this.requiredGroup);
     }
     return false;
   }
