e2748a2c0f5bd661062e29f1c67cab114a3af412
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-03-17 14:49:16 -0400

    Export conversion helper from constants

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -3,7 +3,7 @@ export { default as AREA_COLORS, getAreaColor } from './areaColors';
 export { default as CATALOG_PREFIX_COLORS, getCatPrefixColor } from './catalogPrefixColors';
 export { default as FACULTY_TYPE } from './facultyType';
 export { default as DAY } from './day';
-export { default as OFFERED } from './offered';
+export { default as OFFERED, offeredEnumToString } from './offered';
 export { default as TERM_PATTERN, termPatternEnumToString } from './termPattern';
 export { default as GROUP } from './group';
 export { default as AUTH_MODE } from './authMode';
