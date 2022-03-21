293b80f18c5ad9e60eec368028c18067ca39a751
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-03-21 15:24:36 -0400

    Check both startYear and endYear
    This is mainly future-proofing, as the startYear and endYear values
    we're hard-coding will definitely be in the list, but since we plan to
    add query parameters to set those in the future I'd rather have the
    check in there now.
    
    Also updates the docs for the controller route

--- src/server/report/report.controller.ts
+++ src/server/report/report.controller.ts
@@ -1,8 +1,20 @@
 import {
-  Controller, Inject, UseGuards, Get, Res, BadRequestException,
+  Controller,
+  Inject,
+  UseGuards,
+  Get,
+  Res,
+  BadRequestException,
 } from '@nestjs/common';
 import { GROUP } from 'common/constants';
 import { Response } from 'express';
+import Excel from 'exceljs';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
 import { RequireGroup } from '../auth/group.guard';
 import { Authentication } from '../auth/authentication.guard';
 import { ReportService } from './report.service';
@@ -26,16 +38,35 @@ export class ReportController {
   @Inject(SemesterService)
   private readonly semesterService: SemesterService;
 
+  /**
+   * Provides an xlsx report with all of the course data from the main table.
+   * By default, it provides info on course instances in all semesters from the
+   * currentAcademicYear through to the last academic year in the database.
+   * @TODO  Add query string parameters for startYear and endYear (future
+   *        feature)
+   */
+  @ApiTags('Reports')
+  @ApiOperation({ summary: 'Generate an xlsx report on course instances' })
+  @ApiOkResponse({
+    type: Excel.Workbook,
+    description: 'An excel file containing all of the course instance data in a single sheet',
+  })
+  @ApiBadRequestResponse({
+    description: 'If the starting or ending year of the report does not exist in the database',
+  })
   @Get('/courses')
   public async getCoursesReport(
     @Res() res: Response
   ): Promise<void> {
-    const { academicYear: startYear } = this.configService;
     const allYears = await this.semesterService.getYearList();
-    if (!allYears.includes(startYear.toString())) {
+    const { academicYear: startYear } = this.configService;
+    const endYear = parseInt([...allYears].pop(), 10);
+    // This basically can't happen now, but would be possible if we add the
+    // query paramters in the future
+    if (!allYears.includes(startYear.toString())
+        || !allYears.includes(endYear.toString())) {
       throw new BadRequestException();
     }
-    const endYear = parseInt([...allYears].pop(), 10);
     res.set({
       'Content-Type': 'application/octet-stream',
       'Content-Disposition': `attachment; filename="courses_${startYear}-${endYear}.xlsx"`,
