edf0bee252b1466117bb289d12803e3f84757d4d
Robert Main <rmain@seas.harvard.edu>
2022-04-13 12:47:25 -0400

    Change button ID to match what's expected by `setButtonRef`
    The `setButtonRef` function expects the button ID to be in the format of
    $fieldKey-$courseId. Therefore, the button having an id of
    'edit-course-notes-$courseId' was causing the button not to be focused
    correctly

--- src/client/components/pages/Courses/tableFields.tsx
+++ src/client/components/pages/Courses/tableFields.tsx
@@ -615,7 +615,7 @@ export const tableFields: CourseInstanceListColumn[] = [
       const titleText = hasNotes ? 'View/Edit Notes' : 'Add Notes';
       return (
         <BorderlessButton
-          id={`edit-course-notes-${course.id}`}
+          id={`notes-${course.id}`}
           variant={VARIANT.INFO}
           onClick={({ currentTarget }) => {
             openNotesModal(course, currentTarget.id);
