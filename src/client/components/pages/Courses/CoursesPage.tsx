66ae4063d5e539ab34397c9b13508e6b95898245
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-04-19 11:45:27 -0400

    Remove download logic from CoursesPage
    Replaces the logic for initiating the download with logic for managing
    the modal state, so that we can move the actual downloading down into
    the modal.

--- src/client/components/pages/Courses/CoursesPage.tsx
+++ src/client/components/pages/Courses/CoursesPage.tsx
@@ -44,7 +44,7 @@ import MeetingModal from './MeetingModal';
 import InstructorModal from './InstructorModal';
 import { filterCoursesByInstructors } from '../utils/filterByInstructorValues';
 import OfferedModal from './OfferedModal';
-import downloadAttachment from '../utils/downloadAttachment';
+import ReportDownloadModal from './ReportDownloadModal';
 
 /**
  * The initial, empty state for the filters
@@ -112,6 +112,11 @@ const CoursesPage: FunctionComponent = (): ReactElement => {
     currentViewColumns,
     setCurrentViewColumns,
   ] = useState(defaultView.columns);
+
+  const [
+    reportModalVisible,
+    setReportModalVisible,
+  ] = useState(false);
 
   const dispatchMessage = useContext(MessageContext);
 
@@ -491,28 +496,6 @@ const CoursesPage: FunctionComponent = (): ReactElement => {
     )
   ), [currentViewColumns]);
 
-  /**
-   * Handle downloading our course report, and track the download state so we
-   * can change the button display
-   */
-  const [reportDownloading, setReportDownloading] = useState(false);
-  const downloadCoursesReport = useCallback(() => {
-    setReportDownloading(true);
-    fetch(
-      `${process.env.SERVER_URL}/report/courses`,
-      { credentials: 'include' }
-    ).then(downloadAttachment)
-      .catch((err: Error) => {
-        dispatchMessage({
-          message: new AppMessage(err.message, MESSAGE_TYPE.ERROR),
-          type: MESSAGE_ACTION.PUSH,
-        });
-      })
-      .finally(() => {
-        setReportDownloading(false);
-      });
-  }, [dispatchMessage, setReportDownloading]);
-
   return (
     <div className="course-instance-table">
       <VerticalSpace>
@@ -533,16 +516,15 @@ const CoursesPage: FunctionComponent = (): ReactElement => {
         </ViewModal>
         <MenuFlex>
           <Button
-            variant={reportDownloading ? VARIANT.DEFAULT : VARIANT.INFO}
+            variant={VARIANT.INFO}
             alt="Download a spreadsheet with course data"
-            onClick={downloadCoursesReport}
-            disabled={reportDownloading}
+            onClick={() => { setReportModalVisible(true); }}
           >
             <FontAwesomeIcon
               icon={faDownload}
             />
             {' '}
-            {reportDownloading ? 'Download in Progress' : 'Download Course Report'}
+            Download Course Report
           </Button>
           <Button
             variant={VARIANT.INFO}
@@ -676,6 +658,10 @@ const CoursesPage: FunctionComponent = (): ReactElement => {
           wasFallUpdated);
           closeOfferedModal();
         }}
+      />
+      <ReportDownloadModal
+        isVisible={reportModalVisible}
+        closeModal={() => { setReportModalVisible(false); }}
       />
     </div>
   );
