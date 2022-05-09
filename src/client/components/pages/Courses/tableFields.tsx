6589d70fab45af8d4cb98504fbd2b722be0ceb3d
Robert Main <rmain@seas.harvard.edu>
2022-05-09 15:15:24 -0400

    Collapse enrollment fields
    Collapse individual enrollment fields into one column

--- src/client/components/pages/Courses/tableFields.tsx
+++ src/client/components/pages/Courses/tableFields.tsx
@@ -15,7 +15,15 @@ import {
   TextInput,
 } from 'mark-one';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStickyNote as withNotes, faFolderOpen, faEdit } from '@fortawesome/free-solid-svg-icons';
+import {
+  faStickyNote as withNotes,
+  faFolderOpen,
+  faEdit,
+  IconDefinition,
+  faShoppingCart,
+  faCalendar,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons';
 import { faStickyNote as withoutNotes } from '@fortawesome/free-regular-svg-icons';
 import {
   TERM,
@@ -298,6 +306,68 @@ export const formatMeetings = (
   );
 });
 
+type EnrollmentField = {
+  icon: IconDefinition,
+  name: string,
+  count?: number,
+  key: string,
+};
+
+/**
+ * Helper function that returns a functional component to render enrollment
+ * data in an unordered list for display in a single column
+ */
+export const formatEnrollment = (
+  term: TERM
+): FunctionComponent<FieldContentProps> => React.memo((
+  { course }: FieldContentProps
+): ReactElement => {
+  const semKey = term.toLowerCase() as TermKey;
+  const { [semKey]: instance } = course;
+  const enrollmentData: EnrollmentField[] = [
+    {
+      name: 'Pre-enrollment',
+      key: 'pre',
+      count: instance.preEnrollment,
+      icon: faShoppingCart,
+    },
+    {
+      name: 'Study card enrollment',
+      key: 'study',
+      count: instance.studyCardEnrollment,
+      icon: faCalendar,
+    },
+    {
+      name: 'Actual enrollment',
+      key: 'actual',
+      count: instance.actualEnrollment,
+      icon: faUsers,
+    },
+  ];
+  return (
+    <TableCellList>
+      {
+        enrollmentData
+          .filter(({ count }) => count !== null)
+          .map((item) => (
+            <TableCellListItem
+              key={item.key}
+              title={item.name}
+              aria-label={item.name}
+            >
+              <FontAwesomeIcon
+                icon={item.icon}
+                fixedWidth
+              />
+              {' '}
+              { item.count }
+            </TableCellListItem>
+          ))
+      }
+    </TableCellList>
+  );
+});
+
 /**
  * Describes the additional options passed into the FieldContent functional
  * component
@@ -536,25 +606,11 @@ export const tableFields: CourseInstanceListColumn[] = [
     FieldContent: formatMeetings(TERM.FALL),
   },
   {
-    name: 'Pre',
-    key: 'pre-enrollment-fall',
+    name: 'Enrollment',
+    key: 'enrollment-fall',
     columnGroup: COURSE_TABLE_COLUMN_GROUP.FALL,
     viewColumn: COURSE_TABLE_COLUMN.ENROLLMENT,
-    FieldContent: retrieveValue('preEnrollment', TERM.FALL),
-  },
-  {
-    name: 'Study',
-    key: 'study-card-enrollment-fall',
-    columnGroup: COURSE_TABLE_COLUMN_GROUP.FALL,
-    viewColumn: COURSE_TABLE_COLUMN.ENROLLMENT,
-    FieldContent: retrieveValue('studyCardEnrollment', TERM.FALL),
-  },
-  {
-    name: 'Actual',
-    key: 'actual-enrollment-fall',
-    columnGroup: COURSE_TABLE_COLUMN_GROUP.FALL,
-    viewColumn: COURSE_TABLE_COLUMN.ENROLLMENT,
-    FieldContent: retrieveValue('actualEnrollment', TERM.FALL),
+    FieldContent: formatEnrollment(TERM.FALL),
   },
   {
     name: 'Offered',
@@ -580,25 +636,11 @@ export const tableFields: CourseInstanceListColumn[] = [
     FieldContent: formatMeetings(TERM.SPRING),
   },
   {
-    name: 'Pre',
-    key: 'pre-enrollment-spring',
+    name: 'Enrollment',
+    key: 'enrollment-spring',
     columnGroup: COURSE_TABLE_COLUMN_GROUP.SPRING,
     viewColumn: COURSE_TABLE_COLUMN.ENROLLMENT,
-    FieldContent: retrieveValue('preEnrollment', TERM.SPRING),
-  },
-  {
-    name: 'Study',
-    key: 'study-card-enrollment-spring',
-    columnGroup: COURSE_TABLE_COLUMN_GROUP.SPRING,
-    viewColumn: COURSE_TABLE_COLUMN.ENROLLMENT,
-    FieldContent: retrieveValue('studyCardEnrollment', TERM.SPRING),
-  },
-  {
-    name: 'Actual',
-    key: 'actual-enrollment-spring',
-    columnGroup: COURSE_TABLE_COLUMN_GROUP.SPRING,
-    viewColumn: COURSE_TABLE_COLUMN.ENROLLMENT,
-    FieldContent: retrieveValue('actualEnrollment', TERM.SPRING),
+    FieldContent: formatEnrollment(TERM.SPRING),
   },
   {
     name: 'Notes',
