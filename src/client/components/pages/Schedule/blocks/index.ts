6a1fd0bff88522e16c3ec84ab2e961311cdb7c24
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-06-02 16:21:39 -0400

    Create buttons for course listings
    This adds a mostly style-less button to use within our course listings,
    which provides a semantically sensible way to show the Popover with
    additional information when the user clicks on one.

--- src/client/components/pages/Schedule/blocks/index.ts
+++ src/client/components/pages/Schedule/blocks/index.ts
@@ -1,4 +1,5 @@
 export { default as CourseListing } from './CourseListing';
+export { default as CourseListingButton } from './CourseListingButton';
 export { default as DayBlock } from './DayBlock';
 export { default as SessionBlock } from './SessionBlock';
 export { default as WeekBlock } from './WeekBlock';
