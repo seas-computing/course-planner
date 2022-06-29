8ccff396afd5145305c143be696a6f47eddc71bc
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-06-29 16:21:55 -0400

    Add fade/highlight props to blocks
    First, this adds the isFaded prop to the SessionBlock, so that we can
    selectively fade out the blocks that are either being hidden (via the
    still-to-be-added filter) or that do not contain a meeting of the
    currently chosen course.
    
    Additionally, we're adding isFaded and isHighlighted props to the
    CourseListingButton, so that we can dim out any other meetings inside of
    a highlighted block when one of the courses is selected.

--- src/client/components/pages/Schedule/blocks/CourseListingButton.tsx
+++ src/client/components/pages/Schedule/blocks/CourseListingButton.tsx
@@ -1,15 +1,31 @@
 import styled from 'styled-components';
 
+interface CourseListingButtonProps {
+  /**
+   * Whether this button should have a highlighted background
+   */
+  isHighlighted?: boolean;
+
+  /**
+   * Whether the text of the button should fade into the background, e.g. when
+   * one of the other courses in this block is highlighted
+   */
+  isFaded?: boolean;
+}
+
 /**
  * An essentially unstyled button used to trigger the Popover containing the
  * course details
  */
 const CourseListingButton = styled.button.attrs({
   type: 'button',
-})`
-  background: transparent;
+})<CourseListingButtonProps>`
+  background-color: ${({ isHighlighted }) => (
+    isHighlighted ? 'rgba(255, 255, 255, 0.5)' : 'transparent'
+  )};
   border: none;
   font-size: inherit;
+  opacity: ${({ isFaded }) => (isFaded ? '0.5' : '1')};
   padding: 0;
   margin: 0;
   width: 100%;
