07548c936420528f8aadf67b5758906ddb1d32a9
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2022-12-06 16:51:04 -0500

    Improve service method logic for finding duplicate rooms
    Instead of finding all rooms in the database with the same building and room name and then calling filter on the array of rooms to remove the room currently being edited, an "andWhere" clause was added to filter out the room.

--- src/server/location/location.service.ts
+++ src/server/location/location.service.ts
@@ -292,18 +292,17 @@ export class LocationService {
       relations: ['building'],
     });
 
-    // Check that the entered room is not a duplicate of an existing room
+    // Check that the entered room is not a duplicate of an existing room.
+    // If the user submits the edit modal without updating the room name, make
+    // sure that they don't get an error for creating a duplicate room.
     const dbRooms = await this.roomListingViewRepository.createQueryBuilder()
       .where('LOWER(name) = LOWER(:name)', {
         name: `${existingRoom.building.name} ${roomInfo.name}`,
       })
+      .andWhere('id <> :roomId', { roomId })
       .getMany();
 
-    // If the user submits the edit modal without updating the room name, make
-    // sure that they don't get an error for creating a duplicate room.
-    const otherRooms = dbRooms.filter((room) => room.id !== roomId);
-
-    if (otherRooms.length > 0) {
+    if (dbRooms.length > 0) {
       throw new BadRequestException(`The room ${roomInfo.name} already exists in ${existingRoom.building.name}.`);
     }
 
