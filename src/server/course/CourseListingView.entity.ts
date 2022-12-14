03974c42f8881ddc5af9dd3f4f537d072051346d
Jonathan Seitz <jseitz@seas.harvard.edu>
2022-12-13 21:25:29 -0500

    Update view entity to fetch all related courses
    This implementation is likely not the most efficient solution, but we
    can use a CASE statement to handle all of the possible sets of
    parent/child/sibling relations, and return a comma-separated string of
    catalog numbers. For our canonical case of CS 109A and its "children" AC
    209A and STATS 109A, this will return:
    
    |     course |              sameAs |
    |------------|---------------------|
    |    CS 109A | AC 209A, STATS 109A |
    |    AC 209A | CS 109A, STATS 109A |
    | STATS 109A |    CS 109A, AC 209A |

--- src/server/course/CourseListingView.entity.ts
+++ src/server/course/CourseListingView.entity.ts
@@ -21,12 +21,39 @@ import { CourseInstanceListingView } from 'server/courseInstance/CourseInstanceL
     .addSelect('c.title', 'title')
     .addSelect('c."isUndergraduate"', 'isUndergraduate')
     .addSelect("CONCAT_WS(' ', c.prefix, c.number)", 'catalogNumber')
-    .addSelect('c."sameAs"', 'sameAs')
+    .addSelect('ARRAY_AGG(CONCAT_WS(\' \', "childCourses".prefix, "childCourses".number))', 'childCourseNumbers')
+    .addSelect(`
+      CASE
+        -- 1. A course with children won't have a parent or siblings
+        WHEN count("childCourses".id) > 0
+          THEN STRING_AGG(CONCAT_WS(' ', "childCourses".prefix, "childCourses".number), ', ')
+        -- 2. A course with siblings must have a parent AND cannot have children
+        WHEN count("siblingCourses".id) > 0
+          THEN CONCAT_WS(
+            ', ',
+            CONCAT_WS(' ', "parentCourse".prefix, "parentCourse".number),
+            STRING_AGG(CONCAT_WS(' ', "siblingCourses".prefix, "siblingCourses".number), ', ')
+          )
+        -- 3. A course with a parent but no siblings cannot have children
+        WHEN c."sameAsId" IS NOT NULL
+          THEN CONCAT_WS(' ', "parentCourse".prefix, "parentCourse".number)
+        -- 4. Default to empty string
+        ELSE ''
+      END`, 'sameAs')
     .addSelect('c."isSEAS"', 'isSEAS')
     .addSelect('c.notes', 'notes')
     .addSelect('c."termPattern"', 'termPattern')
+    .addSelect('c."sameAsId"', 'sameAsId')
     .leftJoin(Area, 'a', 'c."areaId" = a.id')
-    .from(Course, 'c'),
+    .leftJoin(Course, 'parentCourse', '"parentCourse"."id" = c."sameAsId"')
+    .leftJoin(Course, 'childCourses', '"childCourses"."sameAsId" = c.id')
+    .leftJoin(Course, 'siblingCourses', '"siblingCourses"."sameAsId" = c."sameAsId" AND "siblingCourses".id <> c.id')
+    .from(Course, 'c')
+    .groupBy('c.id')
+    .addGroupBy('c.title')
+    .addGroupBy('a.name')
+    .addGroupBy('"parentCourse".prefix')
+    .addGroupBy('"parentCourse".number'),
 })
 export class CourseListingView {
   /**
