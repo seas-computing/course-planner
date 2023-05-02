ac570e91fcf416d35210be60f22e5ae67bd15bb7
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2023-05-02 14:42:24 -0400

    Fix Schedule end to end test
    This test was originally sporadically failing. The test was improved by looking for a test course instance with a non-null sameAs value in order to find a parent-child course pair as opposed instead of taking a course and updating the sameAs value in the course admin modal. This takes out several unnecessary steps and cuts down on the amount of time the test needs to run. One important note is to make sure that the test course instance has an isSEAS value of IS_SEAS.Y. Otherwise, the course will not show in the schedule - this was a blocker that took a while to figure out.

--- tests/integration/e2e/SchedulePage.test.tsx
+++ tests/integration/e2e/SchedulePage.test.tsx
@@ -15,18 +15,24 @@ import { SessionModule } from 'nestjs-session';
 import { MemoryRouter } from 'react-router-dom';
 import App from 'client/components/App';
 import request from 'client/api/request';
-import { Repository, Not } from 'typeorm';
+import {
+  Repository,
+  Not,
+  IsNull,
+} from 'typeorm';
 import { SemesterModule } from 'server/semester/semester.module';
 import { Meeting } from 'server/meeting/meeting.entity';
 import { dayEnumToString } from 'common/constants/day';
 import { PGTime } from 'common/utils/PGTime';
+import { CourseInstance } from 'server/courseInstance/courseinstance.entity';
+import { Semester } from 'server/semester/semester.entity';
 import mockAdapter from '../../mocks/api/adapter';
 import { ConfigModule } from '../../../src/server/config/config.module';
 import { ConfigService } from '../../../src/server/config/config.service';
 import { AuthModule } from '../../../src/server/auth/auth.module';
 import { TestingStrategy } from '../../mocks/authentication/testing.strategy';
 import {
-  AUTH_MODE, TERM_PATTERN,
+  AUTH_MODE, IS_SEAS, OFFERED, TERM,
 } from '../../../src/common/constants';
 import { PopulationModule } from '../../mocks/database/population/population.module';
 import { CourseModule } from '../../../src/server/course/course.module';
@@ -41,6 +47,7 @@ describe('End-to-end Schedule Page tests', function () {
   let testModule: TestingModule;
   let authStub: SinonStub;
   let meetingRepository: Repository<Meeting>;
+  let ciRepository: Repository<CourseInstance>;
   const currentAcademicYear = 2019;
   let renderResult: RenderResult;
 
@@ -96,6 +103,9 @@ describe('End-to-end Schedule Page tests', function () {
     meetingRepository = testModule.get(
       getRepositoryToken(Meeting)
     );
+    ciRepository = testModule.get(
+      getRepositoryToken(CourseInstance)
+    );
     const nestApp = await testModule
       .createNestApplication()
       .useGlobalPipes(new BadRequestExceptionPipe())
@@ -108,91 +118,76 @@ describe('End-to-end Schedule Page tests', function () {
     return testModule.close();
   });
   describe('Rendering Schedule Information', function () {
-    let parentMeetings: Meeting[];
-    let childMeetings: Meeting[];
+    let testCi: CourseInstance;
+    let parentMeeting: Meeting;
+    let parentMeetingSemester: Semester;
+    let parentMeetingYear: string;
+    let parentMeetingTerm: TERM;
     beforeEach(async function () {
-      parentMeetings = await meetingRepository.find({
-        relations: ['courseInstance', 'courseInstance.course', 'courseInstance.course.area', 'room', 'room.building'],
+      // Find a course instance that has a same as value. The same as course
+      // will be used as the parent.
+      testCi = await ciRepository.findOneOrFail({
+        relations: ['course', 'course.sameAs', 'semester'],
         where: {
-          courseInstance: {
-            course: {
-              termPattern: TERM_PATTERN.BOTH,
-            },
+          offered: Not(OFFERED.RETIRED),
+          course: {
+            isSEAS: IS_SEAS.Y,
+            sameAs: Not(IsNull()),
           },
         },
       });
-      const parentMeetingCourse = parentMeetings[0].courseInstance.course;
-      childMeetings = await meetingRepository.find({
-        relations: ['courseInstance', 'courseInstance.course', 'courseInstance.course.area', 'room', 'room.building'],
+
+      parentMeeting = await meetingRepository.findOneOrFail({
+        relations: ['courseInstance', 'courseInstance.course', 'courseInstance.course.sameAs', 'courseInstance.semester', 'room', 'room.building'],
         where: {
-          day: Not(parentMeetings[0].day),
           courseInstance: {
-            course: {
-              id: Not(parentMeetingCourse.id),
-              area: {
-                name: Not(parentMeetingCourse.area.name),
-              },
-            },
+            offered: Not(OFFERED.RETIRED),
+            course: testCi.course.sameAs.id,
           },
         },
       });
+
+      parentMeetingSemester = parentMeeting.courseInstance.semester;
+      parentMeetingYear = parentMeetingSemester.academicYear;
+      parentMeetingTerm = parentMeetingSemester.term;
+
       renderResult = render(
-        <MemoryRouter initialEntries={['/course-admin']}>
+        <MemoryRouter initialEntries={['/schedule']}>
           <App />
         </MemoryRouter>
       );
     });
     context('when a course is a child of another', function () {
       it('should display the meeting information of the parent course', async function () {
-        const testMeeting = parentMeetings[0];
-        const testParentCourse = testMeeting.courseInstance.course;
-        const testChildCourse = childMeetings[0].courseInstance.course;
-        await renderResult.findAllByText(testChildCourse.title);
+        await waitForElementToBeRemoved(() => renderResult.getByText('Fetching User Data'));
+        await waitForElementToBeRemoved(() => renderResult.getByText('Fetching Course Schedule'));
+        const semesterDropdown = renderResult.getByLabelText(/semester/i) as HTMLSelectElement;
 
-        // Before setting the child course's "Same As" value to the parent,
-        // we are filtering the Course Admin table by area, because there are
-        // courses with the same title and differing catalog numbers. When
-        // selecting the edit button, we want to make sure we are selecting
-        // the intended one.
-        const areaLabelText = 'The table will be filtered as selected in this area dropdown filter';
-        const areaFilter = renderResult
-          .getByLabelText(areaLabelText) as HTMLSelectElement;
-        fireEvent.change(areaFilter,
-          { target: { value: testChildCourse.area.name } });
-        const catalogText = 'The table will be filtered as characters are typed in this course filter field';
-        const catalogFilter = renderResult
-          .getByLabelText(catalogText) as HTMLInputElement;
-        fireEvent.change(catalogFilter,
-          { target: { value: `${testChildCourse.prefix} ${testChildCourse.number}` } });
-        // Open the child course modal to edit the "Same As" value
-        const editButton = await renderResult
-          .findByLabelText(`Edit course information for ${testChildCourse.title}`,
-            { exact: false });
-        fireEvent.click(editButton);
-        await renderResult.findByRole('dialog');
-        const sameAsSelect = renderResult.getByLabelText('Same As', { exact: false }) as HTMLSelectElement;
-        fireEvent.change(
-          sameAsSelect,
-          { target: { value: testParentCourse.id } }
-        );
-        const submitButton = renderResult.getByText('Submit');
-        fireEvent.click(submitButton);
-        await waitForElementToBeRemoved(() => renderResult.getByRole('dialog'));
+        // Since the semester dropdown contains calendar years, the selection
+        // must be changed depending on the semester.
+        if (parentMeetingTerm === TERM.SPRING) {
+          fireEvent.change(semesterDropdown, { target: { value: `${parentMeetingTerm} ${parentMeetingYear}` } });
+        } else {
+          fireEvent.change(semesterDropdown, { target: { value: `${parentMeetingTerm} ${parseInt(parentMeetingYear, 10) - 1}` } });
+        }
 
-        // Navigate to the schedule page and wait for the table to render
-        const scheduleTab = await renderResult.findByText('Schedule');
-        fireEvent.click(scheduleTab);
+        await waitForElementToBeRemoved(() => renderResult.getByText(
+          'Fetching Course Schedule'
+        ));
+        await renderResult.findByText('7 PM');
+        const testParentCourse = parentMeeting.courseInstance.course;
+        const testChildCourse = testCi.course;
         const parentCatalogNumber = `${testParentCourse.prefix} ${testParentCourse.number}`;
         await renderResult.findAllByText(parentCatalogNumber, { exact: false });
 
         // Make sure the child course displays the parent course's meeting information
-        const parentCourseDay = dayEnumToString(testMeeting.day);
-        const parentStartTime = PGTime.toDisplay(testMeeting.startTime);
-        const parentEndTime = PGTime.toDisplay(testMeeting.endTime);
-        const parentLocation = `${testMeeting.room.building.name} ${testMeeting.room.name}`;
+        const parentCourseDay = dayEnumToString(parentMeeting.day);
+        const parentStartTime = PGTime.toDisplay(parentMeeting.startTime);
+        const parentEndTime = PGTime.toDisplay(parentMeeting.endTime);
+        const parentLocation = `${parentMeeting.room.building.name} ${parentMeeting.room.name}`;
         const parentMeetingInfo = `${parentCourseDay}, ${parentStartTime} to ${parentEndTime} in ${parentLocation}`;
         const childCourseInfo = `${testChildCourse.prefix} ${testChildCourse.number} on ${parentMeetingInfo}`;
-        await renderResult.findByText(childCourseInfo);
+        await renderResult.findByText(childCourseInfo, { exact: false });
       }).timeout(120000);
     });
   });
